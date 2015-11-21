6a7404a61fb9554159e6b6f8587df6829de100bb
Han-Sheng Chin <hschin@gmail.com>
2015-11-21 18:33:41 +0800

    Log data returned from LinkedIn API

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   has_one :profile, dependent: :destroy
 
   def self.from_omniauth(auth)
+    logger.info auth
+
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
