802cf9a08948b120e7f0d61cb02b45ec8e64cf55
Han-Sheng Chin <hschin@gmail.com>
2015-11-27 17:56:18 +0800

    Add contact and t&c fields for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124073720) do
+ActiveRecord::Schema.define(version: 20151127094407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,9 @@ ActiveRecord::Schema.define(version: 20151124073720) do
     t.string   "uid"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "contact_number"
+    t.boolean  "allow_contact"
+    t.boolean  "agree_terms"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
