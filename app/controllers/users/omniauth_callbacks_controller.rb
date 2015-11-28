5a5962b9d05a5bf10fb671638179f8cffff5ef58
Han-Sheng Chin <hschin@gmail.com>
2015-11-28 17:26:24 +0800

    Fix profile and experience creation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,29 +11,33 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       session["devise.linkedin_data"] = request.env["omniauth.auth"]
 
-      profile = @user.create_profile!(
-        headline: auth.extra.raw_info.headline,
-        summary: auth.extra.raw_info.summary,
-        industry: auth.extra.raw_info.industry,
-        specialties: auth.extra.raw_info.specialties,
-        image_url: auth.extra.raw_info.pictureUrls["values"][0],
-        linkedin_url: auth.extra.raw_info.publicProfileUrl,
-        location: auth.extra.raw_info.location.name,
-        country_code: auth.extra.raw_info.location.country.code
-      )
-
-      unless auth.extra.raw_info.positions["_total"] == 0
-        position = auth.extra.raw_info.positions["values"][0]
-        experience = profile.create_experience!(
-          title: position.title,
-          company: position.company.name,
-          start_date: Date.parse('position.startDate.month + " " + position.startDate.year'),
-          description: position.summary
+      if @user.save
+        profile = @user.create_profile(
+          headline: auth.extra.raw_info.headline,
+          summary: auth.extra.raw_info.summary,
+          industry: auth.extra.raw_info.industry,
+          specialties: auth.extra.raw_info.specialties,
+          image_url: auth.extra.raw_info.pictureUrls["values"][0],
+          linkedin_url: auth.extra.raw_info.publicProfileUrl,
+          location: auth.extra.raw_info.location.name,
+          country_code: auth.extra.raw_info.location.country.code
         )
-      end
 
-      sign_in @user
-      redirect_to new_account_path
+        unless auth.extra.raw_info.positions["_total"] == 0
+          position = auth.extra.raw_info.positions["values"][0]
+          experience = profile.experiences.create(
+            title: position.title,
+            company: position.company.name,
+            start_date: Date.parse('position.startDate.month + " " + position.startDate.year'),
+            description: position.summary
+          )
+        end
+
+        sign_in @user
+        redirect_to new_account_path
+      else
+        redirect_to register_path(params["role"])
+      end
     end
   end
 end
