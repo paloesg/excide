c72a99f2d967fcc2b201a913de3deab206e9127a
Han-Sheng Chin <hschin@gmail.com>
2015-12-15 17:29:08 +0800

    Clean up profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,23 +2,9 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
-  # GET /profiles
-  # GET /profiles.json
-  def index
-    @profiles = Profile.all
-  end
-
-  # GET /profiles/1
-  # GET /profiles/1.json
   def show
   end
 
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
-  end
-
-  # GET /profiles/1/edit
   def edit
     if @experiences.blank?
       @experiences.build
@@ -29,8 +15,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # POST /profiles
-  # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
 
@@ -45,23 +29,11 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
   def update
     if @profile.update(profile_params)
       redirect_to profile_path, notice: 'Profile was successfully updated.'
     else
       render :edit
-    end
-  end
-
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
-  def destroy
-    @profile.destroy
-    respond_to do |format|
-      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
