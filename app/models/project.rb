6554ef0987f9c22b82883e886ac4447caa1ceb05
Han-Sheng Chin <hschin@gmail.com>
2016-01-07 16:50:05 +0800

    Refine project form

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ActiveRecord::Base
   belongs_to :business
 
   enum category: [:category1, :category2, :category3]
-  enum budget_type: [:fixed, :rate]
-  enum status: [:draft, :pending, :published, :hidden, :closed, :completed]
+  enum budget_type: [:fixed_rate, :hourly_rate, :daily_rate, :weekly_rate, :monthly_rate]
+  enum status: [:draft, :in_review, :published, :hidden, :closed, :completed]
 end
