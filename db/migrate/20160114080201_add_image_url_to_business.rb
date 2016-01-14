aa55ef5e8fc95fa6d520b6d34fea737f505f8ea6
Han-Sheng Chin <hschin@gmail.com>
2016-01-14 16:26:44 +0800

    Add image url field to businesses

---
+++ db/migrate/20160114080201_add_image_url_to_business.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToBusiness < ActiveRecord::Migration
+  def change
+    add_column :businesses, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107064110) do
+ActiveRecord::Schema.define(version: 20160114080201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160107064110) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
+    t.string   "image_url"
   end
 
   add_index "businesses", ["user_id"], name: "index_businesses_on_user_id", using: :btree
