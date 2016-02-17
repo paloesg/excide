4590438c54b7b50974896807a6c3ab7e8e518f90
Han-Sheng Chin <hschin@gmail.com>
2016-02-17 16:54:06 +0800

    Update grant types for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
 
   enum budget_type: [:fixed_rate, :hourly_rate, :daily_rate, :weekly_rate, :monthly_rate]
   enum status: [:draft, :in_review, :active, :closed, :completed]
-  enum grant: [:icv, :cdg]
+  enum grant: ["Capability and Development Grant", "Global Company Partnership Grant", "HR Shared Services Programme", "Innovation and Capability Voucher", "iSprint", "Market Readiness Assistance Grant", "Productivity and Innovation Credit"]
 
   validates :title, :project_category_id, :description, :start_date, :end_date, :budget, presence: true
 end
