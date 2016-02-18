9c95606ad1fc7760309de0fad865ae1e2f40d915
Han-Sheng Chin <hschin@gmail.com>
2016-02-18 17:04:37 +0800

    Modify proposal submission flow so that proposal is submitted from project details page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_projects, only: [:index, :show]
+  before_action :set_s3_direct_post, only: [:show]
 
   def index
   end
@@ -8,6 +9,8 @@ class ProjectsController < ApplicationController
   def show
     @project = @projects.find(params[:id])
     @business = @project.business
+
+    @proposal = Proposal.new(project_id: @project.id)
   end
 
   private
@@ -15,5 +18,9 @@ class ProjectsController < ApplicationController
   def get_projects
     @user = current_user
     @projects = Project.active
+  end
+
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   end
 end
