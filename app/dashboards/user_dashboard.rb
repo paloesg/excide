b746ed46568567ef66160d138a3d02bef6f727ee
Han-Sheng Chin <hschin@gmail.com>
2016-02-19 17:47:57 +0800

    Update user fields in admin dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -8,24 +8,27 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    first_name: Field::String,
+    last_name: Field::String,
+    email: Field::String,
+    contact_number: Field::String,
     profile: Field::HasOne,
+    business: Field::HasOne,
     id: Field::Number,
+    allow_contact: Field::Boolean,
+    agree_terms: Field::Boolean,
+    provider: Field::String,
+    uid: Field::String,
+    confirmed_at: Field::DateTime,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    email: Field::String,
-    encrypted_password: Field::String,
-    reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
-    remember_created_at: Field::DateTime,
     sign_in_count: Field::Number,
     current_sign_in_at: Field::DateTime,
     last_sign_in_at: Field::DateTime,
     current_sign_in_ip: Field::String.with_options(searchable: false),
     last_sign_in_ip: Field::String.with_options(searchable: false),
-    provider: Field::String,
-    uid: Field::String,
-    first_name: Field::String,
-    last_name: Field::String,
+
   }
 
   # COLLECTION_ATTRIBUTES
@@ -50,21 +53,13 @@ class UserDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :profile,
-    :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :provider,
-    :uid,
     :first_name,
     :last_name,
+    :email,
+    :contact_number,
+    :allow_contact,
+    :agree_terms,
+    :confirmed_at,
   ]
 
   # Overwrite this method to customize how users are displayed
