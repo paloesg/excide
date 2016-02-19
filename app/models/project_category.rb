b9a5b63bbd23cabe3d942d821d90644c02f6b574
Han-Sheng Chin <hschin@gmail.com>
2016-02-19 17:32:21 +0800

    Add in all project categories

--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -1,16 +1,16 @@
 class ProjectCategory < ActiveRecord::Base
   has_many :projects
 
-  enum status: [:active, :inactive]
+  enum status: [:inactive, :active]
 
   belongs_to :parent, class_name: "ProjectCategory"
   has_many :children, class_name: "ProjectCategory", foreign_key: "parent_id"
 
   def self.parent_categories
-    ProjectCategory.active.joins(:children).uniq
+    ProjectCategory.active.joins(:children).uniq.order(:id)
   end
 
   def self.child_categories
-    ProjectCategory.active.joins(:parent)
+    ProjectCategory.active.joins(:parent).order(:id)
   end
 end
