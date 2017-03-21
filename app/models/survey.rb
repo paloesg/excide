65e3ce30361a0eb565245fb72b49823fe71c31f7
Han Sheng Chin <hschin@gmail.com>
2017-03-21 20:57:03 +0800

    Tweak relationships of survey models

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,8 @@ class Survey < ActiveRecord::Base
   belongs_to :user
   belongs_to :business
   belongs_to :template
+
+  has_many :sections
+
+  accepts_nested_attributes_for :business
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -2,5 +2,7 @@ class Template < ActiveRecord::Base
   has_many :sections
   has_many :surveys
 
+  accepts_nested_attributes_for :sections
+
   enum business_model: [:ecommerce, :marketplace, :media, :mobile, :saas, :others]
 end
