60018851542dca5831328b4ce58ad101948a5ad2
Han Sheng Chin <hschin@gmail.com>
2017-03-26 00:05:08 +0800

    Add reminders to admin

---
+++ app/controllers/admin/reminders_controller.rb
@@ -0,0 +1,19 @@
+module Admin
+  class RemindersController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # simply overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = Reminder.all.paginate(10, params[:page])
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    # def find_resource(param)
+    #   Reminder.find_by!(slug: param)
+    # end
+
+    # See https://administrate-docs.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
