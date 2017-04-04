5ad73d22a8ffe0c9e7c8a0d9b9aa572d640c0c73
Han Sheng Chin <hschin@gmail.com>
2017-04-04 18:48:54 +0800

    Create template enquiry mailer and rename general enquiry mailer

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -1,11 +1,19 @@
 class EnquiryMailer < ApplicationMailer
   default from: 'Sam from Excide <sam@excide.co>'
 
-  def general_enquiries(enquiry)
+  def general_enquiry(enquiry)
     @enquiry = enquiry
     address = Mail::Address.new @enquiry.email
     address.display_name = @enquiry.name
     @url  = 'https://calendly.com/sam-excide/'
     mail(to: address.format, subject: 'Thank you for your enquiry')
+  end
+
+  def template_enquiry(enquiry)
+    @enquiry = enquiry
+    address = Mail::Address.new @enquiry.email
+    address.display_name = @enquiry.name
+    @url  = 'http://eepurl.com/cAlEaz'
+    mail(to: address.format, subject: 'Thank you for your interest')
   end
 end
