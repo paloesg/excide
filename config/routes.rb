5d9a64bdec50f324d17ce9cb1782b74a28e076e6
Han-Sheng Chin <hschin@gmail.com>
2017-04-18 17:32:36 +0800

    Styling for corporate planning page

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ Rails.application.routes.draw do
   get 'virtual-financial-officer', to: 'home#vfo', as: :vfo
   get 'financial-analytics-reporting', to: 'home#financial-analytics-reporting', as: :financial_analytics_reporting
   get 'business-plan-assistance', to: 'home#business-plan-assistance', as: :business_plan_assistance
-  get 'corporate-planning-structure', to: 'home#corporate-planning-structure', as: :corporate_planning_structure
+  get 'corporate-planning', to: 'home#corporate-planning', as: :corporate_planning
   get 'forecasting-sensitivity-analysis', to: 'home#forecasting-sensitivity-analysis', as: :forecasting_sensitivity_analysis
   get 'budgeting-forecasting', to: 'home#budgeting-forecasting', as: :bugeting_forecasting
   get 'ipo-support', to: 'home#ipo-support', as: :ipo_support
