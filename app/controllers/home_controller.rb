a788057a09e7f81cabdeb9bdd67a3a6c3ed19fb7
Han-Sheng Chin <hschin@gmail.com>
2017-06-06 17:06:31 +0800

    Clean up home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,6 @@ class HomeController < ApplicationController
     @enquiry = Enquiry.new
   end
 
-  def business
-  end
-
-  def faq
-  end
-
-  def terms
-  end
-
-  def privacy
-  end
-
   def robots
     robots = File.read(Rails.root + "config/robots.#{ENV['HEROKU_NAME']}.txt")
     render :text => robots, :layout => false, :content_type => "text/plain"
