f5c65d6aa8b793780f623542dcf64c7369605ab3
Han-Sheng Chin <hschin@gmail.com>
2017-07-04 19:50:14 +0800

    Display outstanding tasks on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,8 @@ class DashboardsController < ApplicationController
   before_action :set_dashboard
 
   def index
+    @current_section = @workflows.first.template.sections.joins(tasks: :actions).where(actions: {completed: false}).to_ary.shift
+    @tasks = @current_section.tasks
     @documents = @company.documents.last(3).reverse
   end
 
@@ -11,5 +13,6 @@ class DashboardsController < ApplicationController
   def set_dashboard
     @user = current_user
     @company = @user.company
+    @workflows = @company.workflows
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :section
 
   has_many :actions
+
+  def company_action(company)
+    action = self.actions.where(company_id: company.id).first
+  end
 end
