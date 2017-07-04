8f2d391ecbfedf7f71565c4620da8cb61deacfe2
Han-Sheng Chin <hschin@gmail.com>
2017-07-04 20:49:39 +0800

    Add callback to create related actions upon creation of workflow

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -2,4 +2,18 @@ class Workflow < ActiveRecord::Base
   belongs_to :user
   belongs_to :company
   belongs_to :template
+
+  after_create :create_related_actions
+
+  private
+
+  # Create all the actions that need to be completed for a workflow that is associated with a company
+  def create_related_actions
+    sections = self.template.sections
+    sections.each do |s|
+      s.tasks.each do |t|
+        Action.create(task: t, company: self.company, completed: false)
+      end
+    end
+  end
 end
