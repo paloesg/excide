a403fa21f7283dd712eabc194f1741a17d1ac43f
Han-Sheng Chin <hschin@gmail.com>
2017-07-14 16:35:15 +0800

    Admins go to admin page upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    if current_user.lead?
-      new_account_path
-    elsif current_user.has_role? :admin
+    if current_user.has_role? :admin
       admin_root_path
+    elsif current_user.lead?
+      new_account_path
     elsif current_user.has_role? :consultant
       profile_path
     elsif current_user.has_role? :company
