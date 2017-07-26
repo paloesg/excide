1bef63833039c301e345e4de8255342587531bd5
Han-Sheng Chin <hschin@gmail.com>
2017-07-26 16:23:35 +0800

    Add callback to update deadline and reminders when action is completed

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,25 @@
 class Action < ActiveRecord::Base
+  after_save :set_deadline_and_reminders, if: :completed_changed?
+
   belongs_to :task
   belongs_to :company
+
+  has_one :reminder
+
+  private
+
+  def set_deadline_and_reminders
+    if self.completed
+      # Find associated reminders and remove next reminder date
+      self.reminder.update_attributes(next_reminder: nil) if self.reminder.present?
+
+      # Find next action in line and set deadline
+      next_task = self.task.lower_item
+      next_action = next_task.action
+      next_action.update_attributes(deadline: Date.today + next_task.days_to_complete)
+
+      # Create new reminder based on deadline of action and repeat every 2 days
+      Reminder.create(next_reminder: next_action.deadline, repeat: true, freq_value: 2, freq_unit: "days", company_id: self.company_id, task_id: next_task.id, action_id: self.id)
+    end
+  end
 end
