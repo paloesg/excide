fd4bc8cf649321cf889b275759b51c44f9fc02ff
Han-Sheng Chin <hschin@gmail.com>
2017-08-11 15:50:47 +0800

    Some code cleanup

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -30,28 +30,20 @@ class DocumentsController < ApplicationController
     @document = Document.new(document_params)
     @document.company = @company
 
-    respond_to do |format|
-      if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render :show, status: :created, location: @document }
-      else
-        format.html { render :new }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    if @document.save
+      redirect_to @document, notice: 'Document was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /documents/1
   # PATCH/PUT /documents/1.json
   def update
-    respond_to do |format|
-      if @document.update(document_params)
-        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
-        format.json { render :show, status: :ok, location: @document }
-      else
-        format.html { render :edit }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    if @document.update(document_params)
+      redirect_to @document, notice: 'Document was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -60,8 +52,7 @@ class DocumentsController < ApplicationController
   def destroy
     @document.destroy
     respond_to do |format|
-      format.html { redirect_to documents_url, notice: 'Document was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to documents_url, notice: 'Document was successfully destroyed.'
     end
   end
 
