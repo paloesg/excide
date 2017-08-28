e46a0274464917ca688827b4ee98ef402af9d412
Han-Sheng Chin <hschin@gmail.com>
2017-08-28 16:52:39 +0800

    Update sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -36,7 +36,16 @@ SitemapGenerator::Sitemap.create do
   #   Article.find_each do |article|
   #     add article_path(article), :lastmod => article.updated_at
   #   end
-  add faq_path
-
-  add projects_path, :priority => 0.9, :changefreq => 'daily'
+  add about_path
+  add vfo_path
+  add financial_analytics_reporting_path
+  add business_plan_assistance_path
+  add corporate_planning_path
+  add forecasting_sensitivity_analysis_path
+  add bugeting_forecasting_path
+  add ipo_support_path
+  add mergers_acquisitions_support_path
+  add exit_strategy_path
+  add turnaround_management_path
+  add fund_raising_path
 end
