d07a4a1192dfac0bcbb4c068a192b4cabe3e6e0b
Han-Sheng Chin <hschin@gmail.com>
2017-10-06 17:43:26 +0800

    Update theme and layout for dashboard

---
+++ app/assets/javascripts/dashboard/application.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require moment
+//= require datetime_picker
+
+//= require trix
+//= require cocoon
+//= require analytics
+//= require init
+//= require jquery.ui.widget
+//= require z.jquery.fileupload
+
+//= require turbolinks
