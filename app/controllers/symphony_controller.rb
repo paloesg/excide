1fdef0f340a7fd2cce4f6293cbe5b48f52ada4d5
Han-Sheng Chin <hschin@gmail.com>
2017-10-18 23:45:49 +0800

    Only show tasks that belong to user role

--- app/controllers/symphony_controller.rb
+++ app/controllers/symphony_controller.rb
@@ -5,11 +5,12 @@ class SymphonyController < ApplicationController
   before_action :set_workflow, only: [:show]
 
   def show
-    if @workflows.present?
+    if @templates.present?
+      @roles = @user.roles.where(resource_id: @company.id, resource_type: "Company")
       @tasks = []
       @workflows.each do |w|
         w.template.sections.each do |s|
-          @tasks += s.tasks.joins(:company_actions).where(company_actions: {company: @company, completed: false})
+          @tasks += s.tasks.joins(:company_actions).where(role_id: @roles.map(&:id)).where(company_actions: {company: @company, completed: false})
         end
       end
     else
