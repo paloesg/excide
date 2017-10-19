92a993ac62698ed34ccbf5436fe58405f04e42d2
Han-Sheng Chin <hschin@gmail.com>
2017-10-19 18:31:36 +0800

    Add new notification mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,19 @@
+class NotificationMailer < ApplicationMailer
+  default from: 'Excide Symphony <admin@excide.co>'
+
+  def self.deliver_notifications(task, action, users)
+    users.each do |user|
+      task_notification(task, action, user).deliver_now
+    end
+  end
+
+  def task_notification(task, action, user)
+    @task = task
+    @action = action
+    @user = user
+    address = Mail::Address.new @user.email
+    address.display_name = @user.first_name + @user.last_name
+    @url  = 'https://calendly.com/sam-excide/'
+    mail(to: address.format, subject: 'You have a new task to complete')
+  end
+end
