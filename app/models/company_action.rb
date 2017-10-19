f1dc8f88e43d9471ebde011c344108bd0b9a02c8
Han-Sheng Chin <hschin@gmail.com>
2017-10-19 18:32:00 +0800

    Trigger notification mailer for next task when task completed

--- app/models/company_action.rb
+++ app/models/company_action.rb
@@ -4,6 +4,7 @@ class CompanyAction < ActiveRecord::Base
 
   after_save :set_deadline_and_reminders
   after_save :send_notification, if: :completed_changed?
+  after_save :trigger_next_task, if: :completed_changed?
 
   belongs_to :task
   belongs_to :company
@@ -16,20 +17,26 @@ class CompanyAction < ActiveRecord::Base
     if self.completed
       # Find associated reminders and remove next reminder date
       self.reminder.update_attributes(next_reminder: nil) if self.reminder.present?
-
-      unless self.task.last?
-        # Find next action in line and set deadline if not the last task in section
-        next_task = self.task.lower_item
-        next_action = next_task.get_company_action(self.company)
-        next_action.update_columns(deadline: (Date.today + next_task.days_to_complete)) unless next_task.days_to_complete.nil?
-
-        # Create new reminder based on deadline of action and repeat every 2 days
-        create_reminder(next_task, next_action)
-      end
     elsif self.task.first? && self.reminder.nil?
       # If task is first in the list, create own reminder
       self.update_columns(deadline: (Date.today + self.task.days_to_complete)) unless self.task.days_to_complete.nil?
       create_reminder(self.task, self)
+    end
+  end
+
+  def trigger_next_task
+    unless self.task.last? or !self.completed
+      # Find next action in line and set deadline if not the last task in section
+      next_task = self.task.lower_item
+      next_action = next_task.get_company_action(self.company)
+      next_action.update_columns(deadline: (Date.today + next_task.days_to_complete)) unless next_task.days_to_complete.nil?
+
+      # Create new reminder based on deadline of action and repeat every 2 days
+      create_reminder(next_task, next_action)
+
+      # Trigger email notification for next task
+      users = User.with_role(next_task.role.name.to_sym, self.company)
+      NotificationMailer.deliver_notifications(next_task, next_action, users)
     end
   end
 
