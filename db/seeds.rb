878d873b41a00d5cab5865fce443096b897e0636
Han-Sheng Chin <hschin@gmail.com>
2017-10-20 11:20:28 +0800

    Test seed data for user accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,19 @@ industry = ProjectCategory.create({ name: 'Industry Knowledge Network', status: 
 ProjectCategory.create([{ name: 'Sub-contract Agreements', status: 'active', parent: industry }, { name: 'Hourly Consultations', status: 'active', parent: industry }, { name: 'Industry Reports', status: 'active', parent: industry }])
 others = ProjectCategory.create({ name: 'Others', status: 'active' })
 ProjectCategory.create({ name: 'Others', status: 'active', parent: others })
+
+company = Company.create(name: 'Gobbler')
+admin = User.create(email: 'admin@gobbler.com', password: 'password', first_name: 'Admin', last_name: 'Gobbler', contact_number: '12341234', company: company)
+admin.add_role :admin, company
+sales = User.create(email: 'sales@gobbler.com', password: 'password', first_name: 'Sales', last_name: 'Gobbler', contact_number: '12341234', company: company)
+sales.add_role :sales, company
+sales_support = User.create(email: 'sales_support@gobbler.com', password: 'password', first_name: 'Sales Support', last_name: 'Gobbler', contact_number: '12341234', company: company)
+sales_support.add_role :sales_support, company
+procurement = User.create(email: 'procurement@gobbler.com', password: 'password', first_name: 'Procurement', last_name: 'Gobbler', contact_number: '12341234', company: company)
+procurement.add_role :procurement, company
+finance = User.create(email: 'finance@gobbler.com', password: 'password', first_name: 'Finance', last_name: 'Gobbler', contact_number: '12341234', company: company)
+finance.add_role :finance, company
+management = User.create(email: 'management@gobbler.com', password: 'password', first_name: 'Management', last_name: 'Gobbler', contact_number: '12341234', company: company)
+management.add_role :management, company
+vp_sales = User.create(email: 'vp_sales@gobbler.com', password: 'password', first_name: 'VP Sales', last_name: 'Gobbler', contact_number: '12341234', company: company)
+vp_sales.add_role :vp_sales, company
