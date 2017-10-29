4f09194a50426051e0a7c8a75a4556f614a57fe0
Han-Sheng Chin <hschin@gmail.com>
2017-10-29 23:29:26 +0800

    Clean up unused code in home controller

--- app/controllers/symphony/home_controller.rb
+++ app/controllers/symphony/home_controller.rb
@@ -5,18 +5,6 @@ class Symphony::HomeController < ApplicationController
   before_action :set_workflow, only: [:show]
 
   def show
-    if @templates.present?
-      @roles = @user.roles.where(resource_id: @company.id, resource_type: "Company")
-      @tasks = []
-      @workflows.each do |w|
-        w.template.sections.each do |s|
-          @tasks += s.tasks.joins(:company_actions).where(role_id: @roles.map(&:id)).where(company_actions: {company: @company, completed: false})
-        end
-      end
-    else
-      @current_section = nil
-      @tasks = nil
-    end
   end
 
   private
