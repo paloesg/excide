4089e6b3744255c55b5ea52189379d27efd72aa8
Han-Sheng Chin <hschin@gmail.com>
2017-11-07 17:57:08 +0800

    Create actions and views for adding new users

--- app/controllers/symphony/users_controller.rb
+++ app/controllers/symphony/users_controller.rb
@@ -8,10 +8,31 @@ class Symphony::UsersController < ApplicationController
     @users = User.where(company: @company)
   end
 
+  def show
+    @user = User.find_by(id: params[:id], company: @company)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.company = @company
+    if @user.save
+      redirect_to symphony_users_path, notice: 'User successfully created!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_company
-    @admin_user = current_user
-    @company = @admin_user.company
+    @company = current_user.company
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :email, :contact_number, :company_id)
   end
 end
