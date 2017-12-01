3673c49ca67a90bb5c6966d0684edb7a96f05ee6
Han-Sheng Chin <hschin@gmail.com>
2017-12-01 23:24:23 +0800

    Set up cascading deletes for templates, sections, and workflows

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,8 +2,8 @@ class Template < ActiveRecord::Base
   include FriendlyId
   friendly_id :title, use: [:slugged, :finders]
 
-  has_many :sections, -> { order(position: :asc) }
-  has_many :workflows
+  has_many :sections, -> { order(position: :asc) }, dependent: :destroy
+  has_many :workflows, dependent: :destroy
 
   belongs_to :company
 
