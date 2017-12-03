6566526c00bb3a6e6150d5b5c5a827ed8ae5cd6d
Han-Sheng Chin <hschin@gmail.com>
2017-12-04 00:16:16 +0800

    Refactor document specs to use shoulda matchers

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,19 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  subject { described_class.new(identifier: 'IDENTIFIER', file_url: 'https://excide.co/') }
+  it { should validate_presence_of(:identifier) }
+  it { should validate_presence_of(:file_url) }
 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
-  end
-
-  it "is not valid without an identifier" do
-    subject.identifier = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a file_url" do
-    subject.file_url = nil
-    expect(subject).to_not be_valid
-  end
+  it { should belong_to(:company) }
+  it { should belong_to(:workflow) }
+  it { should belong_to(:document_template) }
 end
