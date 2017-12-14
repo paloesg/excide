2d13463590742127a07252967fb6223be4726782
Han-Sheng Chin <hschin@gmail.com>
2017-12-14 22:02:08 +0800

    Add specs for survey section

---
+++ spec/models/survey_section_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe SurveySection, type: :model do
+  it { should validate_presence_of(:unique_name) }
+  it { should validate_presence_of(:display_name) }
+  it { should validate_presence_of(:position) }
+  it { should validate_presence_of(:survey_template) }
+
+  it { should belong_to(:survey_template) }
+  it { should have_many(:questions) }
+  it { should have_many(:segments) }
+end
