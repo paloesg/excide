03339d7a4403ca5ff615b26a30f3a6ed61e6f678
Han-Sheng Chin <hschin@gmail.com>
2017-12-14 22:01:51 +0800

    Add validations for survey sections

--- app/models/survey_section.rb
+++ app/models/survey_section.rb
@@ -6,4 +6,6 @@ class SurveySection < ActiveRecord::Base
   has_many :segments
 
   accepts_nested_attributes_for :questions
+
+  validates :unique_name, :display_name, :position, :survey_template, presence: true
 end
