1f10694abc3eee52fae0c670132049d33f23b7bc
Han-Sheng Chin <hschin@gmail.com>
2018-01-18 14:24:07 +0800

    Hide unneeded client identifier

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Client, type: :model do
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:identifier) }
 
   it { should belong_to(:company) }
   it { should belong_to(:user) }
