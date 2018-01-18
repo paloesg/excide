4d88e34619365fbcae4ea25deed45d59ceb11ca8
Han-Sheng Chin <hschin@gmail.com>
2018-01-18 16:06:29 +0800

    Remove template validation

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Section, type: :model do
   it { should validate_presence_of(:unique_name) }
   it { should validate_presence_of(:display_name) }
   it { should validate_presence_of(:position) }
-  it { should validate_presence_of(:template) }
 
   it { should belong_to(:template) }
   it { should have_many(:tasks) }
