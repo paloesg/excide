13c063488c3dae1724d1b80dbcacef437106bd25
Han-Sheng Chin <hschin@gmail.com>
2018-01-18 15:04:42 +0800

    Allow editing of remarks column of workflow

--- app/controllers/symphony/workflows_controller.rb
+++ app/controllers/symphony/workflows_controller.rb
@@ -82,7 +82,7 @@ class Symphony::WorkflowsController < WorkflowsController
   end
 
   def workflow_params
-    params.require(:workflow).permit(:user_id, :company_id, :template_id, :completed, :deadline, :identifier, :workflowable_id, :workflowable_type, :workflowable)
+    params.require(:workflow).permit(:user_id, :company_id, :template_id, :completed, :deadline, :identifier, :workflowable_id, :workflowable_type, :workflowable, :remarks)
   end
 
   def set_documents
