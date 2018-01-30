bd6a24e7debfcb4b62837aa508fddf8db34ed02c
Han-Sheng Chin <hschin@gmail.com>
2018-01-30 19:07:00 +0800

    Show upcoming activations in conductor home

--- app/controllers/conductor/home_controller.rb
+++ app/controllers/conductor/home_controller.rb
@@ -2,10 +2,20 @@ class Conductor::HomeController < ApplicationController
   layout 'dashboard/application'
 
   before_action :authenticate_user!
+  before_action :set_company_and_roles
 
   def show
+    date_from = params[:start_date] || Date.today
+    date_to = date_from + 1.month
+    @activations = Activation.where(company: @company, start_time: date_from.beginning_of_day..date_to.end_of_day)
   end
 
   private
+
+  def set_company_and_roles
+    @user = current_user
+    @company = @user.company
+    @roles = @user.roles.where(resource_id: @company.id, resource_type: "Company")
+  end
 
 end
