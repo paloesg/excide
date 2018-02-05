c2aaa5ffc4429d020317aab8359973e3ea1ebb36
Vicky Prahastra <vicky@excide.co>
2018-02-05 11:41:52 +0700

    Only company admin can edit company details

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,6 @@ ActiveRecord::Schema.define(version: 20180201070507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activations", force: :cascade do |t|
-    t.integer  "activation_type"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.text     "remarks"
-    t.string   "location"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "company_id"
-    t.integer  "user_id"
-  end
-
-  add_index "activations", ["company_id"], name: "index_activations_on_company_id", using: :btree
-  add_index "activations", ["user_id"], name: "index_activations_on_user_id", using: :btree
-
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
     t.string   "trackable_type"
@@ -460,8 +445,6 @@ ActiveRecord::Schema.define(version: 20180201070507) do
   add_index "workflows", ["user_id"], name: "index_workflows_on_user_id", using: :btree
   add_index "workflows", ["workflowable_type", "workflowable_id"], name: "index_workflows_on_workflowable_type_and_workflowable_id", using: :btree
 
-  add_foreign_key "activations", "companies"
-  add_foreign_key "activations", "users"
   add_foreign_key "clients", "companies"
   add_foreign_key "clients", "users"
   add_foreign_key "company_actions", "companies"
