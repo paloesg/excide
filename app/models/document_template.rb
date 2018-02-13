3fceb473fcb776c12391bd861e45ca5fa2144969
Han-Sheng Chin <hschin@gmail.com>
2018-02-13 15:55:15 +0800

    Remove associations when deleting document template

--- app/models/document_template.rb
+++ app/models/document_template.rb
@@ -5,5 +5,14 @@ class DocumentTemplate < ActiveRecord::Base
   has_many :documents
   has_many :tasks
 
+  before_destroy :remove_associations
+
   validates :title, :file_url, :template, presence: true
+
+  private
+
+  def remove_associations
+    self.tasks.update_all(document_template_id: nil)
+    self.documents.update_all(document_template_id: nil)
+  end
 end
