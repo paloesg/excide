523c442be97f2d30553df6e50bed721b2f82643a
Vicky Prahastra <vicky@excide.co>
2018-02-21 14:31:27 +0700

    Add new attribute to workflow attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,13 @@ module ApplicationHelper
       end
     end
   end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.build_data.last()
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render association.to_s + "_fields", f: builder
+    end
+    link_to(name, '#', class: "add_fields btn btn-sm btn-success", data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end
