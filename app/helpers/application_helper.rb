6092f5f2a485eae60d79dfbb6671db099824a67a
Han-Sheng Chin <hschin@gmail.com>
2018-02-22 15:19:56 +0800

    Update button styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,6 +34,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render association.to_s + "_fields", f: builder
     end
-    link_to(name, '#', class: "add_fields btn btn-sm btn-success", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(name, '#', class: "add_fields btn btn-success", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
