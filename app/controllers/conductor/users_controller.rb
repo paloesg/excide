7a6c489f35c42492a2101879afe613f82456da10
Han-Sheng Chin <hschin@gmail.com>
2018-02-28 15:52:39 +0800

    Add users controller and view for conductor

---
+++ app/controllers/conductor/users_controller.rb
@@ -0,0 +1,64 @@
+class Conductor::UsersController < ApplicationController
+  layout 'dashboard/application'
+
+  before_action :authenticate_user!
+  before_action :set_company
+  before_action :set_company_roles, only: [:new, :create, :edit]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.where(company: @company).order(:id)
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.company = @company
+    if @user.save
+      redirect_to symphony_users_path, notice: 'User successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to symphony_users_path, notice: 'User successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to symphony_users_path, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  def set_company
+    @company = current_user.company
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id], company: @company)
+  end
+
+  def set_company_roles
+    @company_roles = Role.where(resource_id: @company.id, resource_type: "Company")
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :email, :contact_number, :company_id, :role_ids => [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
   end
 
   namespace :conductor do
+    resources :users
     resources :activations
     resources :allocations
     resources :availabilities
