bb454cd07be94d583bf025f0c6e3d33eaea41508
Han-Sheng Chin <hschin@gmail.com>
2018-03-26 16:06:56 +0800

    Update some factories

---
+++ spec/factories/clients.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :client do
+    name Faker::Company.name
+  end
+end
--- spec/factories/workflows.rb
+++ spec/factories/workflows.rb
@@ -1,17 +1,16 @@
 FactoryBot.define do
   factory :workflow do |workflow|
+    association :workflowable, factory: :client
+
     workflow.identifier { Faker::Name.title}
-    workflow.workflowable_type "Client"
+
+    company
 
     after(:create) do |workflow|
       create_list(:section, 3, template: workflow.template)
     end
 
     factory :workflow_with_document do
-      transient do
-        workflow.company
-      end
-
       after(:create) do |w|
         create(:document_template, template: w.template) do |d|
           create(:document, workflow_id: w.id, company: w.company, document_template: d)
