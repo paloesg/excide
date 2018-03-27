2d3d0f1c6681be4c2ee95cc9aff408023db0e562
Han-Sheng Chin <hschin@gmail.com>
2018-03-28 00:12:03 +0800

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,19 @@
 FactoryBot.define do
-  sequence :email do |n|
-    "person#{n}@example.com"
-  end
-end
-
-FactoryBot.define do
-  factory :user, :class => 'User' do
-    email
+  factory :user do
+    sequence(:email) { |n| "person#{n}@example.com" }
     password '12345678'
     password_confirmation '12345678'
+    contact_number Faker::PhoneNumber.phone_number
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
     company
-  end
 
+    factory :admin_user do
+      after(:create) { |user| user.add_role :admin }
+    end
+
+    factory :company_admin do
+      after(:create) { |user| user.add_role :admin, company }
+    end
+  end
 end
