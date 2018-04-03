1102d58f9e28232b342ee852b626b9aad46c762b
Vicky Prahastra <vicky@excide.co>
2018-04-03 13:51:10 +0700

    Index archives for completed workflows

---
+++ app/controllers/symphony/archives_controller.rb
@@ -0,0 +1,33 @@
+class Symphony::ArchivesController < ApplicationController
+  layout 'dashboard/application'
+
+  before_action :authenticate_user!
+  before_action :set_workflow, only: [:index]
+
+  def index
+  end
+
+  private
+
+  def set_workflow
+    @user = current_user
+    @company = @user.company
+    @templates = view_context.get_relevant_templates
+    @workflows_array = @templates.map(&:workflows).flatten
+    @workflows_sort = sort_column(@workflows_array)
+    params[:direction] == "desc" ? @workflows_sort.reverse! : @workflows_sort
+    @workflows = Kaminari.paginate_array(@workflows_sort).page(params[:page]).per(10)
+  end
+
+  def sort_column(array)
+    array.sort_by{
+      |item| if params[:sort] == "template" then item.template.title.upcase
+      elsif params[:sort] == "remarks" then item.remarks ? item.remarks.upcase : ""
+      elsif params[:sort] == "deadline" then item.deadline ? item.deadline : Time.at(0)
+      elsif params[:sort] == "workflowable" then item.workflowable ? item.workflowable&.name.upcase : ""
+      elsif params[:sort] == "completed" then item.completed ? 'Completed' : item.current_section&.display_name
+      elsif params[:sort] == "identifier" then item.identifier ? item.identifier.upcase : ""
+      end
+    }
+  end
+end
