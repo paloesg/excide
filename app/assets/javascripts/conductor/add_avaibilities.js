bc3673545ba9b99e87a6a2c86ccefb90d5e586b0
Han-Sheng Chin <hschin@gmail.com>
2018-04-17 00:12:51 +0800

    Change all quotes to single quotes for consistency

--- app/assets/javascripts/conductor/add_avaibilities.js
+++ app/assets/javascripts/conductor/add_avaibilities.js
@@ -1,12 +1,12 @@
 function allDay(element) {
-  $(element).closest('td').find("input").not(element).prop("checked", element.checked)
+  $(element).closest('td').find('input').not(element).prop('checked', element.checked)
 }
 
 function day(element) {
-  $(element).closest('td').find("#all_day").prop("checked", false)
-  all_input = $(element).closest('td').find("input:checkbox").slice(1)
-  all_checked = $(element).closest('td').find("input:checkbox:checked")
+  $(element).closest('td').find('#all_day').prop('checked', false)
+  all_input = $(element).closest('td').find('input:checkbox').slice(1)
+  all_checked = $(element).closest('td').find('input:checkbox:checked')
   if (all_checked.length == all_input.length) {
-    $(element).closest('td').find("#all_day").prop("checked", true)
+    $(element).closest('td').find('#all_day').prop('checked', true)
   }
 }
