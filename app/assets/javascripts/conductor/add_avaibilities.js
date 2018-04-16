8581df832204a9fcc6292c26148ceb0bfae85e77
Vicky Prahastra <vicky@excide.co>
2018-04-16 18:21:10 +0700

    Change selector javascript code

--- app/assets/javascripts/conductor/add_avaibilities.js
+++ app/assets/javascripts/conductor/add_avaibilities.js
@@ -1,12 +1,12 @@
 function allDay(element) {
-  $(element).parent().parent().find("input").not(element).prop("checked", element.checked)
+  $(element).closest('td').find("input").not(element).prop("checked", element.checked)
 }
 
 function day(element) {
-  $(element).parent().parent().find("input")[1].checked = false
-  all_input = $(element).parent().parent().find("input:checkbox").slice(1)
-  all_checked = $(element).parent().parent().find("input:checkbox:checked")
+  $(element).closest('td').find("#all_day").prop("checked", false)
+  all_input = $(element).closest('td').find("input:checkbox").slice(1)
+  all_checked = $(element).closest('td').find("input:checkbox:checked")
   if (all_checked.length == all_input.length) {
-    $(element).parent().parent().find("input")[1].checked = true
+    $(element).closest('td').find("#all_day").prop("checked", true)
   }
 }
