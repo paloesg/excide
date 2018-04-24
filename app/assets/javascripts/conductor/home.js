3702cd85529b4cbcfd3312e287cbe445e69599bc
Vicky Prahastra <vicky@excide.co>
2018-04-24 15:55:28 +0700

    Move javascript to js files

---
+++ app/assets/javascripts/conductor/home.js
@@ -0,0 +1,19 @@
+$(document).ready( function () {
+  $('.day').popover({
+    html: true,
+    title: 'New activation',
+    container: 'body',
+    placement:'auto left',
+    content: function() {
+      return $('#popover-show').html();
+    }
+  }).on('shown.bs.popover', function () {
+    $('.datetimepicker').datetimepicker({
+      format: "YYYY-MM-DD HH:mm",
+    });
+    date = new Date();
+    $('.datetimepicker').val($(this).attr('td-date') + ' ' + moment(date).format('HH:mm'));
+  }).on("show.bs.popover", function() {
+    $(this).data("bs.popover").tip().css("max-width", "none");
+  });
+});
