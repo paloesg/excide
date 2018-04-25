53a18af86f17664d2e910bdb76c7f0f05ee6d784
Vicky Prahastra <vicky@excide.co>
2018-04-25 14:38:04 +0700

    Should open single popover new activation

--- app/assets/javascripts/conductor/home.js
+++ app/assets/javascripts/conductor/home.js
@@ -3,8 +3,8 @@ $(document).ready( function () {
     html: true,
     title: 'New activation',
     container: 'body',
-    placement:'auto left',
-    content: function() {
+    placement: 'auto left',
+    content: function () {
       return $('#popover-show').html();
     }
   }).on('shown.bs.popover', function () {
@@ -13,7 +13,8 @@ $(document).ready( function () {
     });
     date = new Date();
     $('.datetimepicker').val($(this).attr('td-date') + ' ' + moment(date).format('HH:mm'));
-  }).on("show.bs.popover", function() {
+  }).on("show.bs.popover", function () {
     $(this).data("bs.popover").tip().css("max-width", "none");
+    $('.popover').popover('hide');
   });
 });
