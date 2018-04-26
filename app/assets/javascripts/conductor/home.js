d1ba4d89791a22f40d88d4156b97da36dad5ef03
Vicky Prahastra <vicky@excide.co>
2018-04-26 18:58:16 +0700

    Show activation details different popover

--- app/assets/javascripts/conductor/home.js
+++ app/assets/javascripts/conductor/home.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       container: 'body',
       placement: 'auto left',
       content: function () {
-        return $('#popover-show').html();
+        return $('#new-activation').html();
       }
     }).on("show.bs.popover", function () {
       $('.popover').popover('hide');
@@ -23,7 +23,7 @@ $(document).ready(function () {
       container: 'body',
       placement: 'auto left',
       content: function () {
-        return $('#popover-show').html();
+        return $('#activation-details').html();
       }
     }).on('shown.bs.popover', function (el) {
       element_target = $(el.target).data("bs.popover").tip();
@@ -32,30 +32,29 @@ $(document).ready(function () {
         stepping: 15,
         sideBySide: true
       });
+    }).on("show.bs.popover", function (el) {
+      $('.popover').popover('hide');
       $.ajax({
         type: "GET",
         url: '//' + location.host + location.pathname + "/activations/" + $(this).attr('activation-id'),
         dataType: "json",
         success: function (get_activation) {
-          element_target.find('input, select, textarea').prop('disabled', true);
-          element_target.find('#activation_client_id').val(get_activation['activation']['client_id']);
-          element_target.find('#activation_activation_type').val(get_activation['activation']['activation_type']);
-          element_target.find('#activation_event_owner_id').val(get_activation['activation']['event_owner_id']);
-          element_target.find('#activation_location').val(get_activation['activation']['location']);
+          element_target.find('#activation_client_id').text(get_activation['activation']['client_id']);
+          element_target.find('#activation_activation_type').text(get_activation['activation']['activation_type']);
+          element_target.find('#activation_event_owner_id').text(get_activation['activation']['event_owner_id']);
+          element_target.find('#activation_location').text(get_activation['activation']['location']);
           if (get_activation['address'] != null) {
-            element_target.find('#activation_address_attributes_line_1').val(get_activation['address']['line_1']);
-            element_target.find('#activation_address_attributes_line_2').val(get_activation['address']['line_2']);
-            element_target.find('#activation_address_attributes_postal_code').val(get_activation['address']['postal_code']);
+            element_target.find('#activation_address_attributes_line_1').text(get_activation['address']['line_1']);
+            element_target.find('#activation_address_attributes_line_2').text(get_activation['address']['line_2']);
+            element_target.find('#activation_address_attributes_postal_code').text(get_activation['address']['postal_code']);
           }
-          element_target.find('#activation_start_time').val(get_activation['activation']['start_time']);
-          element_target.find('#activation_end_time').val(get_activation['activation']['end_time']);
-          element_target.find('#activation_remarks').val(get_activation['activation']['remarks']);
+          element_target.find('#activation_start_time').text(new Date(get_activation['activation']['start_time']));
+          element_target.find('#activation_end_time').text(new Date(get_activation['activation']['end_time']));
+          element_target.find('#activation_remarks').text(get_activation['activation']['remarks']);
           element_target.find('.btn').hide();
-          element_target.append('<a href="conductor/activations/' + get_activation['activation']['id'] + '/edit" class="btn btn-default" role="button">Edit activation</a>');
+          element_target.find('.popover-content').append('<p><a href="conductor/activations/' + get_activation['activation']['id'] + '/edit" class="btn btn-default" role="button">Edit activation</a></p>');
         }
       })
-    }).on("show.bs.popover", function () {
-      $('.popover').popover('hide');
     });
   })
 });
