71307a136112138cd6ebae6302871588db68f8f7
Han-Sheng Chin <hschin@gmail.com>
2018-05-10 15:24:49 +0800

    Add error message if user cannot be assigned

--- app/controllers/conductor/allocations_controller.rb
+++ app/controllers/conductor/allocations_controller.rb
@@ -67,20 +67,21 @@ class Conductor::AllocationsController < ApplicationController
   def update
     if allocation_params[:user_id].present?
       # If user id present, user is being assigned
-      @avaibility = User.find(allocation_params[:user_id]).get_availability(@allocation)
+      @availability = User.find(allocation_params[:user_id]).get_availability(@allocation)
     else
       # If user id not present, user is being unassigned
-      @avaibility = @allocation.user.get_availability(@allocation) if @allocation.user
+      @availability = @allocation.user.get_availability(@allocation) if @allocation.user
     end
 
     respond_to do |format|
-      if @avaibility.toggle!(:assigned) and @allocation.update(allocation_params)
+      if @availability and @availability.toggle!(:assigned) and @allocation.update(allocation_params)
         format.html { redirect_to conductor_allocations_path, notice: 'Allocation was successfully updated.' }
         format.json { render :show, status: :ok, location: @allocation }
         format.js   { render js: 'Turbolinks.visit(location.toString());' }
       else
         set_contractor
         set_activations
+        @allocation.errors.add(:user, "not availabile for assignment") if @availability.blank?
         format.html { render :edit }
         format.json { render json: @allocation.errors, status: :unprocessable_entity }
       end
