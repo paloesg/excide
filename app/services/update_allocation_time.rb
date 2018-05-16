bd1d85698223d58da55fe618f8abaeaf8aef441e
Vicky Prahastra <vicky@excide.co>
2018-05-16 11:25:28 +0700

    Should check user availabilities with date

--- app/services/update_allocation_time.rb
+++ app/services/update_allocation_time.rb
@@ -13,7 +13,7 @@ class UpdateAllocationTime
   private
 
   def inform_user
-    if @allocation.user.availabilities.where("start_time <= ?", @new_start_time).where("end_time >= ?", @new_end_time).present?
+    if @allocation.user.availabilities.where(available_date: @allocation.allocation_date).where("start_time <= ?", @new_start_time).where("end_time >= ?", @new_end_time).present?
       NotificationMailer.edit_activation(@activation, @allocation.user).deliver
     else
       NotificationMailer.user_removed_from_activation(@activation, @allocation.user).deliver
