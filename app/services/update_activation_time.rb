af29fdef1e24734bf3515823d210851d488bcc81
Han-Sheng Chin <hschin@gmail.com>
2018-05-25 15:05:26 +0800

    Simplify service object logic

--- app/services/update_activation_time.rb
+++ app/services/update_activation_time.rb
@@ -9,13 +9,21 @@ class UpdateActivationTime
   def run
     begin
       @activation.transaction do
+        @contractors_status = { "update_time" => [], "unassigned" => [] }
         update_activation
-        update_allocations
+        @activation.allocations.each do |allocation|
+          update_allocation(allocation)
+          next if allocation.user.blank?
+          if contractor_available?(allocation)
+            notify_contractor(allocation)
+          else
+            remove_contractor(allocation)
+          end
+        end
       end
-      send_email_to_contractor
-      return {success: true, contractors_status: contractors_status}
+      return {success: true, contractors_status: @contractors_status}
     rescue ActiveRecord::RecordInvalid
-      return {success: false, errors: 'Error update activation time.'}
+      return {success: false, errors: 'Error updating activation time.'}
     end
   end
 
@@ -25,39 +33,24 @@ class UpdateActivationTime
     @activation.update_attributes!(start_time: @new_start_time, end_time: @new_end_time)
   end
 
-  def update_allocations
-    @activation.allocations.each do |allocation|
-      allocation.update_attributes!(allocation_date: @new_start_time, start_time: @new_start_time, end_time: @new_end_time)
-    end
+  def update_allocation(allocation)
+    allocation.update_attributes!(allocation_date: @new_start_time, start_time: @new_start_time, end_time: @new_end_time)
   end
 
-  def check_contractor_availability(allocation)
+  def contractor_available?(allocation)
     allocation.user.availabilities.where(available_date: allocation.allocation_date).where("start_time <= ?", @new_start_time).where("end_time >= ?", @new_end_time).present?
   end
 
-  def send_email_to_contractor
-    @activation.allocations.each do |allocation|
-      next if allocation.user.blank?
-      if check_contractor_availability(allocation)
-        NotificationMailer.edit_activation(@activation, allocation.user).deliver
-      else
-        NotificationMailer.user_removed_from_activation(@activation, allocation.user).deliver
-      end
-    end
+  def notify_contractor(allocation)
+    NotificationMailer.edit_activation(@activation, allocation.user).deliver
+    @contractors_status['update_time'] << allocation.user
   end
 
-  def contractors_status
-    @contractors = { "update_time" => [], "unassigned" => [] }
-    @activation.allocations.each do |allocation|
-      next if allocation.user.blank?
-      if check_contractor_availability(allocation)
-        @contractors['update_time'] << allocation.user
-      else
-        @contractors['unassigned'] << allocation.user
-        allocation.update_attribute(:user_id, nil)
-      end
-    end
-    @contractors
+  def remove_contractor(allocation)
+    removed_user = allocation.user
+    allocation.update_attributes!(user_id: nil)
+    removed_user.get_availability(allocation).toggle!(:assigned)
+    NotificationMailer.user_removed_from_activation(@activation, removed_user).deliver
+    @contractors_status['unassigned'] << removed_user
   end
-
 end
