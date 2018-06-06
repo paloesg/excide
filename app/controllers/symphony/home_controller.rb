b2304ffcd32441bd415e5f34253476e1adc4fee4
Han-Sheng Chin <hschin@gmail.com>
2018-06-06 15:02:31 +0800

    Generate secured API key to restrict search by company

--- app/controllers/symphony/home_controller.rb
+++ app/controllers/symphony/home_controller.rb
@@ -16,6 +16,7 @@ class Symphony::HomeController < ApplicationController
   end
 
   def search
+    @public_key = Algolia.generate_secured_api_key(ENV['ALGOLIASEARCH_API_KEY_SEARCH'], {filters: 'company.slug:' + current_user.company.slug})
   end
 
   private
