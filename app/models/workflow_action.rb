cd09302c7c8315378776e7b03e9adba54d0e8fea
Vicky Prahastra <vicky@excide.co>
2018-06-12 17:24:13 +0700

    Refactoring code

--- app/models/workflow_action.rb
+++ app/models/workflow_action.rb
@@ -99,13 +99,7 @@ class WorkflowAction < ActiveRecord::Base
     workflow_actions = WorkflowAction.where(workflow: self.workflow).joins(:task).where(tasks: { section_id: section.id })
     workflow_actions.each do |action|
       archive_task = Hash.new
-      archive_task[:instructions] = action.task.instructions
-      archive_task[:position] = action.task.position
-      archive_task[:image_url] = action.task.image_url
-      archive_task[:link_url] = action.task.link_url
-      archive_task[:role_id] = action.task.role_id
-      archive_task[:task_type] = action.task.task_type
-      archive_task[:workflow_actions] = { completed: action.completed, deadline: action.deadline, company: action.company.name, assigned_user: action.assigned_user&.full_name, completed_user: action.completed_user&.full_name }
+      archive_task = { instructions: action.task.instructions, position: action.task.position, image_url: action.task.image_url, link_url: action.task.link_url, role_id: action.task.role_id, task_type: action.task.task_type, workflow_actions: { completed: action.completed, deadline: action.deadline, company: action.company.name, assigned_user: action.assigned_user&.full_name, completed_user: action.completed_user&.full_name } }
       archive_tasks << archive_task
     end
     archive_tasks
