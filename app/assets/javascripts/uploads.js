32426a42af217bf70c815ef95ed9404e909c3c2d
Vicky Prahastra <vicky@excide.co>
2018-06-22 10:59:37 +0700

    Fix file url to location host

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -6,12 +6,13 @@ $(document).ready(function () {
   myDropzone.on("success", function (file, request) {
     var resp = $.parseXML(request);
     var filePath = $(resp).find("Key").text();
+    var location = new URL($(resp).find("Location").text())
     $.post('/symphony/documents', {
       authenticity_token: $.rails.csrfToken(),
       document: {
         filename: file.name,
         identifier: file.name,
-        file_url: filePath,
+        file_url: '//' + location['host'] + '/' + filePath,
       }
     })
   });
