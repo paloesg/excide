14e121e09dd45d57130d436231c5fc51cf49ac06
Han-Sheng Chin <hschin@gmail.com>
2018-07-12 17:13:05 +0800

    Tag uploaded invoices to correct template

--- app/controllers/symphony/documents_controller.rb
+++ app/controllers/symphony/documents_controller.rb
@@ -20,7 +20,7 @@ class Symphony::DocumentsController < DocumentsController
       if @document.save
         SlackService.new.new_document(@document).deliver
         if params[:document_type] == 'invoice'
-          @workflow = Workflow.create(user: current_user, company: @company, template: Template.where(company: @company).where('slug LIKE ?', 'payable-invoices%'), identifier: @document.identifier).take
+          @workflow = Workflow.create(user: current_user, company: @company, template: Template.where(company: @company).where('slug LIKE ?', 'payable-invoices%').take, identifier: @document.identifier)
           @document.update_attributes(workflow: @workflow)
         end
 
