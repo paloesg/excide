908909156dca83a767f085e33832283b0b821551
Han-Sheng Chin <hschin@gmail.com>
2018-07-20 12:38:33 +0800

    Remove some unneeded includes in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,9 @@
 //= require jquery.ui.widget
 //= require z.jquery.fileupload
 
-//= require flickr
 //= require flexslider.min
 //= require lightbox.min
-//= require masonry.min
-//= require twitterfetcher.min
-//= require spectragram.min
-//= require ytplayer.min
-//= require countdown.min
 //= require smooth-scroll.min
 //= require scripts
-//= require components/card_element
 
 //= require turbolinks
