f977fbac4587aed3f99573c2c354c533cec2c3d1
Vicky Prahastra <vicky@excide.co>
2018-08-01 15:07:52 +0700

    Slug generated automatically from activation type name

--- app/models/activation_type.rb
+++ app/models/activation_type.rb
@@ -1,6 +1,6 @@
 class ActivationType < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :slug, use: [:slugged, :finders]
+  friendly_id :name, use: [:slugged, :finders]
 
   has_one :activation
   validates :name, :slug, :colour, presence: true
