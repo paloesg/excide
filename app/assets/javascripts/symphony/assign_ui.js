a95b72b6f51ba5d4c59b7781bce71fd0bbe415c4
Vicky Prahastra <vicky@excide.co>
2018-08-02 16:32:46 +0700

    Fix assign user show UI

--- app/assets/javascripts/symphony/assign_ui.js
+++ app/assets/javascripts/symphony/assign_ui.js
@@ -4,12 +4,12 @@ function assign(form_element) {
   tableRow = formParent.parent().parent();
   formJquery.submit();
   $(formParent).on('ajax:success', function () {
-    $(tableRow.find(".glyphicon-ok")).show().fadeTo(500, 200, function () {
-      $(tableRow.find(".glyphicon-ok")).fadeTo(200, 0);
+    $(tableRow.find(".fa-check")).show().fadeTo(500, 200, function () {
+      $(tableRow.find(".fa-check")).fadeTo(200, 0);
     });
   }).on('ajax:error', function () {
-    $(tableRow.find(".glyphicon-remove")).show().fadeTo(500, 200, function () {
-      $(tableRow.find(".glyphicon-remove")).fadeTo(200, 0);
+    $(tableRow.find(".fa-times")).show().fadeTo(500, 200, function () {
+      $(tableRow.find(".fa-times")).fadeTo(200, 0);
     });
   });
 }
