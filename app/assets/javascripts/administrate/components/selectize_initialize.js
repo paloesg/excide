5d462a93d17b32fadeef9fca7a6861bfe7421585
Vicky Prahastra <vicky@excide.co>
2018-08-02 11:31:54 +0700

    Fix initialize selectize when adding nested fields

--- app/assets/javascripts/administrate/components/selectize_initialize.js
+++ app/assets/javascripts/administrate/components/selectize_initialize.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
+callSelectize()
+function callSelectize() {
   $('select').selectize({
     sortField: 'text'
   });
-})
+  // Initialize selectize when add fields (nested fields)
+  $(document).on('click', '.add_fields', function () {
+    callSelectize();
+  });
+}
