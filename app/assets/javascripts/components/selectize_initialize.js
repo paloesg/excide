b11ecea8a374ff8ec9b3c7a734d2687edf49e865
Vicky Prahastra <vicky@excide.co>
2018-08-06 13:24:33 +0700

    Create new class for select workflow type and activation type

--- app/assets/javascripts/components/selectize_initialize.js
+++ app/assets/javascripts/components/selectize_initialize.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+  $('select.selectize-workflow-type').selectize({
+    allowEmptyOption: true,
+    sortField: { field: '$score' },
+    onItemAdd: function (value, $item) {
+      Turbolinks.visit('//' + location.host + location.pathname + '?workflow_type=' + value);
+    }
+  });
+
+  $('select.selectize-activation-type').selectize({
+    allowEmptyOption: true,
+    sortField: { field: '$score' },
+    onItemAdd: function (value, $item) {
+      Turbolinks.visit('//' + location.host + location.pathname + '?activation_type=' + value);
+    }
+  });
+
   $('select.selectize').selectize({
     allowEmptyOption: true,
     sortField: { field: '$score' }
