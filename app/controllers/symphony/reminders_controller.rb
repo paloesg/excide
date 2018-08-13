7b623348395ac8e50eacdaa60aa13b0db94ad6ef
Vicky Prahastra <vicky@excide.co>
2018-08-13 23:21:39 +0700

    Create an edit reminder page

--- app/controllers/symphony/reminders_controller.rb
+++ app/controllers/symphony/reminders_controller.rb
@@ -1,9 +1,31 @@
 class Symphony::RemindersController < ApplicationController
   layout 'dashboard/application'
+  before_action :set_reminder, only: [:edit, :update]
 
   def index
     @user = current_user
     @company = @user.company
     @reminders = @user.reminders
+  end
+
+  def edit
+  end
+
+  def update
+    if @reminder.update(reminder_params)
+      redirect_to symphony_reminders_path, notice: 'Reminder was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_reminder
+    @reminder = Reminder.find(params[:id])
+  end
+
+  def reminder_params
+    params.require(:reminder).permit(:next_reminder, :repeat, :freq_value, :freq_unit, :email, :sms, :slack)
   end
 end
