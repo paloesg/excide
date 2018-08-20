3a4de1460d6012e5476ad4ff1c516f42e3719c11
Vicky Prahastra <vicky@excide.co>
2018-08-20 11:05:50 +0700

    Rename file if duplicate file name

--- app/assets/javascripts/components/multiple_uploads.js
+++ app/assets/javascripts/components/multiple_uploads.js
@@ -40,6 +40,21 @@ $(document).ready(function () {
         }
       });
     });
+    // Check if file name are same & rename the file
+    documentUpload.on("addedfile", function (file) {
+      if (this.files.length) {
+        var _i, _len;
+        for (_i = 0, _len = this.files.length; _i < _len - 1; _i++) {
+          if (this.files[_i].name === file.name) {
+            this.removeFile(file);
+            fileName = file.name.split('.').slice(0, -1).join('.')
+            get_extension = file.name.substring(file.name.lastIndexOf(".") + 1)
+            renameFile = new File([file], fileName + '_' + btoa(Math.random()).substr(5, 5) + '.' + get_extension, { type: file.type });
+            documentUpload.addFile(renameFile);
+          }
+        }
+      }
+    });
     documentUpload.on("queuecomplete", function (file, request) {
       $('#view-invoices-button').show();
     });
