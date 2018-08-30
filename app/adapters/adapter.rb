225454fd2c84005d11d96578f14083aa4e2154d4
Han-Sheng Chin <hschin@gmail.com>
2018-08-30 23:10:55 +0800

    Add methods to create payable and receivable invoices in xero

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -24,5 +24,21 @@ module Adapter
       xero_contact.save!
       return xero_contact.contact_id
     end
+
+    def create_invoice_payable(contact, date, due_date, item_code, description, quantity, price, account)
+      ap = @xero.Invoice.build(type: "ACCPAY", contact: contact, date: date, due_date: due_date)
+      ap.add_line_item(item_code: item_code, description: description, quantity: quantity, unit_amount: price, account_code: account)
+      ap.save
+      return ap.invoice_id
+    end
+
+    def create_invoice_receivable(type, contact, date, due_date, reference, order, account)
+      ar = @xero.Invoice.build(type: type, contact: contact, date: date, due_date: due_date, reference: reference)
+      order.order_products.each do |op|
+        ar.add_line_item(item_code: op.product.sku, description: op.product.name ,quantity: op.quantity, unit_amount: op.price, account_code: account)
+      end
+      ar.save
+      return ar.invoice_id
+    end
   end
 end
