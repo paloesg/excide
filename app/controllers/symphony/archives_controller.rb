e3394fe79fae3a81247d69e1af993ac3c3b66e42
Vicky Prahastra <vicky@excide.co>
2018-09-27 15:26:14 +0700

    Fix show activity log from archive of workflow

--- app/controllers/symphony/archives_controller.rb
+++ app/controllers/symphony/archives_controller.rb
@@ -32,7 +32,7 @@ class Symphony::ArchivesController < ApplicationController
     @section_index = @sections.index(@section)
     @document_templates = DocumentTemplate.where(template: @workflow.template)
     @documents = @company.documents.where(workflow_id: @workflow.id).order(created_at: :desc)
-    @activities = @workflow.archive['activity_log']
+    @activities = @archive['activity_log']
   end
 
   private
