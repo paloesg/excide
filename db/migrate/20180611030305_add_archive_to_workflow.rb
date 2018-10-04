15a6121f592d0957502bf62311a0118f66c38db0
Han-Sheng Chin <hschin@gmail.com>
2018-10-04 17:36:47 +0800

    Update migration file for rails 5

--- db/migrate/20180611030305_add_archive_to_workflow.rb
+++ db/migrate/20180611030305_add_archive_to_workflow.rb
@@ -1,4 +1,4 @@
-class AddArchiveToWorkflow < ActiveRecord::Migration
+class AddArchiveToWorkflow < ActiveRecord::Migration[4.2]
   def change
     add_column :workflows, :archive, :json, default: '[]'
   end
