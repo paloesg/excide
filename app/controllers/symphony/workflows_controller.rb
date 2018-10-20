ce5f7b18be75feeaba29e8c8d52a4f1d81ca49b4
Han-Sheng Chin <hschin@gmail.com>
2018-10-20 22:52:19 +0800

    Fix display of flash message after creating xero invoice

--- app/controllers/symphony/workflows_controller.rb
+++ app/controllers/symphony/workflows_controller.rb
@@ -159,7 +159,7 @@ class Symphony::WorkflowsController < WorkflowsController
       end
     end
 
-    if xero_error = false
+    if xero_error == false
       redirect_to symphony_workflow_path(@template.slug, @workflow.identifier), notice: 'Xero invoice was successfully created.'
     else
       Rails.logger.error("Xero Export Error: #{message}")
