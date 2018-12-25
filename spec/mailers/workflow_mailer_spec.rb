e70a935691c6b76a59abe72e378ea009e037c006
Jonathanlau92 <jonathan@excide.co>
2018-12-25 13:55:59 +0800

    add: testing for mailer (incomplete)

--- spec/mailers/workflow_mailer_spec.rb
+++ spec/mailers/workflow_mailer_spec.rb
@@ -2,4 +2,31 @@ require "rails_helper"
 
 RSpec.describe WorkflowMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+    @workflow = Factory.create(:workflow)
+    EbookConfirmationMailer.confirmation_email(@workflow).deliver
+  end
+
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  it 'should send an email' do
+    ActionMailer::Base.deliveries.count.should == 1
+  end
+
+  it 'renders the receiver email' do
+    ActionMailer::Base.deliveries.first.to.should == @workflow.workflowable.xero_email
+  end
+
+  it 'should set the subject to the correct subject' do
+    ActionMailer::Base.deliveries.first.subject.should == 'Xero bill invoice'
+  end
+
+
+
 end
