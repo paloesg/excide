1170a12e21bb02ed456dd8f2dcae2064f12abb1b
Jonathanlau92 <jonathan@excide.co>
2018-12-31 12:07:47 +0800

    modify: uncomment in the js file and change indentation in the form

--- app/assets/javascripts/symphony/new_workflow.js
+++ app/assets/javascripts/symphony/new_workflow.js
@@ -1,28 +1,22 @@
 $(document).ready(function(){
-    //var clone = $(".form-for-new-client").clone();
     var $detached = true;
     $detached = $(".form-for-new-client").detach();
 
     $(".existing-client-header").click(function(){
         if ( $detached ) {
             $("#headingTwo").click(function(){
-                // console.log("APPEND!");
                 $('#cardBody').append($detached);
                 $detached = null;
             })
         }
         else{
             $detached = $(".form-for-new-client").detach();
-            // console.log("DETACHED!!");
         }
-        // console.log("Final part!");
-        // $detached = $(".form-for-new-client").detach();
         return $detached;
     });
 
     $("#headingTwo").click(function(){
         if($detached){
-            // console.log("SECOND APPEND!!");
             $('#cardBody').append($detached);
             $detached = null;
         }
