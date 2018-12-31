a683c600d646af388e2eb977861c014ee65e0f99
Han-Sheng Chin <hschin@gmail.com>
2018-12-31 13:21:07 +0800

    Fix spacing of js file

--- app/assets/javascripts/symphony/new_workflow.js
+++ app/assets/javascripts/symphony/new_workflow.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-    var $detached = true;
-    $detached = $(".form-for-new-client").detach();
+  var $detached = true;
+  $detached = $(".form-for-new-client").detach();
 
-    $(".existing-client-header").click(function(){
-        if ( $detached ) {
-            $("#headingTwo").click(function(){
-                $('#cardBody').append($detached);
-                $detached = null;
-            })
-        }
-        else{
-            $detached = $(".form-for-new-client").detach();
-        }
-        return $detached;
-    });
+  $(".existing-client-header").click(function(){
+    if ( $detached ) {
+      $("#headingTwo").click(function(){
+        $('#cardBody').append($detached);
+        $detached = null;
+      })
+    }
+    else{
+      $detached = $(".form-for-new-client").detach();
+    }
+    return $detached;
+  });
 
-    $("#headingTwo").click(function(){
-        if($detached){
-            $('#cardBody').append($detached);
-            $detached = null;
-        }
-    })
+  $("#headingTwo").click(function(){
+    if($detached){
+      $('#cardBody').append($detached);
+      $detached = null;
+    }
+  })
 })
