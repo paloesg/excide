09de3a4084ef1004533667a911fb6b4e04bbf927
Jonathanlau92 <jonathan@excide.co>
2019-01-02 16:19:32 +0800

    modify: change the condition of workflow create from !present to blank

--- app/controllers/symphony/workflows_controller.rb
+++ app/controllers/symphony/workflows_controller.rb
@@ -28,7 +28,7 @@ class Symphony::WorkflowsController < WorkflowsController
     @workflow.company = @company
     @workflow.template = @template
 
-    if !params[:workflow][:workflowable_id].present?
+    if params[:workflow][:workflowable_id].blank?
       @xero = Xero.new(session[:xero_auth])
       @workflow.workflowable = Client.create(name: params[:workflow][:client][:name], identifier: params[:workflow][:client][:identifier], company: @company, user: current_user)
       contact_id = @xero.create_contact(@workflow.workflowable)
