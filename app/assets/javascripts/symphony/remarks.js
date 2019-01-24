4c6b0df83301cb299ca07e910517ebe9decee0a1
Jonathanlau92 <jonathan@excide.co>
2019-01-24 21:10:52 +0800

    modify: took out console log from the remark js file

--- app/assets/javascripts/symphony/remarks.js
+++ app/assets/javascripts/symphony/remarks.js
@@ -1,8 +1,6 @@
 $( document ).ajaxSuccess(function( event, xhr, settings ) {
-    console.log("XHR IS: ", xhr.responseText);
     var data = xhr.responseText;
     var jsonData = JSON.parse(data);
-    console.log("the JSON data is: ", jsonData);
     $("#check-" + jsonData.id).show().fadeTo(500, 200, function(){
         $("#check-" + jsonData.id).fadeTo(200, 0);
     });
