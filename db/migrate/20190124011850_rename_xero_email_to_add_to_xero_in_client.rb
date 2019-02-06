9aa3f96f3139db29400725de2dacb543fa10656c
Han-Sheng Chin <hschin@gmail.com>
2019-02-07 01:01:30 +0800

    Remove problematic migrations

--- db/migrate/20190124011850_rename_xero_email_to_add_to_xero_in_client.rb
+++
@@ -1,5 +0,0 @@
-class RenameXeroEmailToAddToXeroInClient < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :clients, :xero_email, :add_to_xero
-  end
-end
--- db/migrate/20190201022246_remove_add_to_xero_from_clients.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAddToXeroFromClients < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :clients, :add_to_xero, :boolean
-  end
-end
