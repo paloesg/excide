4f54da479b280b09a340957c12a0e0d393660b12
Jonathanlau92 <jonathan@excide.co>
2019-03-19 11:29:16 +0800

    modify: rename reference to invoice_reference

--- db/migrate/20190318065344_add_reference_to_invoices.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToInvoices < ActiveRecord::Migration[5.2]
-  def change
-    add_column :invoices, :reference, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_065344) do
+ActiveRecord::Schema.define(version: 2019_03_19_025052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -200,7 +200,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_065344) do
     t.integer "line_amount_type"
     t.integer "invoice_type"
     t.string "xero_invoice_id"
-    t.string "reference"
+    t.string "invoice_reference"
     t.index ["workflow_id"], name: "index_invoices_on_workflow_id"
   end
 
