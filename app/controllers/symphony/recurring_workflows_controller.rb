3f93ace36d6016173f71464702179af1ee3aeb0a
Jonathanlau92 <jonathan@excide.co>
2019-04-22 18:04:06 +0800

    Add form to recurring_workflow

--- app/controllers/symphony/recurring_workflows_controller.rb
+++ app/controllers/symphony/recurring_workflows_controller.rb
@@ -1,3 +1,28 @@
 class Symphony::RecurringWorkflowsController < ApplicationController
+  layout "dashboard/application"
   
+  before_action :authenticate_user!
+  before_action :set_template
+  before_action :set_company
+
+  def new
+    @recurring_workflow = RecurringWorkflow.new
+  end
+
+  def create
+
+  end
+
+  private
+  def recurring_workflow_params
+    params.require(:recurring_workflow).permit(:recurring, :freq_value, :freq_unit, :template_id)
+  end
+
+  def set_template
+    @template = Template.find(params[:recurring_workflow_name])
+  end
+
+  def set_company
+    @company = current_user.company
+  end
 end
--- app/models/recurring_workflow.rb
+++ app/models/recurring_workflow.rb
@@ -1,4 +1,6 @@
 class RecurringWorkflow < ApplicationRecord
   belongs_to :template
   has_many :workflows
+
+  enum freq_unit: {days: 0, weeks: 1, months: 2, years: 3}
 end
