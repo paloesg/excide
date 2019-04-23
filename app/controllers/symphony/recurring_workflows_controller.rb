2252db391199a38be141c8e3e38ba0973d4fdd8f
Jonathanlau92 <jonathan@excide.co>
2019-04-23 11:26:15 +0800

    Recurring workflow create action
    Also creating a workflow out of it

--- app/controllers/symphony/recurring_workflows_controller.rb
+++ app/controllers/symphony/recurring_workflows_controller.rb
@@ -10,7 +10,16 @@ class Symphony::RecurringWorkflowsController < ApplicationController
   end
 
   def create
-
+    @recurring_workflow = RecurringWorkflow.new(recurring_workflow_params)
+    @recurring_workflow.template = @template
+    @recurring_workflow.recurring = true
+    if @recurring_workflow.save
+      #creating the first workflow before recurring it through calling the service object
+      @workflow = Workflow.create(user_id: current_user.id, company_id: @company.id, template_id: @template.id, identifier: (Date.current.to_s + '-' + @template.title + '-' +SecureRandom.hex).parameterize.upcase)
+      @workflow.recurring_workflow = @recurring_workflow
+      @workflow.save
+      redirect_to symphony_workflow_path(@template.slug, @workflow.identifier), notice: 'Workflow was successfully created.'
+    end
   end
 
   private
