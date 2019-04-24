a0fa96dd9f6af0ce995fe3020612ab322027efd8
Jonathanlau92 <jonathan@excide.co>
2019-04-24 15:46:47 +0800

    Modify rake task and service with relation to next_workflow_date
    Instead of getting workflows to get next_workflow_date, now can get next_workflow_date in recurring_workflow

--- app/services/generate_recurring_workflow.rb
+++ app/services/generate_recurring_workflow.rb
@@ -5,18 +5,13 @@ class GenerateRecurringWorkflow
   end
 
   def run
-    get_recurring_workflow_today
-    @current_recurring_workflows.each do |workflow|
-      @next_workflow = Workflow.create(user_id: workflow.user.id, company_id: workflow.company.id, template_id: workflow.template.id, recurring_workflow_id: @recurring_workflow.id, identifier: (Date.current.to_s + '-' + workflow.template.title + '-' + SecureRandom.hex).parameterize.upcase)
-      set_next_recurring_workflow
+    @recurring_workflow.workflows.each do |workflow|
+      Workflow.create(user_id: workflow.user.id, company_id: workflow.company.id, template_id: workflow.template.id, recurring_workflow_id: @recurring_workflow.id, identifier: (Date.current.to_s + '-' + workflow.template.title + '-' + SecureRandom.hex).parameterize.upcase)
     end
+    set_next_recurring_workflow
   end
 
   private
-  def get_recurring_workflow_today
-    @current_recurring_workflows = Workflow.today.where(recurring_workflow_id: @recurring_workflow.id)
-  end
-
   def set_next_recurring_workflow
     @recurring_workflow.next_workflow_date = Date.current + @recurring_workflow.freq_value.send(@recurring_workflow.freq_unit)
     @recurring_workflow.save   
--- lib/tasks/generate_recurring_workflow.rake
+++ lib/tasks/generate_recurring_workflow.rake
@@ -2,9 +2,10 @@ namespace :scheduler do
   desc "This task is called by the Heroku scheduler add-on and to generate recurring workflows"
 
   task :recurring_workflows => :environment do 
-    @recurring_workflows = RecurringWorkflow.all
+    #To check that workflow is recurring today (current date)
+    @recurring_workflows = RecurringWorkflow.today
     @recurring_workflows.each do |recurring_workflow|
-    	GenerateRecurringWorkflow.new(recurring_workflow).run
+      GenerateRecurringWorkflow.new(recurring_workflow).run
     end
   end
 end
