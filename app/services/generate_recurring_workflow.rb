a3f6289648a30d3ffb96b236a6e756f5a8cc01fe
Jonathanlau92 <jonathan@excide.co>
2019-04-25 15:18:00 +0800

    modify for cleaner code in recurring workflows service

--- app/services/generate_recurring_workflow.rb
+++ app/services/generate_recurring_workflow.rb
@@ -5,13 +5,15 @@ class GenerateRecurringWorkflow
   end
 
   def run
-    @recurring_workflow.workflows.each do |workflow|
-      Workflow.create(user_id: workflow.user.id, company_id: workflow.company.id, template_id: workflow.template.id, recurring_workflow_id: @recurring_workflow.id, identifier: (Date.current.to_s + '-' + workflow.template.title + '-' + SecureRandom.hex).parameterize.upcase)
-    end
+    generate_workflow
     set_next_recurring_workflow
   end
 
   private
+  def generate_workflow
+    Workflow.create(company_id: @recurring_workflow.template.company.id, template_id: @recurring_workflow.template.id, recurring_workflow_id: @recurring_workflow.id, identifier: (Date.current.to_s + '-' + @recurring_workflow.template.title + '-' + SecureRandom.hex).parameterize.upcase)
+  end
+
   def set_next_recurring_workflow
     @recurring_workflow.next_workflow_date = Date.current + @recurring_workflow.freq_value.send(@recurring_workflow.freq_unit)
     @recurring_workflow.save   
