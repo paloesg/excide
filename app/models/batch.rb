0411747a709b601b4af5234baab4df5fecf0ffe5
Jonathanlau92 <jonathan@excide.co>
2019-05-08 17:22:52 +0800

    Create custom first and last method for batch
    This is so that we can still use Batch.last since now all ID have been changed to UUID

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,4 +2,13 @@ class Batch < ApplicationRecord
   belongs_to :company
   belongs_to :template
   has_many :workflows, dependent: :destroy
+
+  # replacement for .first/.last because we use uuids
+  def self.first
+    order("batches.created_at").first
+  end
+    
+  def self.last
+    order("batches.created_at DESC").first
+  end
 end
