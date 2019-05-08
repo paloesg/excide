e54f60d842c1d88c28a3a3d42efe0ae0d5a5f9c7
Jonathanlau92 <jonathan@excide.co>
2019-05-08 16:16:37 +0800

    Fix indentation of file

--- db/migrate/20190508075413_change_batch_id_to_uuid.rb
+++ db/migrate/20190508075413_change_batch_id_to_uuid.rb
@@ -1,12 +1,12 @@
 class ChangeBatchIdToUuid < ActiveRecord::Migration[5.2]
-	def change
-	     enable_extension 'uuid-ossp'
-	     add_column :batches, :uuid, :uuid, default: "uuid_generate_v4()", null: false
+  def change
+       enable_extension 'uuid-ossp'
+       add_column :batches, :uuid, :uuid, default: "uuid_generate_v4()", null: false
 
-	   change_table :batches do |t|
-	     t.remove :id
-	     t.rename :uuid, :id
-	   end
-	   execute "ALTER TABLE batches ADD PRIMARY KEY (id);"
-	end
+     change_table :batches do |t|
+       t.remove :id
+       t.rename :uuid, :id
+     end
+     execute "ALTER TABLE batches ADD PRIMARY KEY (id);"
+  end
 end
