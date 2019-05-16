ee47976cd2a51a84dbce7cdf92e6c3765a2589fc
Jonathanlau92 <jonathan@excide.co>
2019-05-16 17:11:35 +0800

    Task instruction appears when mouse hovers over

--- app/assets/javascripts/symphony/popover.js
+++ app/assets/javascripts/symphony/popover.js
@@ -1,9 +1,4 @@
 $(function () {
   // Popover for batch processing for multiple uploads; Popover will show instructions and roles of the tasks
   $('[data-toggle="popover"]').popover();
-  $('.pop').popover().click(function () {
-    setTimeout(function () {
-        $('.pop').popover('hide');
-    }, 5000);
-  });
 });
