d3869c9fb77e3396a5fee46fef587dd25c9d7246
Jonathanlau92 <jonathan@excide.co>
2019-05-22 20:14:37 +0800

    Modify to codacy cleaner code

--- app/assets/javascripts/symphony/remarks.js
+++ app/assets/javascripts/symphony/remarks.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  const $form = $( '.edit_workflow_action' );
+  const $form = $( ".edit_workflow_action" );
 
-  $form.on('ajax:success', function(event, xhr, settings) {
+  $form.on("ajax:success", function(event, xhr, settings) {
     var jsonData = JSON.parse(xhr);
     $("#check-" + jsonData.id).show().fadeTo(500, 200, function(){
         $("#check-" + jsonData.id).fadeTo(200, 0);
