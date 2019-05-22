f97e023c5d5e4da42eaf85b2e9e8c592ead56287
Jonathanlau92 <jonathan@excide.co>
2019-05-22 20:01:17 +0800

    Remove unnecessary code

--- app/assets/javascripts/symphony/remarks.js
+++ app/assets/javascripts/symphony/remarks.js
@@ -1,11 +1,3 @@
-// $( document ).ajaxSuccess(function( event, xhr, settings ) {
-//     var data = xhr.responseText;
-//     var jsonData = JSON.parse(data);
-//     $("#check-" + jsonData.id).show().fadeTo(500, 200, function(){
-//         $("#check-" + jsonData.id).fadeTo(200, 0);
-//     });
-//     $("#remarks-" + jsonData.id).text(jsonData.remarks);
-// });
 $(document).ready(function() {
   const $form = $( '.edit_workflow_action' );
 
