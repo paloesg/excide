92a22eb30c9251b1675a5e313635f1db4e41e2ce
Jonathanlau92 <jonathan@excide.co>
2019-05-22 15:14:43 +0800

    Modify JS script for remarks to only target the form

--- app/assets/javascripts/symphony/remarks.js
+++ app/assets/javascripts/symphony/remarks.js
@@ -1,8 +1,19 @@
-$( document ).ajaxSuccess(function( event, xhr, settings ) {
-    var data = xhr.responseText;
-    var jsonData = JSON.parse(data);
+// $( document ).ajaxSuccess(function( event, xhr, settings ) {
+//     var data = xhr.responseText;
+//     var jsonData = JSON.parse(data);
+//     $("#check-" + jsonData.id).show().fadeTo(500, 200, function(){
+//         $("#check-" + jsonData.id).fadeTo(200, 0);
+//     });
+//     $("#remarks-" + jsonData.id).text(jsonData.remarks);
+// });
+$(document).ready(function() {
+  const $form = $( '.edit_workflow_action' );
+
+  $form.on('ajax:success', function(event, xhr, settings) {
+    var jsonData = JSON.parse(xhr);
     $("#check-" + jsonData.id).show().fadeTo(500, 200, function(){
         $("#check-" + jsonData.id).fadeTo(200, 0);
     });
     $("#remarks-" + jsonData.id).text(jsonData.remarks);
+  });
 });
