49612c7249d771bf53131342a5303bbc60561f0f
Jonathanlau92 <jonathan@excide.co>
2019-05-29 12:30:57 +0800

    Add template routes and controller for front end

---
+++ app/controllers/symphony/templates_controller.rb
@@ -0,0 +1,13 @@
+class Symphony::InvoicesController < ApplicationController
+	layout 'dashboard/application'
+
+  before_action :authenticate_user!
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
