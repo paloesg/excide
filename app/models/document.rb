ec02812e847bcaf4a921c823d2e0696ed2c20637
Vicky Prahastra <vicky@excide.co>
2019-05-31 16:52:45 +0700

    Update document model, add template template slug for algolia

--- app/models/document.rb
+++ app/models/document.rb
@@ -19,9 +19,9 @@ class Document < ApplicationRecord
 
   include AlgoliaSearch
   algoliasearch do
-    attribute :id, :filename, :file_url, :created_at, :updated_at, :remarks
+    attribute :id, :filename, :file_url, :created_at, :updated_at
     attribute :workflow do
-      { identifier: workflow&.identifier, template_title: workflow&.template&.title }
+      { identifier: workflow&.identifier, template_title: workflow&.template&.title, template_slug: workflow&.template&.slug }
     end
     attribute :document_template do
       { title: document_template&.title }
