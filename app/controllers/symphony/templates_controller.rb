b205513f4d7e3dc6f2216612c073bd6891fb996f
Jonathanlau92 <jonathan@excide.co>
2019-06-06 09:52:47 +0800

    Fix indentation

--- app/controllers/symphony/templates_controller.rb
+++ app/controllers/symphony/templates_controller.rb
@@ -5,12 +5,12 @@ class Symphony::TemplatesController < ApplicationController
   before_action :set_company
 
   def index
-  	@templates = Template.all.where(company: @company)
+    @templates = Template.all.where(company: @company)
   end
 
   def edit
-  	@template = Template.find(params[:template_slug])
-  	@section = Section.find(params[:section_id])
+    @template = Template.find(params[:template_slug])
+    @section = Section.find(params[:section_id])
   end
 
   def update
@@ -25,10 +25,10 @@ class Symphony::TemplatesController < ApplicationController
 
   private
   def set_company
-  	@company = current_user.company
+    @company = current_user.company
   end
 
   def template_params
-  	params.require(:template).permit(:title, :company_id, :workflow_type, sections_attributes: [:id, :display_name, :unique_name, :position, tasks_attributes: [:id, :position, :task_type, :instructions, :role_id, :document_template_id, :days_to_complete, :set_reminder, :link_url, :image_url] ])
+    params.require(:template).permit(:title, :company_id, :workflow_type, sections_attributes: [:id, :display_name, :unique_name, :position, tasks_attributes: [:id, :position, :task_type, :instructions, :role_id, :document_template_id, :days_to_complete, :set_reminder, :link_url, :image_url] ])
   end
 end
