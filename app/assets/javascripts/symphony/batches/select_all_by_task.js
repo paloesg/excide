c0fb73a81d86f903cd8be8f97e602d9ae60eab14
Han-Sheng Chin <hschin@gmail.com>
2019-06-18 15:21:50 +0800

    Improve code quality

--- app/assets/javascripts/symphony/batches/select_all_by_task.js
+++ app/assets/javascripts/symphony/batches/select_all_by_task.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   var clicked = false;
   $(".checkall").on("click", function() {
-    var button_checkall = $(this).attr("id")
-    var task_id = button_checkall.replace("selectall-","")
-    $(".completed-"+task_id).prop("checked", !clicked)
-    clicked = !clicked
+    var buttonCheckAll = $(this).attr("id");
+    var task_id = buttonCheckAll.replace("selectall-","");
+    $(".completed-"+task_id).prop("checked", !clicked);
+    clicked = !clicked;
 
-    var workflow_action_ids = []
+    var workflowActionIds = [];
     $(".completed-"+task_id).each(function (index, value){
-      workflow_action_ids.push($(this).attr("workflow_action"))
+      workflowActionIds.push($(this).attr("workflow_action"))
     });
 
     $.post("/symphony/workflow/task/toggle-all", {
-      workflow_action_ids: workflow_action_ids
+      workflow_action_ids: workflowActionIds
     }).done(function() {
-      location.reload()
+      location.reload();
     });
   });
 });
