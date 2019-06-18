e788c8cbc041f0b413364e5a312adcd32877743a
Han-Sheng Chin <hschin@gmail.com>
2019-06-18 15:28:30 +0800

    Fix some js code quality issues

--- app/assets/javascripts/symphony/batches/select_all_by_task.js
+++ app/assets/javascripts/symphony/batches/select_all_by_task.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   var clicked = false;
   $(".checkall").on("click", function() {
     var buttonCheckAll = $(this).attr("id");
-    var task_id = buttonCheckAll.replace("selectall-","");
-    $(".completed-"+task_id).prop("checked", !clicked);
+    var taskId = buttonCheckAll.replace("selectall-","");
+    $(".completed-"+taskId).prop("checked", !clicked);
     clicked = !clicked;
 
     var workflowActionIds = [];
-    $(".completed-"+task_id).each(function (index, value){
-      workflowActionIds.push($(this).attr("workflow_action"))
+    $(".completed-"+taskId).each(function (index, value){
+      workflowActionIds.push($(this).attr("workflow_action"));
     });
 
     $.post("/symphony/workflow/task/toggle-all", {
