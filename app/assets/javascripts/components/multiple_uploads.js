990cb9f2f8343232c4b00525f98bbaaaf73d4005
Vicky Prahastra <vicky@excide.co>
2019-06-19 10:40:13 +0700

    Revert multiple upload to 'add validation'

--- app/assets/javascripts/components/multiple_uploads.js
+++ app/assets/javascripts/components/multiple_uploads.js
@@ -8,44 +8,34 @@ $(document).ready(function () {
     }
   })
 
-  //upload documents on workflows and batch page
-  if ($(".action_id").length){
-    $( ".action_id" ).each(function( index ) {
-      var action_id = $(this).attr('id')
-      var workflow_action_id = $('#'+action_id).val();
-      var action_id_str = action_id.substr(10);
-
-      if($(".uploadToXero"+workflow_action_id).length){
-        var cleanFilename = function (name) {
-          fileName = name.split('.').slice(0, -1).join('.')
-          get_extension = name.substring(name.lastIndexOf(".") + 1)
-          // Filter out special characters and spaces in filename (same as parametrize function in rails)
-          filter_filename = fileName.toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/(^-|-$)/g,'');
-          return filter_filename + '.' + get_extension;
-        };
-        var documentUploadToXero = new Dropzone(".uploadToXero"+workflow_action_id,{
-          timeout: 0,
-          renameFilename: cleanFilename,
-        })
-        documentUploadToXero.on("success", function(file, request){
-          var resp = $.parseXML(request);
-          var filePath = $(resp).find("Key").text();
-          var location = new URL($(resp).find("Location").text());
-          if($("#uploadToXero"+workflow_action_id).length){
-            //check this part of drag and drop
-            $.post('/symphony/documents', {
-              authenticity_token: $.rails.csrfToken(),
-              workflow: $('#workflow_identifier_'+action_id_str).val(),
-              workflow_action: workflow_action_id,
-              document: {
-                filename: file.upload.filename,
-                file_url: '//' + location['host'] + '/' + filePath
-              }
-            });
+  if($(".uploadToXero").length){
+    var cleanFilename = function (name) {
+      fileName = name.split('.').slice(0, -1).join('.')
+      get_extension = name.substring(name.lastIndexOf(".") + 1)
+      // Filter out special characters and spaces in filename (same as parametrize function in rails)
+      filter_filename = fileName.toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/(^-|-$)/g,'');
+      return filter_filename + '.' + get_extension;
+    };
+    var documentUploadToXero = new Dropzone(".uploadToXero",{
+      timeout: 0,
+      renameFilename: cleanFilename,
+    })
+    documentUploadToXero.on("success", function(file, request){
+      var resp = $.parseXML(request);
+      var filePath = $(resp).find("Key").text();
+      var location = new URL($(resp).find("Location").text());
+      if($("#uploadToXero").length){
+        //check this part of drag and drop
+        $.post('/symphony/documents', {
+          authenticity_token: $.rails.csrfToken(),
+          workflow: $('#workflow_identifier').val(),
+          document: {
+            filename: file.upload.filename,
+            file_url: '//' + location['host'] + '/' + filePath
           }
-        })
+        });
       }
-    });
+    })
   }
 
   // $("#uploader").length 'To check #uploader is exists'
@@ -57,25 +47,13 @@ $(document).ready(function () {
       filter_filename = fileName.toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/(^-|-$)/g,'');
       return filter_filename + '.' + get_extension;
     };
-    if ($("#documentIndex")) {
-      var documentUpload = new Dropzone('.multiple_uploads', { timeout: 0, renameFilename: cleanFilename });
-    } else {
-      var documentUpload = new Dropzone(".multiple_uploads", {
-        timeout: 0,
-        renameFilename: cleanFilename,
-        autoProcessQueue: false,
-        parallelUploads: 100,
-        uploadMultiple: false,
-      });
-    }
-    documentUpload.on("sending", function(file) {
-      if ($("#documentIndex")) {
-        if ( $('#template_id').val() == "" ) {
-          alert('Template is required.');
-          this.removeFile(file);
-        }
-      }
-    })
+    var documentUpload = new Dropzone(".multiple_uploads", {
+      timeout: 0,
+      renameFilename: cleanFilename,
+      autoProcessQueue: false,
+      parallelUploads: 100,
+      uploadMultiple: false,
+    });
     documentUpload.on("addedfile", function () {
       $('#drag-and-drop-submit').removeAttr('disabled');
     });
@@ -123,21 +101,6 @@ $(document).ready(function () {
     });
     documentUpload.on("queuecomplete", function (file, request) {
       $('#view-invoices-button').show();
-      // Get url files after uploaded
-      var url_files = []
-      if ($("#documentIndex")) {
-        $.each(this.files, function(index, value) {
-          var key     = $(value.xhr.responseXML).find("Key").text();
-          var parser  = document.createElement('a');
-          parser.href = $(value.xhr.responseXML).find("Location").text()
-          var url     = '//' + parser.hostname + '/' + key;
-          url_files.push(url)
-        });
-      }
-      $.post('/symphony/documents/index-create', {
-        authenticity_token: $.rails.csrfToken(),
-        url_files: url_files
-      });
     });
   };
 });
