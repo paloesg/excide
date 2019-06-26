161ea0b48c7799984d0fee5fd5f31cc428a1ec34
Lim Ning <e0324216@u.nus.edu>
2019-06-26 13:39:37 +0800

    Created spec files and shoulda tests

--- spec/models/availability_spec.rb
+++ spec/models/availability_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Availability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:end_time) }
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:available_date) }
+
+  it { should belong_to(:user) }
+
 end
---
+++ spec/models/company_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  it { should belong_to(:consultant) }
+  it { should belong_to(:associate) }
+  it { should belong_to(:shared_service) }
+
+  it { should have_many(:users) }
+  it { should have_many(:documents) }
+  it { should have_many(:templates) }
+  it { should have_many(:workflows) }
+  it { should have_many(:recurring_workflows) }
+  it { should have_many(:workflow_actions) }
+  it { should have_many(:clients) }
+  it { should have_many(:activations) }
+  it { should have_many(:reminders) }
+  it { should have_many(:batches) }
+  it { should have_one(:address) }
+
+  it { should accept_nested_attributes_for(:address) }
+
+  it { should define_enum_for(:company_type) }
+  it { should define_enum_for(:gst_quarter) }
+end
