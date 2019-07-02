7021ef232afbec8085a6f6703686aefb3f3c5ebe
Vicky Prahastra <vicky@excide.co>
2019-07-02 17:02:01 +0700

    Fix undefined method find_all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,8 @@
 
 require_relative 'support/controller_helpers'
 require 'devise'
-require 'pundit/rspec'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.include ControllerHelpers, type: :controller
