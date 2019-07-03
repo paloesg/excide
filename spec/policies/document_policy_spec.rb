e6204a3a68de7ea12800a7ace3afced5effa984d
Vicky Prahastra <vicky@excide.co>
2019-07-03 09:58:07 +0700

    Test document policies for admin company

--- spec/policies/document_policy_spec.rb
+++ spec/policies/document_policy_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe DocumentPolicy do
   subject { DocumentPolicy.new(user, document) }
   company = FactoryBot.create(:company)
   user_of_document = FactoryBot.create(:user, company: company)
+  company_admin = FactoryBot.create(:company_admin, company: company)
   let(:document) { FactoryBot.create(:document, company: company, user: user_of_document) }
 
   context "for a user" do
@@ -22,5 +23,14 @@ RSpec.describe DocumentPolicy do
     it { should permit(:update) }
     it { should permit(:edit) }
     it { should_not permit(:destroy) }
+  end
+
+  context "for a admin of company" do
+    let(:user) { company_admin }
+    it { should permit(:show) }
+    it { should permit(:create) }
+    it { should permit(:update) }
+    it { should permit(:edit) }
+    it { should permit(:destroy) }
   end
 end
