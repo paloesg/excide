6dfcab62f4733b0a2c27e831aa249ccc3dff7c51
Vicky Prahastra <vicky@excide.co>
2019-07-03 14:36:47 +0700

    Create invoices factories

---
+++ spec/factories/invoices.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :invoice do
+    invoice_identifier { Faker::Internet.email }
+    invoice_date { Date.today }
+    due_date { Date.today.next_month }
+    line_items { [] }
+    line_amount_type { 0 }
+    invoice_type { 0 }
+    xero_invoice_id { 0 }
+    xero_contact_id { 0 }
+    xero_contact_name { "sansa" }
+    currency { "USD" }
+    approved { true }
+    total { 123 }
+    user
+  end
+end
