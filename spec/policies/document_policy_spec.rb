85f0854432de01ec0ca73615588d5324ca3cd461
Vicky Prahastra <vicky@excide.co>
2019-07-03 09:44:07 +0700

    Write test for user's document

--- spec/policies/document_policy_spec.rb
+++ spec/policies/document_policy_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 RSpec.describe DocumentPolicy do
   subject { DocumentPolicy.new(user, document) }
-  let(:document) { FactoryBot.create(:document) }
+  company = FactoryBot.create(:company)
+  user_of_document = FactoryBot.create(:user, company: company)
+  let(:document) { FactoryBot.create(:document, company: company, user: user_of_document) }
 
   context "for a user" do
     let(:user) { FactoryBot.create(:user) }
     it { should_not permit(:show) }
+    it { should permit(:create) }
+    it { should_not permit(:update) }
+    it { should_not permit(:edit) }
+    it { should_not permit(:destroy) }
+  end
+
+  context "for a user with the same company with document" do
+    let(:user) { user_of_document }
+    it { should permit(:show) }
+    it { should permit(:create) }
+    it { should permit(:update) }
+    it { should permit(:edit) }
+    it { should_not permit(:destroy) }
   end
 end
