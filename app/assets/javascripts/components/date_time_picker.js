fce6eb506fc7ed18dc95d0e543ea4d01df7a2bc5
frediansimanjuntak <fredian.pc@gmail.com>
2019-07-18 16:15:17 +0700

    Add auto format date on js

--- app/assets/javascripts/components/date_time_picker.js
+++ app/assets/javascripts/components/date_time_picker.js
@@ -12,4 +12,46 @@ $(document).on("turbolinks:load", function() {
     format: 'HH:mm',
     stepping: 15
   });
+
+
+  //auto convert date
+
+  var tabKeyPressed = false;
+
+  $(".autodate").keydown(function(e) {
+    tabKeyPressed = e.keyCode == 9;
+    if (tabKeyPressed) {
+      e.preventDefault();
+      return;
+    }
+  });
+
+  $(".autodate").keyup(function(e) {
+    if (tabKeyPressed) {
+      let dateStr = $(this).val();
+      let dateVar = dateStr.replace(/[\. ,:-]+/g, "-");
+      var dsplit = dateVar.split("-");
+      if (!dsplit[2]){
+        dsplit[2] = new Date().getFullYear();
+      }
+
+      var d = new Date(dsplit[2],dsplit[1]-1,dsplit[0]);
+
+      if (d == "Invalid Date"){
+        dateVar = dsplit.join();
+        d = new Date(dateVar);
+        if (d == "Invalid Date"){
+          d = new Date();
+        }
+      }
+      d = moment(d).format("D MMM YYYY")
+      $(this).val(d);
+      e.preventDefault();
+      return;
+    }
+  });
+
+  $(".dateinvoice").datetimepicker({
+    format: "D MMM YYYY",
+  });
 });
