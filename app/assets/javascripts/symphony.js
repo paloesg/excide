04c0cadbfa715be74abded2381ecdd08e113a2fe
Jonathanlau92 <jonathan@excide.co>
2019-07-20 09:13:42 +0800

    Fix javascript for adding nested fields
    Error: The 1st time when clicking the button, it appears as one field. Subsequently clicks results in creating incremental fields (2 fields, then 3 fields etc)

--- app/assets/javascripts/symphony.js
+++ app/assets/javascripts/symphony.js
@@ -59,46 +59,41 @@ $(document).on("turbolinks:load", function(){
         dropdownParent: "body"
       });
       $('.data-attributes').find('tr:last-child').find('.create').val('1');
-      return event.preventDefault();
     });
+    return event.preventDefault();
     //General way of adding attribute through link_to_add_row method in application helper
-    $('form').on('click', '.add_task_fields', function(event) {
-      var regexp, time;
-      time = new Date().getTime();
-      regexp = new RegExp($(this).data('id'), 'g');
-      $( '.task-in-section-' + $(this).data('sectionId') ).append($(this).data('fields').replace(regexp, time));
-      //Loop section count to get the index of the section's array
-      $( ".section-count" ).each(function( index ) {
-        $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_task_type']").selectize({
-          dropdownParent: "body"
-        });
-        $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_role_id']").selectize({
-          dropdownParent: "body"
-        });
-        $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_document_template_id']").selectize({
-          dropdownParent: "body"
-        });
+    $( '.task-in-section-' + $(this).data('sectionId') ).append($(this).data('fields').replace(regexp, time));
+    //Loop section count to get the index of the section's array
+    $( ".section-count" ).each(function( index ) {
+      $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_task_type']").selectize({
+        dropdownParent: "body"
       });
-      return event.preventDefault();
-    });
-    //if radio button is checked, disable or enable the relevant fields
-    $('input:radio[name="radioContact"]').click(
-      function(){
-        if($(this).val() == 'existing'){
-          $('.new-disable').attr('disabled', true);
-          $('.existing-contact-disable')[0].selectize.enable();
-        }
-        else{
-          $('.existing-contact-disable')[0].selectize.disable();
-          $('.new-disable').attr('disabled', false);
-        }
-      }
-    )
-    return $('input').change(function(event) {
-      return $(this).closest('td').next().find('.update').val('1');
+      $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_role_id']").selectize({
+        dropdownParent: "body"
+      });
+      $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_document_template_id']").selectize({
+        dropdownParent: "body"
+      });
     });
     return event.preventDefault();
   });
+  //if radio button is checked, disable or enable the relevant fields
+  $('input:radio[name="radioContact"]').click(
+    function(){
+      if($(this).val() == 'existing'){
+        $('.new-disable').attr('disabled', true);
+        $('.existing-contact-disable')[0].selectize.enable();
+      }
+      else{
+        $('.existing-contact-disable')[0].selectize.disable();
+        $('.new-disable').attr('disabled', false);
+      }
+    }
+  )
+  return $('input').change(function(event) {
+    return $(this).closest('td').next().find('.update').val('1');
+  });
+  return event.preventDefault();
   //if radio button is checked, disable or enable the relevant fields
   $('input:radio[name="radioContact"]').click(function(){
     if($(this).val() == 'existing'){
