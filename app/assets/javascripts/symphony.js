2a3983a1866cc3acde2310ebb181ec19a99fc0fc
frediansimanjuntak <fredian.pc@gmail.com>
2019-07-22 14:30:14 +0800

    Fix JS duplicate function

--- app/assets/javascripts/symphony.js
+++ app/assets/javascripts/symphony.js
@@ -61,26 +61,6 @@ $(document).on("turbolinks:load", function(){
       $('.data-attributes').find('tr:last-child').find('.create').val('1');
       return event.preventDefault();
     });
-    //General way of adding attribute through link_to_add_row method in application helper
-    $('form').on('click', '.add_task_fields', function(event) {
-      var regexp, time;
-      time = new Date().getTime();
-      regexp = new RegExp($(this).data('id'), 'g');
-      $( '.task-in-section-' + $(this).data('sectionId') ).append($(this).data('fields').replace(regexp, time));
-      //Loop section count to get the index of the section's array
-      $( ".section-count" ).each(function( index ) {
-        $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_task_type']").selectize({
-          dropdownParent: "body"
-        });
-        $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_role_id']").selectize({
-          dropdownParent: "body"
-        });
-        $("select[id$='template_sections_attributes_" + index + "_tasks_attributes_" + time + "_document_template_id']").selectize({
-          dropdownParent: "body"
-        });
-      });
-      return event.preventDefault();
-    });
 
     //if radio button is checked, disable or enable the relevant fields
     $("input:radio[name='radioContact']").click(
