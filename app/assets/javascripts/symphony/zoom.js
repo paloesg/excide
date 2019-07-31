858f5443c5cefc5c73657adeb1e441ade527e954
Jonathanlau92 <jonathan@excide.co>
2019-07-31 22:14:34 +0800

    Clean code to fix codacy issues

--- app/assets/javascripts/symphony/zoom.js
+++ app/assets/javascripts/symphony/zoom.js
@@ -1,9 +1,9 @@
-$(document).on('turbolinks:load', function() { 
+$(document).on("turbolinks:load", function() { 
   function magnify(imgID, zoom) {
     var $img;
     $img = $("." + imgID);
     //load the image to get the proper height of the image after it is loaded, but have to off the 'load', so that it loads the image on refresh (this is to prevent the function not working on refresh)
-    $img.off('load').on('load', function(){
+    $img.off("load").on("load", function(){
       var glass, imageUrl, w, h, bw;
       /* Create magnifier glass: */
       glass = $("<div>");
@@ -21,13 +21,19 @@ $(document).on('turbolinks:load', function() {
       w = glass[0].offsetWidth / 2;
       h = glass[0].offsetHeight / 2;
 
-      /* Execute a function when someone moves the magnifier glass over the image: */
-      glass.on("mousemove", moveMagnifier);
-      $img.on("mousemove", moveMagnifier);
-
-      /*and also for touch screens:*/
-      glass.on("touchmove", moveMagnifier);
-      $img.on("touchmove", moveMagnifier);
+      function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        /* Get the x and y positions of the image: */
+        a = $img[0].getBoundingClientRect();
+        // Calculate the cursor's x and y coordinates, relative to the image: 
+        x = e.pageX - a.left;
+        y = e.pageY - a.top;
+        /* Consider any page scrolling: */
+        x = x - window.pageXOffset;
+        y = y - window.pageYOffset;
+        return {x, y};
+      };
       
       function moveMagnifier(e) {
         var pos, x, y;
@@ -38,7 +44,7 @@ $(document).on('turbolinks:load', function() {
         x = pos.x;
         y = pos.y;
         /* Prevent the magnifier glass from being positioned outside the image: */
-        if (x > $img[0].width - (w / zoom)) {x = $img[0].width - (w / zoom);}
+        if (x > $img[0].width - (w / zoom)){x = $img[0].width - (w / zoom);}
         if (x < w / zoom) {x = w / zoom;}
         if (y > $img[0].height - (h / zoom)) {y = $img[0].height - (h / zoom);}
         if (y < h / zoom) {y = h / zoom;}
@@ -52,19 +58,13 @@ $(document).on('turbolinks:load', function() {
         });
       };
 
-      function getCursorPos(e) {
-        var a, x = 0, y = 0;
-        e = e || window.event;
-        /* Get the x and y positions of the image: */
-        a = $img[0].getBoundingClientRect();
-        // Calculate the cursor's x and y coordinates, relative to the image: 
-        x = e.pageX - a.left;
-        y = e.pageY - a.top;
-        /* Consider any page scrolling: */
-        x = x - window.pageXOffset;
-        y = y - window.pageYOffset;
-        return {x : x, y : y};
-      };
+      /* Execute a function when someone moves the magnifier glass over the image: */
+      glass.on("mousemove", moveMagnifier);
+      $img.on("mousemove", moveMagnifier);
+
+      /*and also for touch screens:*/
+      glass.on("touchmove", moveMagnifier);
+      $img.on("touchmove", moveMagnifier);
     }); 
   };
   //check for the container before magnifying
