15cf1f7e0ef43356b9fa251064c84a6e980c2c32
frediansimanjuntak <fredian.pc@gmail.com>
2019-08-02 09:42:33 +0700

    Refactor service to generate batch
    - make transaction to create batch procedure

--- app/services/generate_batch.rb
+++ app/services/generate_batch.rb
@@ -1,16 +1,42 @@
 class GenerateBatch
-  def initialize(user, template)
+  def initialize(user, template, params_batch)
     @user = user
     @template = template
+    @params_batch = params_batch
+    @company = @user.company
   end
 
   def run
-    generate_batch
+    Batch.transaction do
+      generate_batch
+      generate_documents
+      @batch
+    end
   end
 
   private
 
   def generate_batch
-    Batch.create(user: @user, template: @template, company: @user.company)
+    @batch = Batch.create(user: @user, template: @template, company: @user.company)
+  end
+
+  def generate_documents
+    @params_batch[:document_attributes].each do |k, v|
+      @document = Document.new(filename: v['document']['filename'], file_url: v['document']['file_url'])
+      @document.company = @company
+      @document.user = @user
+      generate_workflows
+      @document.workflow = @workflow
+      @document.save
+    end
+  end
+
+  def generate_workflows
+    @workflow = Workflow.new(user: @user, company: @company, template: @template, workflowable: @client)
+    @workflow.template_data(@template)
+    # Set workflow to belong to the most recently created batch
+    # TODO: Fix concurrency issues if 2 people create batch at the same time
+    @workflow.batch = Batch.last
+    @workflow.save
   end
 end
