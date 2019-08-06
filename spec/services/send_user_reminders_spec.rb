c89e1605a6eeaf62b26a114674c32adc9c2e9672
Vicky Prahastra <vicky@excide.co>
2019-08-06 16:21:17 +0700

    Testing email and slack notification is sent out

--- spec/services/send_user_reminders_spec.rb
+++ spec/services/send_user_reminders_spec.rb
@@ -2,14 +2,41 @@ require './app/services/send_user_reminders'
 require 'rails_helper'
 
 RSpec.describe SendUserReminders do
-  reminder = FactoryBot.create(:reminder)
-  let(:send_user_reminders) { SendUserReminders.run(reminder.user) }
+  set_email = FactoryBot.create(:user, settings: [{
+    "reminder_sms": "",
+    "reminder_email": "true",
+    "reminder_slack": "",
+  }])
 
-  context "notification sent out" do
-    it { p send_user_reminders }
+  set_slack = FactoryBot.create(:user, settings: [{
+    "reminder_sms": "",
+    "reminder_email": "",
+    "reminder_slack": "true",
+  }])
+
+  set_sms = FactoryBot.create(:user, settings: [{
+    "reminder_sms": "true",
+    "reminder_email": "",
+    "reminder_slack": "",
+  }])
+
+  email_reminder = FactoryBot.create(:reminder, user: set_email)
+  slack_reminder = FactoryBot.create(:reminder, user: set_slack)
+  sms_reminder = FactoryBot.create(:reminder, user: set_sms)
+
+  send_email_reminder = SendUserReminders.run(email_reminder.user)
+  send_slack_reminder = SendUserReminders.run(slack_reminder.user)
+  send_sms_reminder = SendUserReminders.run(sms_reminder.user)
+
+  context "Send user reminder" do
     it "receive email to user" do
-      expect(ActionMailer::Base.deliveries.first.subject).to eq('Here are your reminders for today')
-      expect(ActionMailer::Base.deliveries.first.to).to eq(["#{reminder.user.email}"])
+      expect(send_email_reminder.email_status.present?).to eq(true)
+      expect(send_email_reminder.email_status.subject).to eq('Here are your reminders for today')
+      expect(send_email_reminder.email_status.to).to eq(["#{email_reminder.user.email}"])
+    end
+    it "ping to slack" do
+      expect(send_slack_reminder.slack_status.present?).to eq(true)
+      expect(send_slack_reminder.slack_status.read_body).to eq("ok")
     end
   end
 end
