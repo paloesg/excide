31d2d48b8d10936d19cccecd24c7816b142c30f6
frediansimanjuntak <fredian.pc@gmail.com>
2019-08-08 17:24:02 +0700

    Refactor view to show completed icon and change to use one button start

--- app/controllers/symphony/invoices_controller.rb
+++ app/controllers/symphony/invoices_controller.rb
@@ -39,6 +39,7 @@ class Symphony::InvoicesController < ApplicationController
 
     if @invoice.save
       if @workflow.batch
+        WorkflowAction.find(params[:workflow_action_id]).update_columns(completed: true) if params[:workflow_action_id].present?
         redirect_to symphony_batch_path(batch_template_name: @workflow.batch.template.slug, id: @workflow.batch.id), notice: "Invoice created successfully!"
       else
         redirect_to symphony_invoice_path(workflow_name: @workflow.template.slug, workflow_id: @workflow.id, id: @invoice.id), notice: "Invoice created successfully!"
