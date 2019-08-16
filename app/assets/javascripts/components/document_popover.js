dc2d81ca43f91e5fd208bc2b076330223a85ad6d
Vicky Prahastra <vicky@excide.co>
2019-08-16 14:04:03 +0700

    Minor fix

--- app/assets/javascripts/components/document_popover.js
+++ app/assets/javascripts/components/document_popover.js
@@ -1,11 +1,11 @@
 $(document).on("turbolinks:load", function() {
-  $('.pdf-preview').popover({
+  $(".pdf-preview").popover({
     html : true,
     placement : "auto",
     trigger : "focus",
     sanitize : false,
-    content: function() {
-      return '<iframe src="https://docs.google.com/viewer?url=' + $(this).attr('data-document') + '&embedded=true" frameborder="0" height="300px" width="250px"></iframe>';
-      }
+    content() {
+      return "<iframe src='https://docs.google.com/viewer?url=" + $(this).attr("data-document") + "&embedded=true' frameborder='0' height='300px' width='250px'></iframe>";
+    }
   });
 });
