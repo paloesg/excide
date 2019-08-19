63bacecc9789b30fedf66f08ae76d202449c6782
fredian <fredian@excide.co>
2019-08-19 11:10:48 +0700

    Fix migration code using up and down

--- db/migrate/20190816081328_change_document_approved_to_status.rb
+++ db/migrate/20190816081328_change_document_approved_to_status.rb
@@ -1,7 +1,13 @@
 class ChangeDocumentApprovedToStatus < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column_default :invoices, :approved, nil
     change_column :invoices, :approved, 'integer USING CAST(approved AS integer)'
     rename_column :invoices, :approved, :status
+  end
+
+  def down    
+    rename_column :invoices, :status, :approved
+    change_column :invoices, :approved, 'boolean USING CAST(approved AS boolean)'
+    change_column_default :invoices, :approved, false
   end
 end
