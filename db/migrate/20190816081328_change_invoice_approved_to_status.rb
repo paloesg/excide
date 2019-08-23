ff0ee5aef9a6d55ace0d6c58fe95dda09bf27366
Han-Sheng Chin <hschin@gmail.com>
2019-08-24 00:09:52 +0800

    Fix naming for migration file

--- db/migrate/20190816081328_change_invoice_approved_to_status.rb
+++ db/migrate/20190816081328_change_invoice_approved_to_status.rb
@@ -1,11 +1,11 @@
-class ChangeDocumentApprovedToStatus < ActiveRecord::Migration[5.2]
+class ChangeInvoiceApprovedToStatus < ActiveRecord::Migration[5.2]
   def up
     change_column_default :invoices, :approved, nil
     change_column :invoices, :approved, 'integer USING CAST(approved AS integer)'
     rename_column :invoices, :approved, :status
   end
 
-  def down    
+  def down
     rename_column :invoices, :status, :approved
     change_column :invoices, :approved, 'boolean USING CAST(approved AS boolean)'
     change_column_default :invoices, :approved, false
