80ad0873415be963e1d740cad890a14e51a7a55e
Han-Sheng Chin <hschin@gmail.com>
2019-08-26 17:54:14 +0800

    Add widget for new workflow back on dashboard page

--- app/controllers/symphony/home_controller.rb
+++ app/controllers/symphony/home_controller.rb
@@ -5,8 +5,10 @@ class Symphony::HomeController < ApplicationController
 
   def index
     @templates = policy_scope(Template).assigned_templates(current_user)
-    @workflows_array = policy_scope(Workflow).includes(:template, :workflowable).where(template: @templates)
-    @outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where.not(deadline: nil).where(company: @company).order(:deadline).includes(:task)
+    @workflow_count = policy_scope(Workflow).where(template: @templates).count
+    @outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where.not(deadline: nil).where(company: @company).order(:deadline).includes(:task).count
+    @batch_count = policy_scope(Batch).count
+    @reminder_count = current_user.reminders.count
     @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", allow_any: ['utf8', 'authenticity_token'], success_action_status: '201', acl: 'public-read')
   end
 end
