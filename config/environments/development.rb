04698aadd202685fc3151e2435c695f0742e2dc4
Jonathanlau92 <jonathan@excide.co>
2019-08-30 10:47:55 +0800

    Setup active_storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
