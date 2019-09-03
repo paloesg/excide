21299c26cf116dce59929e456d2077bc96556094
Vicky Prahastra <vicky@excide.co>
2019-09-03 12:01:39 +0700

    Fix minor, global variable

--- app/assets/javascripts/components/load_batch.js
+++ app/assets/javascripts/components/load_batch.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  /*global moment*/
   // Check #table-batches element is exist in the page
   if ($("#table-batches")) {
     $.post("/symphony/batches/load_batch/", function(data) {}).done(function(data) {
@@ -14,7 +15,7 @@ $(document).ready(function(){
           "<td>"+ batch["workflows"].length +"</td>" +
           "<td><div class='progress'><div class='progress-bar progress-bar-striped' aria-valuemax='100' aria-valuemin='0' aria-valuenow='10' role='progressbar' style='width:" + batch["action_completed_progress"] + "%' >"  + batch["action_completed_progress"] + "%</div></div></td>" +
         "</tr>");
-      })
+      });
       $("#completed-batches").text(data["completed_batches"]);
       $("#batches-count").text(data["batches"].length);
     });
