5492012913f786b29abf989325abeb87c0c67c2f
Vicky Prahastra <vicky@excide.co>
2019-09-03 11:05:21 +0700

    Minor fix

--- app/assets/javascripts/components/load_batch.js
+++ app/assets/javascripts/components/load_batch.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
   // Check #table-batches element is exist in the page
   if ($("#table-batches")) {
-    function loadBatch() {
-      $.post("/symphony/batches/load_batch/", function(data) {
-        console.log(data);
-      }).done(function(data) {
-        $(".batch-loader").remove();
-        // Add batch with the details into table batches
-        $.each(data["batches"], function(index, batch) {
-          $("#table-batches").append("<tr> \
-          <td>"+(index + 1)+"</td> \
-          <td><a href='batches/"+ batch["template"]["slug"] +"/"+ batch["id"] +"'>"+ batch["name"] +"</a></td> \
-          <td>"+ batch["user"]["first_name"] +" "+ batch["user"]["last_name"] +"</td> \
-          <td>"+ moment.parseZone(batch["updated_at"]).format("YYMMDD-H:m:s") +"</td> \
-          <td>"+ batch["get_completed_workflows"] +"</td> \
-          <td>"+ batch["workflows"].length +"</td> \
-          <td><div class='progress'><div class='progress-bar progress-bar-striped' aria-valuemax='100' aria-valuemin='0' aria-valuenow='10' role='progressbar' style='width:" + batch["action_completed_progress"] + "%' >"  + batch["action_completed_progress"] + "%</div></div></td> \
-          </tr>");
-        })
-        $("#completed-batches").text(data["completed_batches"]);
-        $("#batches-count").text(data["batches"].length);
+    $.post("/symphony/batches/load_batch/", function(data) {
+      Console.log(data);
+    }).done(function(data) {
+      $(".batch-loader").remove();
+      // Add batch with the details into table batches
+      $.each(data["batches"], function(index, batch) {
+        $("#table-batches").append("<tr>" +
+          "<td>"+(index + 1)+"</td>" +
+          "<td><a href='batches/"+ batch["template"]["slug"] +"/"+ batch["id"] +"'>"+ batch["name"] +"</a></td>" +
+          "<td>"+ batch["user"]["first_name"] +" "+ batch["user"]["last_name"] +"</td>" +
+          "<td>"+ moment.parseZone(batch["updated_at"]).format("YYMMDD-H:m:s") +"</td>" +
+          "<td>"+ batch["get_completed_workflows"] +"</td>" +
+          "<td>"+ batch["workflows"].length +"</td>" +
+          "<td><div class='progress'><div class='progress-bar progress-bar-striped' aria-valuemax='100' aria-valuemin='0' aria-valuenow='10' role='progressbar' style='width:" + batch["action_completed_progress"] + "%' >"  + batch["action_completed_progress"] + "%</div></div></td>" +
+        "</tr>");
       })
-    }
-    loadBatch(0,0);
+      $("#completed-batches").text(data["completed_batches"]);
+      $("#batches-count").text(data["batches"].length);
+    })
   }
-})
+});
