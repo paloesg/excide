91421a1df18d8454cd31327fdef44483ec817fc7
Jonathanlau92 <jonathan@excide.co>
2019-09-05 18:46:47 +0800

    Add update ability before sending to xero

--- app/controllers/symphony/invoices_controller.rb
+++ app/controllers/symphony/invoices_controller.rb
@@ -71,8 +71,11 @@ class Symphony::InvoicesController < ApplicationController
     end
     @invoice.save
     if @invoice.update(invoice_params)
+      #If associate wants to update invoice before sending to xero, symphony finds the params update_field and then redirect to the same invoice EDIT page
+      if params[:update_field] == "success" and !@invoice.xero_total_mismatch?
+        redirect_to edit_symphony_invoice_path(workflow_name: @workflow.template.slug, workflow_id: @workflow.id, id: @workflow.invoice.id, workflow_action_id: params[:workflow_action_id]), notice: 'Symphony invoice successfully updated'
       #when invoice updates with the rounding line item, update the invoice in Xero as well
-      if @invoice.xero_total_mismatch?
+      elsif @invoice.xero_total_mismatch?
         #In batch, check whether there is a next workflow
         workflow_action = @workflow.workflow_actions.find(params[:workflow_action_id])
         next_wf = @workflow.batch.next_workflow(@workflow, workflow_action)
