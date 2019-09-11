896a0ae2b67fa7372a88d54f2dfe819e953f92d5
Vicky Prahastra <vicky@excide.co>
2019-09-11 17:10:10 +0700

    Refactor code

--- app/models/workflow_action.rb
+++ app/models/workflow_action.rb
@@ -10,7 +10,6 @@ class WorkflowAction < ApplicationRecord
 
   after_save :clear_reminders, if: :ordered_workflow_task_completed?
   after_save :trigger_next_task, if: :ordered_workflow_task_completed?
-  after_save :send_email_summary , if: :check_all_actions_completed?
   after_save :workflow_completed , if: :check_all_actions_completed?
 
   belongs_to :task
@@ -67,12 +66,9 @@ class WorkflowAction < ApplicationRecord
     WorkflowAction.from("(#{assigned_actions(user).to_sql} UNION #{role_actions(user.roles).to_sql}) AS workflow_actions")
   end
 
-  def send_email_summary
-    WorkflowMailer.email_summary(self.workflow, self.workflow.user,self.workflow.company).deliver_later
-  end
-
   def workflow_completed
     self.workflow.update_column('completed', true)
+    WorkflowMailer.email_summary(self.workflow, self.workflow.user,self.workflow.company).deliver_later
   end
 
   # Check if workflow belongs to a batch, get all the actions by task grouping for the batch and check that all actions are completed
