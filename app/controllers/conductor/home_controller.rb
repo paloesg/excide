b0834700365603f3775e1da9ef78beb040faed24
frediansimanjuntak <fredian.pc@gmail.com>
2019-09-19 14:54:06 +0700

    Fix naming variable

--- app/controllers/conductor/home_controller.rb
+++ app/controllers/conductor/home_controller.rb
@@ -7,12 +7,18 @@ class Conductor::HomeController < ApplicationController
   def show
     date_from = params[:start_date] ? params[:start_date].to_date.beginning_of_month : Date.current.beginning_of_month
     date_to = date_from.to_date + 1.month
-    # get_activations = Activation.where(company: @company, start_time: date_from..date_to)
+
+    #split params to set default selected in selectize
+    params[:activation_types] = params[:activation_types].split(',') unless params[:activation_types].blank? 
+    params[:allocation_users] = params[:allocation_users].split(',') unless params[:allocation_users].blank?
+    params[:project_clients] = params[:project_clients].split(',') unless params[:project_clients].blank?
+    
+    #filter activation using scope setup in model
     @activations = Activation.company(@company.id)
     @activations = @activations.start_time(date_from..date_to)
-    @activations = @activations.activation(params[:activation_type]) unless params[:activation_type].blank?
-    @activations = @activations.allocation(params[:allocator]) unless params[:allocator].blank?
-    @activations = @activations.client(params[:client]) unless params[:client].blank?
+    @activations = @activations.activation(params[:activation_types]) unless params[:activation_types].blank?
+    @activations = @activations.allocation(params[:allocation_users]) unless params[:allocation_users].blank?
+    @activations = @activations.client(params[:project_clients]) unless params[:project_clients].blank?
 
     # Only show activations relevant to contractor if logged in as contractor
     @activations = @activations.joins(:allocations).where(allocations: { user_id: @user.id }) if @user.has_role? :contractor, :any
