07e88c602a8e5d7ff55c04e2f5b42ce175c44ab5
Lim Ning <e0324216@u.nus.edu>
2019-09-19 19:04:53 +0800

    Fix failing rspec

--- app/models/xero_contact.rb
+++ app/models/xero_contact.rb
@@ -1,5 +1,5 @@
 class XeroContact < ApplicationRecord
   belongs_to :company
   validates :name, presence: true
-  validates :contact_id, uniqueness: true
+  validates :contact_id, presence: true, uniqueness: true
 end
