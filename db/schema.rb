22c94045b1c179105b959b5f4d1a01c734928d28
Lim Ning <e0324216@u.nus.edu>
2019-10-02 17:11:20 +0800

    Create migration file for pdf_converted_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_081722) do
+ActiveRecord::Schema.define(version: 2019_10_02_090613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -529,6 +529,15 @@ ActiveRecord::Schema.define(version: 2019_10_01_081722) do
     t.index ["workflowable_type", "workflowable_id"], name: "index_workflows_on_workflowable_type_and_workflowable_id"
   end
 
+  create_table "xero_contacts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "contact_id"
+    t.bigint "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_xero_contacts_on_company_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "allocations", "events"
   add_foreign_key "allocations", "users"
@@ -590,4 +599,5 @@ ActiveRecord::Schema.define(version: 2019_10_01_081722) do
   add_foreign_key "workflows", "templates"
   add_foreign_key "workflows", "users"
   add_foreign_key "workflows", "workflow_actions"
+  add_foreign_key "xero_contacts", "companies"
 end
