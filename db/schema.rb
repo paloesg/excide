a85ffd7ed3bbf47e7ee20017d1402e2615105cbc
Han-Sheng Chin <hschin@gmail.com>
2019-10-14 11:30:43 +0800

    Fix schema for test

--- db/schema.rb
+++ db/schema.rb
@@ -593,5 +593,4 @@ ActiveRecord::Schema.define(version: 2019_10_02_090613) do
   add_foreign_key "workflows", "templates"
   add_foreign_key "workflows", "users"
   add_foreign_key "workflows", "workflow_actions"
-  add_foreign_key "xero_contacts", "companies"
 end
