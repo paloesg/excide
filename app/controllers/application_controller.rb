c70f170f6bbad7414cf2443839d4a7154e2ef8e9
Han-Sheng Chin <hschin@gmail.com>
2019-10-16 14:26:55 +0800

    Handle 401 unauthorized errors separately

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::Base
   #If record is not found on xero, it will return flash message as string
   rescue_from Xeroizer::RecordInvalid, URI::InvalidURIError, ArgumentError, with: :xero_error
   #Error occurs for eg, the tax rate doesn't match with account code. Xero returns an exception in XML, hence the need to parse it truncate it in the xero_error_api_exception method
-  rescue_from Xeroizer::ApiException, OAuth::Unauthorized, with: :xero_error_api_exception
+  rescue_from Xeroizer::ApiException, with: :xero_error_api_exception
+  rescue_from OAuth::Unauthorized, with: :xero_unauthorized
 
   after_action :store_location
 
@@ -56,5 +57,11 @@ class ApplicationController < ActionController::Base
     message = 'Xero returned an error: ' + e.parsed_xml.text.to_s.truncate(200) + '. Please ensure you have filled in all the required data in the right format.'
     Rails.logger.error("Xero Error: #{message}")
     redirect_to session[:previous_url], alert: message
+  end
+
+  def xero_unauthorized
+    message = 'You are not authorized to access the Xero account this company is connected to. Please disconnect the Xero account to continue.'
+    Rails.logger.error("Xero Error: 401 Unauthorized")
+    redirect_to edit_company_path, alert: message
   end
 end
