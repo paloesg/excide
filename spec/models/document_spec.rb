c70abeec3c4d0cf5aa2b00039c78df1689aa32c3
Han-Sheng Chin <hschin@gmail.com>
2019-10-16 13:16:42 +0800

    Remove uniqeness spec

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe Document, type: :model do
   it { should validate_presence_of(:file_url) }
   it { should validate_presence_of(:filename) }
 
-  it { should validate_uniqueness_of(:file_url) }
-
   it { should belong_to(:company) }
   it { should belong_to(:workflow) }
   it { should belong_to(:document_template) }
