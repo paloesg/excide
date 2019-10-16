8dfe967efdfb3c3ee6fc848567033a541de44593
Han-Sheng Chin <hschin@gmail.com>
2019-10-16 13:23:46 +0800

    Update faker syntax in template factory to remove deprecation warning

--- spec/factories/document_templates.rb
+++ spec/factories/document_templates.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :document_template do
     title { Faker::Job.title }
-    file_url { Faker::File.file_name('foo/bar', 'baz', 'doc') }
+    file_url { Faker::File.file_name(dir: 'foo/bar', name: 'baz', ext: 'doc') }
     template
     user
   end
--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :document do
     filename { Faker::File.file_name }
     remarks { Faker::Lorem.sentence }
-    file_url { Faker::File.file_name(dir: 'foo/bar', name: 'baz', ext: 'doc') }
+    file_url { Faker::File.file_name(dir: 'foo/bar', name: 'baz', ext: 'pdf') }
 
     company
     document_template
