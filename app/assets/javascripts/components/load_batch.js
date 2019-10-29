0d8f39a97c0d3c650fdd721bec9bd3ec0ee9deaa
Vicky Prahastra <vicky@excide.co>
2019-10-29 15:14:44 +0800

    Disable start button depend by task role

--- app/assets/javascripts/components/load_batch.js
+++ app/assets/javascripts/components/load_batch.js
@@ -23,8 +23,7 @@ $(document).ready(function(){
 
   if ($("#table-batch").length) {
     $.post("/symphony/batches/load_batch", { id: $("#batch_id").val() } ).done(function(data) {
-      // console.log(data)
-
+      console.log(data)
       $.each(data["sections"], function(index, section) {
         // Card sections
         $("#table-batch").append(
@@ -84,11 +83,8 @@ $(document).ready(function(){
           return new Date (b["created_at"]) - new Date (a["created_at"])
         })
         $.each(sortWorkflowActions, function(index, action) {
-
           let disabledButton = "disabled";
-
-          // Do not disable start button for first task
-          if (index>0) {
+          if (index>0 && data["current_user"]["get_role_ids"].includes(action["task"]["role_id"]) || (index>0 && data["is_admin"])) {
             disabledButton = "";
           }
 
@@ -103,7 +99,6 @@ $(document).ready(function(){
               "<td class='action text-center'><a role='button' class='btn btn-success btn-sm mb-2 "+disabledButton+"' href='/symphony/"+data["batch"]["template"]["slug"]+"/"+workflow["id"]+"/invoices/new?invoice_type=payable&workflow_action_id="+action["id"]+"'>Start</a></td>"
             )
           }
-
           // Do not disable button for next task if previous task is completed
           workflowRow = $("#section-"+action["task"]["section_id"]+" .card-body > table > tbody > #wf_"+workflow["id"]);
           $.each(workflowRow.find("td.action > .completed-task"), function(index, rowAction) {
