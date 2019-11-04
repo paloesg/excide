7472fbe6771590b210e47480f74b9518e7d89b12
Vicky Prahastra <vicky@excide.co>
2019-11-04 17:45:36 +0700

    Create new role page

--- app/controllers/symphony/roles_controller.rb
+++ app/controllers/symphony/roles_controller.rb
@@ -1,13 +1,43 @@
 class Symphony::RolesController < ApplicationController
   layout "dashboard/application"
   before_action :set_company
+  before_action :set_role, only: [:edit]
 
   def index
-    @roles = Role.all
+    @roles = Role.where(resource_id: @company.id, resource_type: "Company")
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.find_or_create_by(role_params)
+    if @role.save
+      redirect_to symphony_roles_path, notice: 'Role successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_role
+    @role = Role.find(params[:id])
   end
 
   def set_company
     @user = current_user
     @company = @user.company
+  end
+
+  def role_params
+    params.require(:role).permit(:name, :resource_id, :resource_type)
   end
 end
