dbc9b7da39a23e253b568e6f0bb78c0252c20650
frediansimanjuntak <fredian.pc@gmail.com>
2019-11-08 16:02:09 +0700

    Fix strip js format

--- app/assets/javascripts/components/stripe.js
+++ app/assets/javascripts/components/stripe.js
@@ -1,48 +1,49 @@
 $(document).on("turbolinks:load", function() {
-  var stripe = Stripe('pk_test_QSqR9niHI76R9dmfZETPjEmE00F3Hm3VEy');
-  var elements = stripe.elements();
+  if ($('#card-element').length) {
+    var stripe = Stripe('pk_test_QSqR9niHI76R9dmfZETPjEmE00F3Hm3VEy');
+    var elements = stripe.elements();
 
-  var card = elements.create('card');
-  card.mount('#card-element');
-
-  card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
-    if (event.error) {
-      displayError.setAttribute('class', 'alert alert-danger');
-      displayError.textContent = event.error.message;
-    } else {
-      displayError.setAttribute('class', '');
-      displayError.textContent = '';
-    }
-  });
-
-  var form = document.getElementById('edit_user');
-  
-  form.addEventListener('submit', function(event) {
-    stripe.createToken(card).then(function(result) {
-      if (result.error) {
-        // Inform the user if there was an error
-        var errorElement = document.getElementById('card-errors');
-        errorElement.setAttribute('class', 'alert alert-danger');
-        errorElement.textContent = result.error.message;
-        return false;
-      } else {
-        // Send the token to your server
-        stripeTokenHandler(result.token);
+    var card = elements.create('card');
+    card.mount('#card-element');
+    
+    card.addEventListener('change', function(event) {
+      // Check input( $( this ).val() ) for validity here
+      var displayError = $('#card-errors');
+      if(event.error) {
+        displayError.attr('class', 'alert alert-danger')    
+        displayError.text(event.error.message);
+      }
+      else {
+        displayError.attr('class', '')    
+        displayError.text('');
       }
     });
-  });
-  
-  function stripeTokenHandler(token) {
-    // Insert the token ID into the form so it gets submitted to the server
-    var form = document.getElementById('edit_user');
-    var hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'user[stripe_card_token]');
-    hiddenInput.setAttribute('value', token.id);
-    form.appendChild(hiddenInput);
-
-    // Submit the form
-    form.submit();
+    $("form#edit_user").submit(function(event){
+      event.preventDefault()
+      stripe.createToken(card).then(function(result) {
+        if (result.error) {
+          var displayError = $('#card-errors');
+          displayError.attr('class', 'alert alert-danger')    
+          displayError.text(result.error.message);
+          var submit_button = $('.additional-information-submit')
+          submit_button.removeAttr('disabled');
+        } else {
+          stripeTokenHandler(result.token);
+        }
+      })
+    });
   }
 });
+  
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('edit_user');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'user[stripe_card_token]');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
