3798b90270a32f011de18c16f63320c247dc6123
frediansimanjuntak <fredian.pc@gmail.com>
2019-11-12 16:48:30 +0700

    Fix edit user parameter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,12 +56,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
     if(params[:user][:stripe_card_token])
       customer = Stripe::Customer.create({email: current_user.email, card: params[:user][:stripe_card_token]})
       resource.stripe_customer_id = customer.id
     end
-    resource.save
-    super
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
   end
 
   # DELETE /resource
@@ -93,6 +107,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :contact_number])
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :stripe_card_token, :stripe_customer_id)
   end
 
   # The path used after sign up.
