4f7e9b03548c80d708b3b8026c5d8ba381bff64a
frediansimanjuntak <fredian.pc@gmail.com>
2019-11-26 17:33:28 +0700

    Add asset metronic for admin template

---
+++ app/assets/javascripts/metronic/application-footer.js
@@ -0,0 +1,5 @@
+
+//= require metronic/plugins.bundle
+//= require metronic/scripts.bundle
+//= require metronic/pages/dashboard
+//= require metronic/pages/crud/forms/widgets/select2
---
+++ app/assets/javascripts/metronic/application.js
@@ -0,0 +1,19 @@
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require moment
+//= require tempusdominus-bootstrap-4.js
+
+//= require trix
+//= require cocoon
+//= require analytics
+//= require init
+//= require jquery.ui.widget
+//= require z.jquery.fileupload
+//= require algolia/v3/algoliasearch.min
+//= require selectize
+
+//= require turbolinks
+//= require turbolinks-compatibility
