43fe11c8a94d432894b27bd10127f731f792b920
frediansimanjuntak <fredian.pc@gmail.com>
2019-11-27 10:07:44 +0700

    Fix footer and header

--- app/assets/javascripts/metronic/application.js
+++ app/assets/javascripts/metronic/application.js
@@ -1,6 +1,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require_tree
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require moment
@@ -14,6 +15,28 @@
 //= require z.jquery.fileupload
 //= require algolia/v3/algoliasearch.min
 //= require selectize
+
+//= require symphony
+//= require symphony/new_workflow
+//= require components/date_time_picker
+//= require symphony/assign_ui
+//= require symphony/remarks
+//= require symphony/popover
+//= require symphony/batches/select_all_by_task
+//= require symphony/zoom
+//= require symphony/invoices/xero
+//= require conductor/create_allocations
+//= require conductor/add_availabilities
+//= require conductor/home
+
+//= require dropzone
+//= require components/multiple_uploads
+//= require components/multiple_uploads_and_edit
+//= require components/popover_initialize
+//= require components/selectize_initialize
+//= require components/document_popover
+//= require components/load_batch
+//= require components/stripe
 
 //= require turbolinks
 //= require turbolinks-compatibility
