010e769a42fa415e7e6799f146f3bf87ab9e9c3b
LimNing <47408304+LimNing@users.noreply.github.com>
2019-12-18 15:05:45 +0800

    Update 20190919070839_rename_column_event_owner.rb

--- db/migrate/20190919070839_rename_column_event_owner.rb
+++ db/migrate/20190919070839_rename_column_event_owner.rb
@@ -1,5 +1,5 @@
 class RenameColumnEventOwner < ActiveRecord::Migration[5.2]
-   def change
-     rename_column :activations, :event_owner_id, :staffer_id
-   end
+  def change
+    rename_column :activations, :event_owner_id, :staffer_id
+  end
 end
