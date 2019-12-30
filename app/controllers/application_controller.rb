f006e8aa02e29109834786001b8c001f614ca2a8
Han-Sheng Chin <hschin@gmail.com>
2019-12-30 18:28:41 +0800

    Add back exception option for protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
+  protect_from_forgery prepend: true, with: :exception
 
   include Adapter
   include Pundit
