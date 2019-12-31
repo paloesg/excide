3f95e7d3c738ce62a91636ea5115e10f02e3b742
Jonathanlau92 <jonathan@excide.co>
2019-12-31 10:36:52 +0800

    Add past_invoice column to store invoice when user cancels subscription

--- app/services/downgrade_subscription.rb
+++ app/services/downgrade_subscription.rb
@@ -5,6 +5,8 @@ class DowngradeSubscription
 
   def run
     Stripe::Subscription.delete(@company.stripe_subscription_plan_data["id"])
+    # append stripe invoice data to past invoice when subscription is cancelled
+    @company.past_invoice << @company.stripe_invoice_data
     @company.downgrade
     @company.save
   end
