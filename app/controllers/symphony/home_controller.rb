4498ba2d41f66cdea9f65bdd50be722d1d6769fb
Lim Ning <e0324216@u.nus.edu>
2020-01-02 11:00:37 +0800

    Change relation to through: sections and simplify join code

--- app/controllers/symphony/home_controller.rb
+++ app/controllers/symphony/home_controller.rb
@@ -5,7 +5,7 @@ class Symphony::HomeController < ApplicationController
 
   def index
     @templates = policy_scope(Template).assigned_templates(current_user)
-    @template_pro = @templates.joins(:sections => :tasks).where(tasks: {task_type: ["create_invoice_payable", "xero_send_invoice", "create_invoice_receivable", "coding_invoice"]})
+    @template_pro = @templates.joins(sections: :tasks).where(tasks: {task_type: ["create_invoice_payable", "xero_send_invoice", "create_invoice_receivable", "coding_invoice"]})
     @workflow_count = policy_scope(Workflow).where(template: @templates).count
     @outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where.not(deadline: nil).where(company: @company).order(:deadline).includes(:task).count
     @batch_count = policy_scope(Batch).count
