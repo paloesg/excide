46a5f29f7fa5d6f9d7a93ed744a9e1a41937d2a6
Vicky Prahastra <vicky@excide.co>
2020-01-07 10:05:19 +0700

    Call stack js files directly

--- app/javascript/packs/home/application.js
+++ app/javascript/packs/home/application.js
@@ -17,5 +17,24 @@ require("turbolinks").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true))
 
+require("jquery")
+
+import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap';
-import '../../stack/application.js';
+import 'dropzone/dist/dropzone';
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+window.moment = require("moment")
+window.Dropzone = require("dropzone")
+window.mixpanel = require("mixpanel-browser")
+window.Turbolinks = require("turbolinks")
+
+require("selectize")
+require("algoliasearch")
+require("tempusdominus-bootstrap-4")
+
+require("../components/stripe");
+require("../components/turbolinks-compatibility")
