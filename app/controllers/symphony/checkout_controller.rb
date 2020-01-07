44015e533d7a833d2eeeeaf5656f69a9a87b6e1e
Jonathanlau92 <jonathan@excide.co>
2020-01-07 10:34:09 +0800

    Combine past_invoices and current_invoices json into 1 key, and rename service object for downgrade

--- app/controllers/symphony/checkout_controller.rb
+++ app/controllers/symphony/checkout_controller.rb
@@ -38,7 +38,7 @@ class Symphony::CheckoutController < ApplicationController
 
   def cancel
     authorize :checkout, :cancel?
-    DowngradeSubscription.new(current_user.company).run
+    DowngradeSubscriptionService.new(current_user.company).run
     redirect_to symphony_root_path, alert: 'You have cancelled your subscription. Re-subscribe to PRO for more Symphony advanced features.'
   end
 end
--- app/services/downgrade_subscription_service.rb
+++ app/services/downgrade_subscription_service.rb
@@ -1,11 +1,12 @@
-class DowngradeSubscription
+class DowngradeSubscriptionService
   def initialize(company)
     @company = company
   end
 
   def run
-    append_past_invoices
     delete_stripe_subscription
+    downgrade_company
+    update_company_when_downgraded
   end
 
   private
@@ -13,11 +14,11 @@ class DowngradeSubscription
     Stripe::Subscription.delete(@company.stripe_subscription_plan_data["subscription"]["id"])
   end
 
-  def append_past_invoices
-    # append stripe invoice data to past invoice when subscription is cancelled
-    @company.stripe_subscription_plan_data["past_invoices"] << @company.stripe_subscription_plan_data["current_invoice"]
-    @company.stripe_subscription_plan_data["current_invoice"] = nil
+  def downgrade_company
     @company.downgrade
+  end
+
+  def update_company_when_downgraded
     # Disconnect from xero when downgraded
     @company.update_attributes(expires_at: nil, access_key: nil, access_secret: nil, session_handle: nil, xero_organisation_name: nil)
     @company.save
