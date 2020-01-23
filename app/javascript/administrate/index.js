bb235db561ab03672e32dcba7e899c3979eb42ac
frediansimanjuntak <fredian@excide.co>
2020-01-23 14:49:45 +0700

    Remove javascript from assets and move to javascript webpacker

---
+++ app/javascript/administrate/index.js
@@ -0,0 +1,12 @@
+window.moment = require("moment");
+require("selectize");
+require("algoliasearch");
+require("tempusdominus-bootstrap-4");
+require("blueimp-file-upload");
+
+require("./components/_search");
+require("./components/date_time_picker");
+require("./components/has_many_form");
+require("./components/image_upload");
+require("./components/selectize_initialize");
+require("./components/table");
