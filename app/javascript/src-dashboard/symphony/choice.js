5c2b6ea46721e153b2c6d0c0e559ee5d625e0813
Jonathanlau92 <jonathan@excide.co>
2020-01-24 11:53:23 +0800

    Use cocoon to add choices

--- app/javascript/src-dashboard/symphony/choice.js
+++ app/javascript/src-dashboard/symphony/choice.js
@@ -1,17 +1,24 @@
+$(document).on("cocoon:after-insert", function(e, addedItem) {
+  $('select.question-choice').on('change', function() {
+    if ($(addedItem).find('.question-choice').val() != 'multiple'){
+      $(this).closest('div').next().addClass('kt-hide');
+    }
+    else{
+      $(this).closest('div').next().removeClass('kt-hide');
+    }
+  });
+});
 $(document).on("turbolinks:load", function() {
   $('select.question-choice').on('change', function() {
     selectedValue = $(this).val();
-    multipleChoiceDetails = $(this).closest('.multiple-choice-field');
+    multipleChoiceDetails = $(this).closest('#multiple-choice-field');
     console.log("SELECTEDVALUE: ", selectedValue);
-    console.log("THIS: ", $(this).closest('td').closest('tr').next()[0]);
+    console.log("THIS: ", $(this).closest('div').next()[0]);
     if(selectedValue != 'multiple') {
-      $(this).closest('td').closest('tr').next().addClass('kt-hide');
+      $(this).closest('div').next().addClass('kt-hide');
     }
     else{
-      $(this).closest('td').closest('tr').next().removeClass('kt-hide');
+      $(this).closest('div').next().removeClass('kt-hide');
     }
-    // if(selectedValue == TRUE) { guarantorDetails.hide(); } 
-    // else { guarantorDetails.show(); }
-
   });
 });
