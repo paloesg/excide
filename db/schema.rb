21cf6d8c0d0bf9cc26a4138c070ed85eee7838b2
Han-Sheng Chin <hschin@gmail.com>
2020-01-30 16:03:48 +0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_042558) do
+ActiveRecord::Schema.define(version: 2020_01_23_101023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
