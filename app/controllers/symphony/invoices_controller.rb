630c926f3e9e3ee773188868cab69dfdb8e293bb
Vicky Prahastra <vicky@excide.co>
2020-02-03 11:22:59 +0700

    Display tracking category form database

--- app/controllers/symphony/invoices_controller.rb
+++ app/controllers/symphony/invoices_controller.rb
@@ -338,9 +338,9 @@ class Symphony::InvoicesController < ApplicationController
     @full_account_code      = @xero.get_accounts.map{|account| (account.code + ' - ' + account.name) if account.code.present?} #would not display account if account.code is missing
     @full_tax_code          = @xero.get_tax_rates.map.map{|t| [t.name + ' (' + t.display_tax_rate.to_s + '%) - ' + t.tax_type, {'data-rate': "#{t.display_tax_rate}"}] } # Combine tax codes and tax names as a string
     @currencies             = @xero.get_currencies
-    @tracking_name          = @xero.get_tracking_options
-    @tracking_categories_1  = @tracking_name[0]&.options&.map{|option| option}
-    @tracking_categories_2  = @tracking_name[1]&.options&.map{|option| option}
+    @tracking_name          = current_user.company.xero_tracking_category
+    @tracking_categories_1  = @tracking_name[0]&.options&.map{|option| JSON.parse(option)}
+    @tracking_categories_2  = @tracking_name[1]&.options&.map{|option| JSON.parse(option)}
     @items                  = @company.xero_line_items.map{|item| (item.item_code + ': ' + (item.description || '-')) if item.item_code.present?}
   end
 
