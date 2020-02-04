71303fa81b3f0a525519351a4f3b6b7cd3584e46
Jonathanlau92 <jonathan@excide.co>
2020-02-04 15:41:31 +0800

    Send stripe email asynchronously

--- app/services/stripe/event_handler.rb
+++ app/services/stripe/event_handler.rb
@@ -46,15 +46,13 @@ module Stripe
       period_start = subscription["current_period_start"]
       period_end = subscription["current_period_end"]
       invoice_pdf = event.data.object["invoice_pdf"]
-      StripeNotificationMailer.payment_successful(@current_user, period_start, period_end, invoice_pdf).deliver
+      StripeNotificationMailer.payment_successful(@current_user, period_start, period_end, invoice_pdf).deliver_later
     end
 
     def handle_charge_failed(event)
-      @current_user = User.find(170)
-
-      # @current_user = User.find_by(stripe_customer_id: event.data.object.customer)
+      @current_user = User.find_by(stripe_customer_id: event.data.object.customer)
       # Stripe creates a charge in the backend automatically even when it is one time checkout session.
-      StripeNotificationMailer.charge_failed(@current_user).deliver
+      StripeNotificationMailer.charge_failed(@current_user).deliver_later
     end
   end
 end
