ef3be7d54f74f75f6a93f53fc34e9941cb7f875c
Jonathanlau92 <jonathan@excide.co>
2020-02-04 16:26:13 +0800

    Add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -182,6 +182,7 @@ class User < ApplicationRecord
     self.roles.pluck(:name).include? role.name
   end
 
+  # In administrate, the json is updated to string in the database. This code is to check that if setting is a string, it will parse it to a JSON.
   def settings=(value)
     self[:settings] = value.is_a?(String) ? JSON.parse(value) : value
   end
