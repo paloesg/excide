d26f622145e875148815f87993f77631af4576f8
Jonathanlau92 <jonathan@excide.co>
2020-02-11 20:14:02 +0800

    Delete responses when deleting questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :survey_section
   acts_as_list scope: :survey_section
 
-  has_many :responses
+  has_many :responses, dependent: :destroy
 
   has_and_belongs_to_many :choices
 
