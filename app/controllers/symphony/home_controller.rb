e2d3b0778b0b7905c79038483ce094c70b3daaed
Han-Sheng Chin <hschin@gmail.com>
2020-03-03 11:38:03 +0800

    Update dashboard view

--- app/controllers/symphony/home_controller.rb
+++ app/controllers/symphony/home_controller.rb
@@ -6,11 +6,34 @@ class Symphony::HomeController < ApplicationController
   def index
     @templates = policy_scope(Template).assigned_templates(current_user)
     @template_pro = @templates.joins(sections: :tasks).where(tasks: {task_type: ["create_invoice_payable", "xero_send_invoice", "create_invoice_receivable", "coding_invoice"]})
-    @workflow_count = policy_scope(Workflow).where(template: @templates).count
-    @outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where.not(deadline: nil).where(company: @company).order(:deadline).includes(:task).count
+
+    @workflows_array = policy_scope(Workflow).includes(:template, :workflowable).where(template: @templates)
+    workflows_sort = sort_column(@workflows_array)
+    params[:direction] == "desc" ? workflows_sort.reverse! : workflows_sort
+    if params[:workflow_type].blank?
+      templates_type = workflows_sort
+    else
+      templates_type = workflows_sort.select{ |t| t.template.slug == params[:workflow_type] }
+    end
+    @workflows = Kaminari.paginate_array(templates_type).page(params[:page]).per(10)
+
+    @outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where.not(deadline: nil).where(company: @company).order(:deadline).includes(:task)
+
     @batch_count = policy_scope(Batch).count
     @reminder_count = current_user.reminders.where(company: current_user.company).count
     @recurring_count = current_user.company.recurring_workflows.all.count
     @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", allow_any: ['utf8', 'authenticity_token'], success_action_status: '201', acl: 'public-read')
+  end
+
+  private
+
+  def sort_column(array)
+    array.sort_by{
+      |item| if params[:sort] == "template" then item.template.title.upcase
+      elsif params[:sort] == "deadline" then item.deadline ? item.deadline : Time.at(0)
+      elsif params[:sort] == "completed" then item.completed ? 'Completed' : item.current_section&.section_name
+      elsif params[:sort] == "id" then item.id ? item.id.upcase : ""
+      end
+    }
   end
 end
