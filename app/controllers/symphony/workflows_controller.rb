c0caf604f8b91299609d33941b52063aa4d36764
Jonathanlau92 <jonathan@excide.co>
2020-03-13 11:36:40 +0800

    Send task notification when clicked 'Send Reminder'

--- app/controllers/symphony/workflows_controller.rb
+++ app/controllers/symphony/workflows_controller.rb
@@ -177,6 +177,7 @@ class Symphony::WorkflowsController < ApplicationController
       if current_task.role.present?
         users = User.with_role(current_task.role.name.to_sym, @company)
         users.each do |user|
+          current_action.notify :users, key: "workflow_action.task_notify", parameters: { printable_notifiable_name: "#{current_action.task.instructions}", workflow_action_id: current_action.id }, send_later: false
           SlackService.new.task_notification(current_task, current_action, user).deliver if (user.settings[0]&.reminder_slack == 'true' && @company.basic? == 'false')
           if @company.basic? == 'false'
             to_number = '+65' + user.contact_number
