903be2e71ece263349c6df7186123022cda14810
Jonathanlau92 <jonathan@excide.co>
2020-03-26 13:16:18 +0800

    Add stack theme css to webpacker

---
+++ app/webpacker/packs/home/footer.js
@@ -0,0 +1,18 @@
+var jQueryBridget = require('jquery-bridget');
+var Flickity = require('flickity');
+
+jQueryBridget( 'flickity', Flickity, $ );
+
+require("../../src/javascripts/stack/js/easypiechart.min");
+require("../../src/javascripts/stack/js/parallax");
+require("../../src/javascripts/stack/js/typed.min");
+require("../../src/javascripts/stack/js/datepicker");
+require("../../src/javascripts/stack/js/isotope.min");
+require("../../src/javascripts/stack/js/ytplayer.min");
+require("../../src/javascripts/stack/js/granim.min");
+require("../../src/javascripts/stack/js/jquery.steps.min");
+require("../../src/javascripts/stack/js/countdown.min");
+require("../../src/javascripts/stack/js/twitterfetcher.min");
+require("../../src/javascripts/stack/js/spectragram.min");
+require("../../src/javascripts/stack/js/smooth-scroll.min");
+require("../../src/javascripts/stack/js/scripts");
