b4eb26c7514dea60f42bbae729683c4a4de75bf3
Jonathanlau92 <jonathan@excide.co>
2020-04-29 20:38:04 +0800

    [WIP] Upload mail attachments to active storage

--- app/mailboxes/replies_mailbox.rb
+++ app/mailboxes/replies_mailbox.rb
@@ -3,8 +3,28 @@ class RepliesMailbox < ApplicationMailbox
 
   def process
     return unless @user
-    @user.remarks = mail.decoded
-    @user.save
+    # puts "mail attahcments: #{mail.attachments}"
+    # if mail.attachments.empty?
+    #   puts "mail attahcments empty: #{mail.attachments}"
+    # else
+    #   puts "mail attahcments: #{mail.attachments}"
+    #   mail.attachments.each do |attachment|
+    #     # Get back the latest 
+    #     puts 'echo starts'
+    #     Document.create(filename: attachment.filename, file_url: "w.e.d", user: @user, company: @user.company)
+    #     puts 'echo ends'
+    #     # ActiveStorage::Attachment.last.blob.service_url.sub(/\?.*/, '')
+    #   end
+    # end
+    mail.attachments.each do |attachment|
+      blob = ActiveStorage::Blob.create_after_upload!(
+        io: StringIO.new(attachment.body.to_s),
+        filename: attachment.filename,
+        content_type: attachment.content_type
+      )
+      # Document.create(filename: attachment.filename, file_url: )
+    end
+    # puts "mailcatcher mailbox: #{mail}"
   end
 
   private
