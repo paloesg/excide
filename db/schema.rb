84691d1a12482963402ae7b492efe182fafc7926
Han-Sheng Chin <hschin@gmail.com>
2020-05-26 00:33:22 +0800

    Add column for time spent in mins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_120329) do
+ActiveRecord::Schema.define(version: 2020_05_25_162416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -569,6 +569,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_120329) do
     t.integer "completed_user_id"
     t.text "remarks"
     t.uuid "workflow_id"
+    t.integer "time_spent_mins"
     t.index ["assigned_user_id"], name: "index_workflow_actions_on_assigned_user_id"
     t.index ["company_id"], name: "index_workflow_actions_on_company_id"
     t.index ["completed_user_id"], name: "index_workflow_actions_on_completed_user_id"
