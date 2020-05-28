023240e824bc6ae0aa2eee51525262a2bfa7981d
Han-Sheng Chin <hschin@gmail.com>
2020-05-28 16:08:08 +0800

    Show user in list as long as they have any role that belongs to company

--- app/controllers/symphony/users_controller.rb
+++ app/controllers/symphony/users_controller.rb
@@ -8,7 +8,7 @@ class Symphony::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :change_company, :notification_settings, :update_notification]
 
   def index
-    @users = User.where(company: @company).order(:id).includes(:roles)
+    @users = User.joins(:roles).where(:roles => {resource_id: @company.id}).order(:id).uniq
   end
 
   def show
