b8039a3dbdcedcfbc13ecab1ae5b5b6e3ac860fa
Jonathanlau92 <jonathan@excide.co>
2020-06-02 15:41:41 +0800

    View choices when selected val is multiple or single on page loading

--- app/webpacker/src/javascripts/dashboard/symphony/choice.js
+++ app/webpacker/src/javascripts/dashboard/symphony/choice.js
@@ -1,4 +1,10 @@
 $(document).on("turbolinks:load", function() {
+  // Loop through all the question type and reveal the choices if selected option is multiple or single
+  $('select.question-choice').each(function(){
+    if(($(this).children("option:selected").val() == 'multiple') || ($(this).children("option:selected").val() == 'single')){
+      $('div#multiple-choice-field').removeClass('kt-hide');
+    }
+  })
   $('select.question-choice').on('change', function() {
     var selectedValue = $(this).val();
     var multipleChoiceDetails = $(this).closest('#multiple-choice-field');
@@ -11,7 +17,7 @@ $(document).on("turbolinks:load", function() {
   });
   $(".questions").on("cocoon:after-insert", function(e, addedItem) {
     $('select.question-choice').on('change', function() {
-      // Check if the added item value is undefined, only run the function if its defined. This is to prevent the code from crashing when we add choices instead of questions.
+      // Only run the function if the added item value is defined. This is to prevent the code from crashing when we add choices instead of questions.
       if ( ($(addedItem).find('.question-choice').val()) ){
         if ($(addedItem).find('.question-choice').val() === 'multiple' || $(addedItem).find('.question-choice').val() === 'single'){
           $(this).closest('div').next().removeClass('kt-hide');
