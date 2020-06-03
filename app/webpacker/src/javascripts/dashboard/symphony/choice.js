0be842b0ccfaa2e86ed78f8585ad170620a64a5d
Jonathanlau92 <jonathan@excide.co>
2020-06-03 13:25:50 +0800

    Modify JS to show choices when qn type is multiple or single

--- app/webpacker/src/javascripts/dashboard/symphony/choice.js
+++ app/webpacker/src/javascripts/dashboard/symphony/choice.js
@@ -1,10 +1,13 @@
 $(document).on("turbolinks:load", function() {
-  // Loop through all the question type and reveal the choices if selected option is multiple or single
+  // Show all the choices after cocoon save when the select value is single or multiple
+  // Loop through all the question choice and then remove kt-hide for #multiple-choice-field
   $('select.question-choice').each(function(){
     if(($(this).children("option:selected").val() == 'multiple') || ($(this).children("option:selected").val() == 'single')){
-      $('div#multiple-choice-field').removeClass('kt-hide');
+      $(this).closest('div').next().removeClass('kt-hide');
     }
   })
+
+  // This is when the user edits the question type on an existing survey template. If the user selects multiple or single in the dropdown, the choice button will appear upon chosen. 
   $('select.question-choice').on('change', function() {
     var selectedValue = $(this).val();
     var multipleChoiceDetails = $(this).closest('#multiple-choice-field');
@@ -15,9 +18,10 @@ $(document).on("turbolinks:load", function() {
       $(this).closest('div').next().addClass('kt-hide');
     }
   });
+
+  // This is when the user haven't save the survey template and added a new question through cocoon. Only run the function if the added item value is defined. This is to prevent the code from crashing when we add choices instead of questions.
   $(".questions").on("cocoon:after-insert", function(e, addedItem) {
     $('select.question-choice').on('change', function() {
-      // Only run the function if the added item value is defined. This is to prevent the code from crashing when we add choices instead of questions.
       if ( ($(addedItem).find('.question-choice').val()) ){
         if ($(addedItem).find('.question-choice').val() === 'multiple' || $(addedItem).find('.question-choice').val() === 'single'){
           $(this).closest('div').next().removeClass('kt-hide');
