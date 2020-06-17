34d7c2980f3d9120e69f1155037d4e29679563b8
Jonathanlau92 <jonathan@excide.co>
2020-06-17 15:12:35 +0800

    Fix codacy issues

--- app/webpacker/src/javascripts/dashboard/symphony/assign_ui.js
+++ app/webpacker/src/javascripts/dashboard/symphony/assign_ui.js
@@ -8,13 +8,12 @@ window.assign = function(form_element) {
     data: formJquery,
     dataType: 'JSON'
   }).done(function (data) {
-      console.log("SUCCESS");
       $(tableRow.find(".fa-check")).show().fadeTo(500, 200, function () {
         $(tableRow.find(".fa-check")).fadeTo(200, 0);
       });
   }).fail(function (data) {
       $(tableRow.find(".fa-times")).show().fadeTo(500, 200, function () {
         $(tableRow.find(".fa-times")).fadeTo(200, 0);
-      })
+      });
   });
-}
+};
