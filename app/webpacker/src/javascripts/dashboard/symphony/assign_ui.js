5165465c4b426dba7d4fbdca5d181a3b19f4f0c0
Jonathanlau92 <jonathan@excide.co>
2020-06-17 14:36:00 +0800

    Modify ajax code for assigning task to work

--- app/webpacker/src/javascripts/dashboard/symphony/assign_ui.js
+++ app/webpacker/src/javascripts/dashboard/symphony/assign_ui.js
@@ -1,18 +1,20 @@
-$(document).on("turbolinks:load", function() {
-  $('.selectize-user-assignment').on('change', function() {
-    console.log("IM HERE BABYYY", $(this).parent()); 
-    let formJquery = $(this).parent();
-    let formParent = formJquery.parent();
-    let tableRow = formParent.parent().parent();
-    formJquery.submit();
-    $(formParent).on('ajax:success', function () {
+window.assign = function(form_element) {
+  let formJquery = $(form_element);
+  let formParent = formJquery.parent();
+  let tableRow = formParent.parent().parent();
+  $.ajax({
+    type: 'PATCH',
+    url: '/workflow_actions/update/' + formJquery.data('action-id'),
+    data: formJquery,
+    dataType: 'JSON'
+  }).done(function (data) {
+      console.log("SUCCESS");
       $(tableRow.find(".fa-check")).show().fadeTo(500, 200, function () {
         $(tableRow.find(".fa-check")).fadeTo(200, 0);
       });
-    }).on('ajax:error', function () {
+  }).fail(function (data) {
       $(tableRow.find(".fa-times")).show().fadeTo(500, 200, function () {
         $(tableRow.find(".fa-times")).fadeTo(200, 0);
-      });
-    });
+      })
   });
-});
+}
--- app/webpacker/src/javascripts/dashboard/symphony/remarks.js
+++ app/webpacker/src/javascripts/dashboard/symphony/remarks.js
@@ -1,6 +1,5 @@
 $(document).on("turbolinks:load", function() {
   let form = $( ".edit_workflow_action, .multiple_edit" );
-
   form.on("ajax:success", function(event) {
     const [data, status, xhr] = event.detail;
     $("#check-" + data['workflow_action'].id).show().fadeTo(800, 200, function(){
