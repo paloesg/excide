7f1fe79a58d809fe7ba705192374c15c015fd499
Jonathanlau92 <jonathan@excide.co>
2020-06-17 14:49:23 +0800

    Find correct ID for remarks

--- app/webpacker/src/javascripts/dashboard/symphony/remarks.js
+++ app/webpacker/src/javascripts/dashboard/symphony/remarks.js
@@ -2,8 +2,8 @@ $(document).on("turbolinks:load", function() {
   let form = $( ".edit_workflow_action, .multiple_edit" );
   form.on("ajax:success", function(event) {
     const [data, status, xhr] = event.detail;
-    $("#check-" + data['workflow_action'].id).show().fadeTo(800, 200, function(){
-        $("#check-" + data['workflow_action'].id).fadeTo(200, 0);
+    $("#check-" + data.id).show().fadeTo(800, 200, function(){
+        $("#check-" + data.id).fadeTo(200, 0);
     });
   });
 });
