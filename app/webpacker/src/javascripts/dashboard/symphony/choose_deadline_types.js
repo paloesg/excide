91797feabdf50fa9a7bb5c65bd6f4c7740b705bc
Jonathanlau92 <jonathan@excide.co>
2020-06-23 13:21:49 +0800

    Add deadline fields to template NEW

--- app/webpacker/src/javascripts/dashboard/symphony/choose_deadline_types.js
+++ app/webpacker/src/javascripts/dashboard/symphony/choose_deadline_types.js
@@ -1,22 +1,22 @@
 $(document).on("turbolinks:load", function() {
   // Show the form field on load
-  if($('select#deadlines-type').val() === 'xth_day_of_the_month'){
+  if($('select.deadlines-type').val() === 'xth_day_of_the_month'){
     $('#template_deadline_day').closest('.form-group').removeClass('kt-hide');
     $('#template_days_to_complete').closest('.form-group').addClass('kt-hide');
   }
-  else if($('select#deadlines-type').val() === 'days_to_complete'){
+  else if($('select.deadlines-type').val() === 'days_to_complete'){
     $('#template_days_to_complete').closest('.form-group').removeClass('kt-hide');
     $('#template_deadline_day').closest('.form-group').addClass('kt-hide');
   }
   // Show the form field on change
-  $('select#deadlines-type').on('change', function() {
-    console.log("OPTION VALUE: ", $('select#deadlines-type').val());
-    console.log("days to comple VALUE: ", $('#template_days_to_complete').closest('.form-group'));
-    if($('select#deadlines-type').val() === 'xth_day_of_the_month'){
+  $('select.deadlines-type').on('change', function() {
+    console.log("OPTION VALUE: ", $('select.deadlines-type').val());
+    console.log("days to comple VALUE: ", $('.template_days_to_complete').closest('.form-group'));
+    if($('select.deadlines-type').val() === 'xth_day_of_the_month'){
       $('#template_deadline_day').closest('.form-group').removeClass('kt-hide');
       $('#template_days_to_complete').closest('.form-group').addClass('kt-hide');
     }
-    else if($('select#deadlines-type').val() === 'days_to_complete'){
+    else if($('select.deadlines-type').val() === 'days_to_complete'){
       $('#template_days_to_complete').closest('.form-group').removeClass('kt-hide');
       $('#template_deadline_day').closest('.form-group').addClass('kt-hide');
     }
@@ -24,10 +24,10 @@ $(document).on("turbolinks:load", function() {
   // Show the warning message if date entered are 29, 30 or 31
   $('#template_deadline_day').on('input', function(){
     if ( $('#template_deadline_day').val() === '29' || $('#template_deadline_day').val() === '30' || $('#template_deadline_day').val() === '31' ){
-      $('span#deadline-warning').removeClass('kt-hide');
+      $('span.deadline-warning').removeClass('kt-hide');
     }
     else{
-      $('span#deadline-warning').addClass('kt-hide');
+      $('span.deadline-warning').addClass('kt-hide');
     }
   });
 });
