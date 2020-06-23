1c5f0138dc5174dfaecc69b344055a534f26ca30
Jonathanlau92 <jonathan@excide.co>
2020-06-23 10:15:03 +0800

    Add warning message if date are 29, 30 or 31

--- app/webpacker/src/javascripts/dashboard/symphony/choose_deadline_types.js
+++ app/webpacker/src/javascripts/dashboard/symphony/choose_deadline_types.js
@@ -1,4 +1,5 @@
 $(document).on("turbolinks:load", function() {
+  // Show the form field on load
   if($('select#deadlines-type').val() === 'xth_day_of_the_month'){
     $('#template_deadline_day').closest('.form-group').removeClass('kt-hide');
     $('#template_days_to_complete').closest('.form-group').addClass('kt-hide');
@@ -7,7 +8,7 @@ $(document).on("turbolinks:load", function() {
     $('#template_days_to_complete').closest('.form-group').removeClass('kt-hide');
     $('#template_deadline_day').closest('.form-group').addClass('kt-hide');
   }
-  
+  // Show the form field on change
   $('select#deadlines-type').on('change', function() {
     console.log("OPTION VALUE: ", $('select#deadlines-type').val());
     console.log("days to comple VALUE: ", $('#template_days_to_complete').closest('.form-group'));
@@ -18,6 +19,15 @@ $(document).on("turbolinks:load", function() {
     else if($('select#deadlines-type').val() === 'days_to_complete'){
       $('#template_days_to_complete').closest('.form-group').removeClass('kt-hide');
       $('#template_deadline_day').closest('.form-group').addClass('kt-hide');
+    }
+  });
+  // Show the warning message if date entered are 29, 30 or 31
+  $('#template_deadline_day').on('input', function(){
+    if ( $('#template_deadline_day').val() === '29' || $('#template_deadline_day').val() === '30' || $('#template_deadline_day').val() === '31' ){
+      $('span#deadline-warning').removeClass('kt-hide');
+    }
+    else{
+      $('span#deadline-warning').addClass('kt-hide');
     }
   });
 });
