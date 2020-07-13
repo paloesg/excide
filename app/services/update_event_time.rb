f9fc49343ac397f371525ba7bcdac6d69c19c560
Jonathanlau92 <jonathan@excide.co>
2020-07-13 09:58:30 +0800

    Allow users to edit their own event

--- app/services/update_event_time.rb
+++ app/services/update_event_time.rb
@@ -1,9 +1,10 @@
 class UpdateEventTime
 
-  def initialize(event, new_start_time, new_end_time)
+  def initialize(event, new_start_time, new_end_time, user)
     @event     = event
     @new_start_time = new_start_time
     @new_end_time   = new_end_time
+    @user = user
     @associates_updated = 0
     @associates_unassigned = 0
   end
@@ -14,16 +15,13 @@ class UpdateEventTime
         @event.transaction do
           update_event
           @event.allocations.each do |allocation|
-            old_allocation, allocation = update_allocation(allocation)
-            next if allocation.user.blank?
-            if associate_available?(allocation)
-              notify_associate(allocation)
-            else
-              remove_associate(allocation, old_allocation)
-            end
+            # Update availability
+            update_availability(allocation)
+            # update allocation for the event
+            allocation.user.blank? ? next : update_allocation(allocation)
           end
         end
-        OpenStruct.new(success?: true, event: @event, message: success_message)
+        OpenStruct.new(success?: true, event: @event, message: 'Edited event successfully')
       rescue ActiveRecord::RecordInvalid
         OpenStruct.new(success?: false, event: @event)
       end
@@ -47,33 +45,10 @@ class UpdateEventTime
   end
 
   def update_allocation(allocation)
-    old_allocation = allocation.dup
-    allocation.update_attributes!(allocation_date: @new_start_time, start_time: @new_start_time, end_time: @new_end_time)
-    return old_allocation, allocation
+    allocation.update_attributes!(allocation_date: @new_start_time, start_time: @new_start_time, end_time: @new_end_time, user_id: @user.id)
   end
 
-  def associate_available?(allocation)
-    allocation.user.availabilities.where(available_date: allocation.allocation_date).where("start_time <= ?", @new_start_time).where("end_time >= ?", @new_end_time).present?
-  end
-
-  def notify_associate(allocation)
-    NotificationMailer.edit_event(@event, allocation.user).deliver_later
-    @associates_updated += 1
-  end
-
-  def remove_associate(allocation, old_allocation)
-    removed_user = allocation.user
-    allocation.update_attributes!(user_id: nil)
-    allocation.availability.update_column(:assigned, allocation.availability.allocations.present?)
-    allocation.availability.allocations.delete(allocation)
-    NotificationMailer.user_removed_from_event(@event, removed_user).deliver_later
-    @associates_unassigned += 1
-  end
-
-  def success_message
-    message = ''
-    (message += "#{@associates_updated} associate(s) informed of the new event time. ") if @associates_updated > 0
-    (message += "#{@associates_unassigned} associate(s) unassigned from the event due to the time change. ") if @associates_unassigned > 0
-    return message
+  def update_availability(allocation)
+    allocation.availability.update_attributes(available_date: @new_start_time, user_id: @user.id, start_time: @new_start_time, end_time: @new_end_time)
   end
 end
