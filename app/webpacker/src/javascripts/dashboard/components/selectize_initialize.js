62bb0bafe3ac7b373bf3aaf0e7e229ab2ff3f19a
LimNing <e0324216@u.nus.edu>
2020-07-17 18:35:01 +0800

    add filter created_at and tasks, minor fixes to deadline sort and disabled checkbox

--- app/webpacker/src/javascripts/dashboard/components/selectize_initialize.js
+++ app/webpacker/src/javascripts/dashboard/components/selectize_initialize.js
@@ -9,13 +9,15 @@ $(document).on("turbolinks:load", function() {
     },
     onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
   });
+  
+  $('.tasks-filter-button').click(function (e) {
+    var tasks_selectize = $('select.selectize-tasks').selectize();
+    var created_at_selectize = $('select.selectize-created-at').selectize();
 
-  $('select.selectize-tasks').selectize({
-    allowEmptyOption: true,
-    onItemAdd: function (value, $item) {
-      Turbolinks.visit('//' + location.host + location.pathname + '?tasks=' + value);
-    },
-    onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+    var tasksData = (tasks_selectize[0].selectize).getValue();
+    var createdAtData = (created_at_selectize[0].selectize).getValue();
+
+    Turbolinks.visit('//' + location.host + location.pathname + '?tasks=' + tasksData + '&created_at='+ createdAtData);
   });
 
   if ($('#new-event.d-none')[0] == undefined) {
