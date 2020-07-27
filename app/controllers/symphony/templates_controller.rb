c41534c92cf741b47688e0be6e5bc1853b1f8e5c
Han Sheng Chin <hschin@gmail.com>
2020-07-27 22:01:36 +0800

    Fix multiple redirect error when deleting templates

--- app/controllers/symphony/templates_controller.rb
+++ app/controllers/symphony/templates_controller.rb
@@ -82,9 +82,8 @@ class Symphony::TemplatesController < ApplicationController
   def destroy
     authorize @template
     if @template.destroy
-      redirect_back fallback_location: symphony_templates_path
       respond_to do |format|
-        format.html { redirect_to symphony_templates_path, notice: 'Template was successfully deleted.' }
+        format.html { redirect_back fallback_location: symphony_templates_path, notice: 'Routine was successfully deleted.' }
         format.js  { flash[:notice] = 'Routine was successfully deleted.' }
       end
     end
