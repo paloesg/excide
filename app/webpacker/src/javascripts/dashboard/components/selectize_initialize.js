7684f2e8468e874f4a21e6a0498fb1a552c0394f
Simon <simon_tsh@hotmail.com>
2020-08-04 17:04:51 +0800

    converted task and activity history filter dropdown

--- app/webpacker/src/javascripts/dashboard/components/selectize_initialize.js
+++ app/webpacker/src/javascripts/dashboard/components/selectize_initialize.js
@@ -1,92 +1,129 @@
-$(document).on("turbolinks:before-render", function() {
-  $('select.selectize')[0].selectize.destroy();
-})
-$(document).on("turbolinks:load", function() {
-  $('select.selectize-workflow-type').selectize({
+$(document).on("turbolinks:before-render", function () {
+  $("select.selectize")[0].selectize.destroy();
+});
+$(document).on("turbolinks:load", function () {
+  $("select.selectize-workflow-type").selectize({
     allowEmptyOption: true,
     onItemAdd: function (value, $item) {
-      Turbolinks.visit('//' + location.host + location.pathname + '?workflow_type=' + value);
+      Turbolinks.visit(
+        "//" + location.host + location.pathname + "?workflow_type=" + value
+      );
     },
-    onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+    onFocus: function () {
+      $(".selectize-input input").attr("style", "width: auto;");
+    },
   });
 
-  $('select.selectize-year').selectize({
+  $("select.selectize-year").selectize({
     allowEmptyOption: true,
     onItemAdd: function (value, $item) {
-      Turbolinks.visit('//' + location.host + location.pathname + '?year=' + value);
+      Turbolinks.visit(
+        "//" + location.host + location.pathname + "?year=" + value
+      );
     },
-    onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+    onFocus: function () {
+      $(".selectize-input input").attr("style", "width: auto;");
+    },
   });
 
-  $('.tasks-filter-button').click(function (e) {
-    var tasks_selectize = $('select.selectize-tasks').selectize();
-    var created_at_selectize = $('select.selectize-created-at').selectize();
-    var types_selectize = $('select.selectize-types').selectize();
+  $(".tasks-filter-button").click(function (e) {
+    var tasks_selectize = $("select.select2-tasks").select2();
+    var created_at_selectize = $("select.select2-created-at").select2();
+    var types_selectize = $("select.select2-types").select2();
 
-    var tasksData = (tasks_selectize[0].selectize).getValue();
-    var createdAtData = (created_at_selectize[0].selectize).getValue();
-    var typesData = (types_selectize[0].selectize).getValue();
+    var tasksData = tasks_selectize[0].value;
+    var createdAtData = created_at_selectize[0].value;
+    var typesData = types_selectize[0].value;
 
-    Turbolinks.visit('//' + location.host + location.pathname + '?tasks=' + tasksData + '&created_at=' + createdAtData + '&types=' + typesData);
+    Turbolinks.visit(
+      "//" +
+        location.host +
+        location.pathname +
+        "?tasks=" +
+        tasksData +
+        "&created_at=" +
+        createdAtData +
+        "&types=" +
+        typesData
+    );
   });
 
-  $('.activity-history-filter-button').click(function (e) {
-    var created_at_selectize = $('select.selectize-created-at').selectize();
-    var createdAtData = (created_at_selectize[0].selectize).getValue();
-    Turbolinks.visit('//' + location.host + location.pathname + '?created_at=' + createdAtData);
+  $(".activity-history-filter-button").click(function (e) {
+    var created_at_selectize = $("select.select2-created-at").select2();
+    var createdAtData = created_at_selectize[0].value;
+    Turbolinks.visit(
+      "//" + location.host + location.pathname + "?created_at=" + createdAtData
+    );
   });
 
-  if ($('#new-event.d-none')[0] == undefined) {
-    $('select.selectize').selectize({
-      onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+  if ($("#new-event.d-none")[0] == undefined) {
+    $("select.selectize").selectize({
+      onFocus: function () {
+        $(".selectize-input input").attr("style", "width: auto;");
+      },
     });
 
-    $('select.selectize-user-assignment').selectize();
+    $("select.selectize-user-assignment").selectize();
   }
 
-  $('select.project-clients').selectize({
+  $("select.project-clients").selectize({
     placeholder: "Choose project clients",
-    plugins: ['remove_button'],
+    plugins: ["remove_button"],
     allowEmptyOption: true,
-    onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+    onFocus: function () {
+      $(".selectize-input input").attr("style", "width: auto;");
+    },
   });
 
-  $('select.allocation-users').selectize({
+  $("select.allocation-users").selectize({
     placeholder: "Choose associates",
-    plugins: ['remove_button'],
+    plugins: ["remove_button"],
     allowEmptyOption: true,
-    onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+    onFocus: function () {
+      $(".selectize-input input").attr("style", "width: auto;");
+    },
   });
 
-  $('select.event-type').selectize({
+  $("select.event-type").selectize({
     placeholder: "Choose event type",
-    plugins: ['remove_button'],
+    plugins: ["remove_button"],
     allowEmptyOption: true,
-    onFocus: function () { $(".selectize-input input").attr("style", "width: auto;"); }
+    onFocus: function () {
+      $(".selectize-input input").attr("style", "width: auto;");
+    },
   });
 
   $("select.role-select-users").selectize({
-    plugins: ['remove_button'],
+    plugins: ["remove_button"],
     delimiter: " ",
     persist: false,
     create(input) {
       return {
         value: input,
-        text: input
+        text: input,
       };
-    }
+    },
   });
 
-  $('.conductor-filter-button').click(function (e) {
-    var project_client_selectize = $('select.project-clients').selectize();
-    var allocation_user_selectize = $('select.allocation-users').selectize();
-    var event_type_selectize = $('select.event-type').selectize();
+  $(".conductor-filter-button").click(function (e) {
+    var project_client_selectize = $("select.project-clients").selectize();
+    var allocation_user_selectize = $("select.allocation-users").selectize();
+    var event_type_selectize = $("select.event-type").selectize();
 
-    var projectClientData = (project_client_selectize[0].selectize).getValue();
-    var allocationUserData = (allocation_user_selectize[0].selectize).getValue();
-    var eventTypeData = (event_type_selectize[0].selectize).getValue();
+    var projectClientData = project_client_selectize[0].selectize.getValue();
+    var allocationUserData = allocation_user_selectize[0].selectize.getValue();
+    var eventTypeData = event_type_selectize[0].selectize.getValue();
 
-    Turbolinks.visit('//' + location.host + location.pathname + '?event_types=' + eventTypeData +'&project_clients='+ projectClientData +'&allocation_users='+ allocationUserData);
+    Turbolinks.visit(
+      "//" +
+        location.host +
+        location.pathname +
+        "?event_types=" +
+        eventTypeData +
+        "&project_clients=" +
+        projectClientData +
+        "&allocation_users=" +
+        allocationUserData
+    );
   });
-
-})
+});
