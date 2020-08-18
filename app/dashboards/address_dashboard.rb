ee9c068df10e7577babc354a0c8121c293885d99
Han Sheng Chin <hschin@gmail.com>
2020-08-19 01:02:56 +0800

    Update fields for address and user dashboard

--- app/dashboards/address_dashboard.rb
+++ app/dashboards/address_dashboard.rb
@@ -26,16 +26,20 @@ class AddressDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :addressable,
     :id,
     :line_1,
     :line_2,
+    :postal_code,
+    :city,
+    :country,
+    :state,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :id,
+    :addressable,
     :line_1,
     :line_2,
     :postal_code,
@@ -61,7 +65,7 @@ class AddressDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how addresses are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(address)
-  #   "Address ##{address.id}"
-  # end
+  def display_resource(address)
+    "#{address.addressable.name}'s Address"
+  end
 end
