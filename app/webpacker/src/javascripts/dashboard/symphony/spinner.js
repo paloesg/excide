bb051b374e5a314bb793715128c618f243c38cd8
Jonathanlau92 <jonathan@excide.co>
2020-08-18 11:00:35 +0800

    Fix codacy issues

--- app/webpacker/src/javascripts/dashboard/symphony/spinner.js
+++ app/webpacker/src/javascripts/dashboard/symphony/spinner.js
@@ -1,6 +1,5 @@
 $(document).on("turbolinks:load", function() {
   $(".start-loading").click(function(e){
-    console.log("I am here");
     $("#loading").removeClass("d-none");
     setInterval(function(){ 
      $("#loading").addClass("d-none");
