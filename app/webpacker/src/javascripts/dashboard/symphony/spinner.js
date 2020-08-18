1dd5c8fa95b52ee05678334c8e7c61ddd6f844e8
Jonathanlau92 <jonathan@excide.co>
2020-08-18 09:39:18 +0800

    Load spinner for 5 seconds

--- app/webpacker/src/javascripts/dashboard/symphony/spinner.js
+++ app/webpacker/src/javascripts/dashboard/symphony/spinner.js
@@ -1,11 +1,9 @@
 $(document).on("turbolinks:load", function() {
   $(".start-loading").click(function(e){
     console.log("I am here");
-    // e.preventDefault();
     $("#loading").removeClass("d-none");
-    // setInterval(function(){ 
-    //  $("#loading").addClass("d-none");
-    // }, 3000);
-    // Turbolinks.visit(window.location);
+    setInterval(function(){ 
+     $("#loading").addClass("d-none");
+    }, 5000);
   });
 });
