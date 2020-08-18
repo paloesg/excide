09592c6618d69c381225f78c2297422cfa8545e0
Jonathanlau92 <jonathan@excide.co>
2020-08-18 10:55:53 +0800

    Change wording of filtering dropdown

--- app/controllers/symphony/workflows_controller.rb
+++ app/controllers/symphony/workflows_controller.rb
@@ -61,6 +61,7 @@ class Symphony::WorkflowsController < ApplicationController
     @workflows = @template.workflows.select{|wf| params[:year].present? ? wf.deadline&.year.to_s == params[:year] : wf.deadline&.year == Time.current.year}.sort_by{|wf| wf.created_at}
     # Determine how many years in the filtering options based on deadline
     @years_to_filter = @template.workflows.pluck(:deadline).map { |d| d.present? ? d.year : [Time.current.year] }.uniq
+    @year = params[:year].present? ? params[:year].to_i : Date.current.year
 
     if @workflow.completed?
       @section = params[:section_id] ? @sections.find(params[:section_id]) : @sections.last
