b4e84b4150333e772dc6a5f60018bae7e9fc6594
Jonathanlau92 <jonathan@excide.co>
2020-08-19 10:51:14 +0800

    Change indentation to spaces and add semicolon

--- app/webpacker/src/javascripts/dashboard/symphony/spinner.js
+++ app/webpacker/src/javascripts/dashboard/symphony/spinner.js
@@ -1,12 +1,12 @@
 $(document).on("turbolinks:load", function() {
-	// Check for the class "first-reload" to trigger the loading spinner
-	// Afterwards, redirect to the same URL but without the params
-	if ($("div#loading").hasClass('first-reload')) {
-		$("#loading").removeClass("d-none");
-		setTimeout(function(){ 
-		  $("#loading").addClass("d-none");
-		  // Refresh the page
-		  Turbolinks.visit(window.location.href.split('?')[0])
-		}, 6000);
-	} 
+  // Check for the class "first-reload" to trigger the loading spinner
+  // Afterwards, redirect to the same URL but without the params
+  if ($("div#loading").hasClass('first-reload')) {
+    $("#loading").removeClass("d-none");
+    setTimeout(function(){ 
+      $("#loading").addClass("d-none");
+      // Refresh the page
+      Turbolinks.visit(window.location.href.split('?')[0]);
+    }, 6000);
+  } 
 });
