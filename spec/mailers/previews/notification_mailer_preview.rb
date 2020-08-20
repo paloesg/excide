44501daf1bb71401de6c9565dbd50bf9b898644a
PangKimJin <pangkimjin@u.nus.edu>
2020-08-20 11:04:31 +0800

    removed all instances of task_notification, first_task_notification, and unordered_workflow_notification mailers.

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,14 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 #you can either log in to User.last account or change the User.last to User.find(your id)
 class NotificationMailerPreview < ActionMailer::Preview
-  def first_task_notification
-    NotificationMailer.first_task_notification(Task.last, Batch.last, User.last)
-  end
-
-  def unordered_workflow_notification
-    NotificationMailer.unordered_workflow_notification(Template.where(workflow_type: 1).first.workflows.first.user, Template.where(workflow_type: 1).first.sections.map{|sect| sect.tasks }.flatten.compact, WorkflowAction.last)
-  end
-
   def create_event
     NotificationMailer.create_event(Event.last, User.last)
   end
