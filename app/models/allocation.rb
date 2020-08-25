dec72fef98f7d3a07ff501a32d95043cb108287c
Jonathanlau92 <jonathan@excide.co>
2020-08-25 15:31:11 +0800

    Remove start time and end time validation

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -10,7 +10,6 @@ class Allocation < ApplicationRecord
   monetize :rate_cents, allow_nil: true
 
   validates :event, :allocation_type, :allocation_date, :start_time, :end_time, presence: true
-  validate :end_must_be_after_start
 
   def self.to_csv
     attributes = ['S. S/N', 'Full Name', 'Date', 'Hours Charged', 'Job Nature', 'Client', 'Service Line']
@@ -38,16 +37,5 @@ class Allocation < ApplicationRecord
 
   def hours
     (self.end_time - self.start_time) / 3600
-  end
-
-  private
-
-  def end_must_be_after_start
-    # Skip this validation if start and end time not present to prevent errors
-    return if start_time.blank? or end_time.blank?
-
-    if end_time < start_time
-      errors.add(:end_time, "must be after start time")
-    end
   end
 end
