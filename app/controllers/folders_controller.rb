3f16f7ec13906ca180be9e548a265ec2f818c2cc
Han Sheng Chin <hschin@gmail.com>
2020-09-04 14:21:31 +0800

    Fix unscoped call in folder controller

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -64,7 +64,7 @@ class FoldersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_folder
-      @folder = Folder.find(params[:id])
+      @folder = current_user.company.folders.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
