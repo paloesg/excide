5c6c6733ccee5d6a172251f2484e7024c657f2be
PangKimJin <pangkimjin@u.nus.edu>
2020-09-07 11:20:58 +0800

    removed unused Twilio code.

--- app/services/send_daily_summary.rb
+++ app/services/send_daily_summary.rb
@@ -3,10 +3,6 @@ class SendDailySummary
   
     def initialize(user)
       @user = user
-      @from_number = ENV['TWILIO_NUMBER']
-      @account_sid = ENV['TWILIO_ACCOUNT_SID']
-      @auth_token = ENV['TWILIO_AUTH_TOKEN']
-      @client = Twilio::REST::Client.new @account_sid, @auth_token
     end
   
     def run
