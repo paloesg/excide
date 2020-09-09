fbf9b99bba760d6d431a63f7e9e7857c34b031ba
PangKimJin <pangkimjin@u.nus.edu>
2020-09-09 11:02:18 +0800

    added more comments and fixed tabspace.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
-
-    # controller for the menu page for users with multiple products
-    def index
-        @products = current_user.company.products
-    end
+  # controller for the menu page for users with multiple products
+  def index
+    @products = current_user.company.products
+  end
 end
