8667c8e178a775128812182bc6eaf737314ae4b2
PangKimJin <pangkimjin@u.nus.edu>
2020-09-11 11:07:54 +0800

    removed placeholder redirection case for nil products.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.company.name.present?
-      if current_user.company.products == nil
-        products_url
-      elsif current_user.company.products.length > 1
+      if current_user.company.products.length > 1
         products_url
       elsif current_user.company.products.length == 1
         if current_user.company.products[0] == 'Symphony'
