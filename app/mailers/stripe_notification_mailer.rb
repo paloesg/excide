2ab322ff04f95bed2943ad6d2123fb4f2685f5da
Jonathanlau92 <jonathan@excide.co>
2020-09-17 13:17:28 +0800

    Migrate Stripe mailer to use sendgrid API for email sending

--- app/mailers/stripe_notification_mailer.rb
+++ app/mailers/stripe_notification_mailer.rb
@@ -1,41 +1,39 @@
 class StripeNotificationMailer < ApplicationMailer
   default from: 'Excide Symphony <admin@excide.co>'
-
-  def send_stripe_email(user, message)
-    @user = user
-    mail(to: @user.email, from: 'Paloe Symphony <admin@excide.co>', subject: '[Symphony]Subscription Update', body: 'Some body',  template_id: ENV['SENDGRID_EMAIL_TEMPLATE'], dynamic_template_data: {
-        firstName: @user.first_name,
-        message: message
-      }
-    )
-  end
-
   # def upcoming_payment_notification(user)
   #   @user = user
   #   mail(to: @user.email, subject: '[Upcoming billing for Symphony PRO]')
   # end
 
   def cancel_subscription_notification(user, period_end)
-    @user = user
-    @period_end = period_end
-    mail(to: @user.email, subject: '[You have cancelled your subscription]')
+    mail(to: 'jonathan.lau@paloe.com.sg', from: 'Paloe Symphony <admin@excide.co>', subject: '[Symphony]Subscription Update', body: 'Some body',  template_id: "d-b943fd3d9f7e4770951f4a1cb1d9e8c2", dynamic_template_data: {
+        firstName: user.first_name,
+        period_end: period_end
+      }
+    )
   end
 
   def payment_successful(user, start_date, end_date, invoice_pdf)
-    @user = user
-    @start_date = start_date
-    @end_date = end_date
-    @invoice_pdf = invoice_pdf
-    mail(to: @user.email, subject: '[Payment has been made successfully]')
+    mail(to: 'jonathan.lau@paloe.com.sg', from: 'Paloe Symphony <admin@excide.co>', subject: '[Symphony]Subscription Update', body: 'Some body',  template_id: "d-58d4d5a62dbc4cfe9eab4ddedfd2e24f", dynamic_template_data: {
+        firstName: user.first_name,
+        start_date: start_date,
+        end_date: end_date,
+        invoice_pdf: invoice_pdf
+      }
+    )
   end
 
   def charge_failed(user)
-    @user = user
-    mail(to: @user.email, subject: '[Payment to subscribe Symphony PRO has failed]')
+    mail(to: 'jonathan.lau@paloe.com.sg', from: 'Paloe Symphony <admin@excide.co>', subject: '[Symphony]Subscription Update', body: 'Some body',  template_id: ENV['SENDGRID_EMAIL_TEMPLATE'], dynamic_template_data: {
+        firstName: user.first_name,
+      }
+    )
   end
 
   def free_trial_ending_notification(user)
-    @user = user
-    mail(to: @user.email, subject: '[Free trial ending]')
+    mail(to: 'jonathan.lau@paloe.com.sg', from: 'Paloe Symphony <admin@excide.co>', subject: '[Symphony]Subscription Update', body: 'Some body',  template_id: "d-a8b2a552f6e74c5d957cebe120a35800", dynamic_template_data: {
+        firstName: user.first_name,
+      }
+    )
   end
 end
