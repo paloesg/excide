17e8a2d87f1d45e16915555d1a64ec8c83baa935
Han Sheng Chin <hschin@gmail.com>
2020-09-25 01:21:25 +0800

    Update permissions admin dashboard

--- app/dashboards/permission_dashboard.rb
+++ app/dashboards/permission_dashboard.rb
@@ -8,7 +8,7 @@ class PermissionDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    folder: Field::BelongsTo,
+    permissible: Field::Polymorphic.with_options(classes: [Folder, Document]),
     role: Field::BelongsTo,
     id: Field::String.with_options(searchable: false),
     can_write: Field::Boolean,
@@ -24,21 +24,23 @@ class PermissionDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  folder
+  permissible
   role
   id
+  can_view
+  can_download
   can_write
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  folder
+  permissible
   role
   id
-  can_write
   can_view
   can_download
+  can_write
   created_at
   updated_at
   ].freeze
@@ -47,11 +49,11 @@ class PermissionDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  folder
+  permissible
   role
-  can_write
   can_view
   can_download
+  can_write
   ].freeze
 
   # COLLECTION_FILTERS
