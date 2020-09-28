96d0a9dc0d39e866015009c8870da54b8090904d
PangKimJin <pangkimjin@u.nus.edu>
2020-09-28 22:06:02 +0800

    fixed typo and added more comments.

--- app/controllers/symphony/templates_controller.rb
+++ app/controllers/symphony/templates_controller.rb
@@ -77,7 +77,8 @@ class Symphony::TemplatesController < ApplicationController
           @template.set_next_workflow_date(@workflow)
           redirect_to symphony_workflows_path(workflow_name: @template.slug)
         else
-          @template.update_workflows
+          # update deadlines of wf and wfa
+          @template.update_deadlines
           flash[:notice] = 'Template has been saved.'
           redirect_to edit_symphony_template_path(@template)
         end
