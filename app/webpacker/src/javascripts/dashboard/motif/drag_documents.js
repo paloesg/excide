de0b77aeef03d1e69cf492f479fb06a4adbbe419
Jonathanlau92 <jonathan@excide.co>
2020-10-01 23:31:49 +0800

    Remove comments and console logs

--- app/webpacker/src/javascripts/dashboard/motif/drag_documents.js
+++ app/webpacker/src/javascripts/dashboard/motif/drag_documents.js
@@ -14,15 +14,11 @@ $(document).on("turbolinks:load", function() {
   draggable.on("drag:over", (e) => {
     // Store the element that a file was dragged into
     source = e;
-    console.log("what is source: ", $("#" + source.data.over.closest('tr').id));
     // $("#" + source.data.over.closest('tr').id).css({ "border": "4px dashed blue" })
   });
 
   draggable.on("drag:stop", (e) => {
     // On release, send an AJAX reqeust to store file into folder
-    console.log("source 2", source);
-    console.log("source 2", source.data);
-    console.log("target closest TR", source.data.over.closest('tr'));
     let tableRow = source.data.over.closest('tr')
     $.ajax({
       type: "PATCH",
@@ -33,29 +29,10 @@ $(document).on("turbolinks:load", function() {
       },
       dataType: "JSON"
     }).done((result) => {
-      console.log("WHAT IS result: ", result);
       const linkTo = result["link_to"];
       Turbolinks.visit(linkTo);
     });
     // execute your drop action: e.originalSource on lastOver
     source = undefined;
-    console.log("2nd new source", source)
   });
-
-  // draggable.on('drag:over:container', (res) => console.log('drag:container', res.data.sensorEvent.target));
-  // draggable.on('drag:stop', (res) => console.log('drag:stop', res));
-
-  // draggable.on('drag:out', (res) => {
-  //   console.log('targeted_tr', res.sensorEvent.target.closest("tr"));
-  //   console.log('dragged_element', res.data.originalSource.id);
-  //   let targeted_tr = res.sensorEvent.target.closest("tr");
-  //   let dragged_element_id = res.data.originalSource.id;
-  //   $.ajax({
-  //     type: "get",
-  //     url: "/motif/hello_world",
-  //     dataType: "json",
-  //   }).done(function(data) {
-  //     console.log("DONED!", data);
-  //   });
-  // });
 });
