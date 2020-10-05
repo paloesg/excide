e450b371cb50ade13a60d024aeeac002c2d9f7a4
LimNing <e0324216@u.nus.edu>
2020-10-05 17:53:27 +0800

    Toggle offcanvas with some td

--- app/webpacker/src/javascripts/dashboard/motif/drawer.js
+++ app/webpacker/src/javascripts/dashboard/motif/drawer.js
@@ -11,8 +11,9 @@ $(document).on("turbolinks:load", function () {
         overlay: true,
         closeBy: "drawer_close_" + dataAttribute,
         toggleBy: {
-          target: "drawer_toggle_" + dataAttribute,
-          state: "mobile-toggle-active",
+          0: "drawer_toggle_" + dataAttribute + "_0",
+          1: "drawer_toggle_" + dataAttribute + "_1",
+          2: "drawer_toggle_" + dataAttribute + "_2"
         },
       }
     );
