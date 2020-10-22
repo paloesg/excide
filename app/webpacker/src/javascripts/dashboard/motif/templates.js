631aa01fd1c1e046a00e3a751439b2f224242e0c
Jonathanlau92 <jonathan@excide.co>
2020-10-22 23:22:31 +0800

    Make drawer dynamic based on ID

--- app/webpacker/src/javascripts/dashboard/motif/templates.js
+++ app/webpacker/src/javascripts/dashboard/motif/templates.js
@@ -9,7 +9,7 @@ $(document).on("turbolinks:load", function () {
     // Find the nearest td descendant and add drawer toggle ID to it based on it's unique timestamp 
     $(insertedItem).find("td.drawer-toggle").attr('id', 'drawer_toggle_' + $(insertedItem).data("drawer"));
     console.log("What is jquery data attr: ", $(insertedItem).data("drawer"));
-    $("#tasksOffcanvas").append("<div class='offcanvas offcanvas-right offcanvas-top p-5 overflow-auto' id='drawer_task'><div class='row'><div class='col-md-11'><h5 class='d-inline'>Hello world</h5></div><div class='col-md-1'><a class='d-inline' href='#' id='drawer_close_" + e.timeStamp + "'><i class='material-icons-outlined'>close</i></a></div></div></div>");
+    $("#tasksOffcanvas").append("<div class='offcanvas offcanvas-right offcanvas-top p-5 overflow-auto' id='drawer_task_"+ e.timeStamp +"'><div class='row'><div class='col-md-11'><h5 class='d-inline'>Hello world</h5></div><div class='col-md-1'><a class='d-inline' href='#' id='drawer_close_" + e.timeStamp + "'><i class='material-icons-outlined'>close</i></a></div></div></div>");
   });
 
   $('#tasks').on('cocoon:after-insert', function(e, insertedItem, originalEvent) {
@@ -18,7 +18,7 @@ $(document).on("turbolinks:load", function () {
     $(".task-drawer-row").each(function () {
       let dataAttribute = $(this).data("drawer");
       const offcanvasObject = new Offcanvas(
-        "drawer_task",
+        "drawer_task_" + dataAttribute,
         {
           baseClass: "offcanvas",
           overlay: true,
