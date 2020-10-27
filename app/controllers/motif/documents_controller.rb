d5196d6730974b6aad0cddbdd8fc3a94c277da69
Jonathanlau92 <jonathan@excide.co>
2020-10-27 12:33:32 +0800

    Change count method to length for calculating access number

--- app/controllers/motif/documents_controller.rb
+++ app/controllers/motif/documents_controller.rb
@@ -9,7 +9,7 @@ class Motif::DocumentsController < ApplicationController
   def index
     @folder = Folder.new
     @folders = policy_scope(Folder).roots
-    @documents = policy_scope(Document).where(folder_id: nil).order(created_at: :desc)
+    @documents = policy_scope(Document).where(folder_id: nil).order(created_at: :desc).includes(:permissions)
     @users = @company.users.includes(:permissions)
     @activities = PublicActivity::Activity.order("created_at desc").where(trackable_type: "Document").first(10)
   end
