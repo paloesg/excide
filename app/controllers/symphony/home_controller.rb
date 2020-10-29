fea80d088385a235bda2a6f857e2047245766993
Jonathanlau92 <jonathan@excide.co>
2020-10-29 15:56:50 +0800

    Remove my task and activity history codes from symphony

--- app/controllers/symphony/home_controller.rb
+++ app/controllers/symphony/home_controller.rb
@@ -14,37 +14,6 @@ class Symphony::HomeController < ApplicationController
     @outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where.not(deadline: nil).where(company: current_user.company).order(:deadline).includes(:task).first(3)
   end
 
-  def tasks
-    if params[:tasks].blank? || params[:tasks] == "Only incomplete tasks"
-      @get_outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where.not(completed: true).where(company: current_user.company).includes(:task)
-    elsif params[:tasks] == "All tasks"
-      @get_outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where(company: current_user.company).includes(:task)
-    else
-      @get_outstanding_actions = WorkflowAction.includes(:workflow).all_user_actions(current_user).where(completed: true).where(company: current_user.company).includes(:task)
-    end
-    # Filtering by days
-    @get_outstanding_actions = @get_outstanding_actions.where('created_at >= ?', Time.current - params[:created_at].to_i.days) unless params[:created_at].blank?
-    # Actions that depends the type of task (with repetition task or adhoc)
-    if params[:types] == "Tasks related to Routine"
-      @get_outstanding_actions = @get_outstanding_actions.select {|action| action.workflow_id.present?}
-    elsif params[:types] == "Adhoc tasks"
-      @get_outstanding_actions = @get_outstanding_actions.select {|action| action.workflow_id.nil?}
-    end
-    @outstanding_actions = Kaminari.paginate_array(@get_outstanding_actions).page(params[:page]).per(10)
-    @actions_sort = sort_column(@outstanding_actions).reverse!
-    params[:direction] == "desc" ? @actions_sort.reverse! : @actions_sort
-  end
-
-  def activity_history
-    if params[:created_at].present?
-      @get_activities = PublicActivity::Activity.includes(:owner).where.not(recipient_type: "Event").where(owner_id: current_user.id).where('created_at >= ?', Time.current - params[:created_at].to_i.days).order("created_at desc")
-    else
-      # Get all current_user activities
-      @get_activities = PublicActivity::Activity.includes(:owner).where.not(recipient_type: "Event").where(owner_id: current_user.id).order("created_at desc")
-    end
-    @activities = Kaminari.paginate_array(@get_activities).page(params[:page]).per(10)
-  end
-
   def add_tasks_to_timesheet
     params[:tasks].each do |task_id|
       @event_type = EventType.find_or_create_by(name: Task.find(task_id.to_i).instructions)
