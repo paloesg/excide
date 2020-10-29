f85bc7e11ba33fcd8044d7907a81ec69ef571c88
Han Sheng Chin <hschin@gmail.com>
2020-10-29 22:06:58 +0800

    Add a general layout for non-product pages like login and sign up

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,2 @@
 class Users::PasswordsController < Devise::PasswordsController
-  layout 'symphony/application'
-  
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'symphony/application'
-
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -24,7 +22,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       company.account_type = 0
       company.save
       resource.company = company
-      if params[:product].present? 
+      if params[:product].present?
         resource.company.products = [params[:product]]
         resource.company.save
         resource.save
