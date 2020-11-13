f982c447ca1e91c0b496b520ddaec4a70eeef034
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-11-13 16:07:51 +0800

    Add set company private methods in home controller

--- app/controllers/motif/home_controller.rb
+++ app/controllers/motif/home_controller.rb
@@ -1,6 +1,8 @@
 class Motif::HomeController < ApplicationController
   layout 'motif/application'
-
+  
+  before_action :set_company
+  before_action :set_franchisee, except: :index
   before_action :authenticate_user!
 
   def index
@@ -18,6 +20,18 @@ class Motif::HomeController < ApplicationController
   end
 
   private
+
+  def set_company
+    @company = current_user.company
+  end
+
+  def set_franchisee
+    @franchisee = Franchisee.find(params[:id])
+  end
+
+  def franchisee_params
+    params.require(:franchisee).permit(:name, :website_url, :established_date, :annual_turnover_rate, :currency, :address, :description, :contact_person_details, :profile_picture, :contact,  address_attributes: [:id, :line_1, :line_2, :postal_code, :city, :country, :state], user_attributes: [:id, :first_name, :last_name, :email, :contact_number])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :contact_number, :company_id, :outlet_id, :stripe_card_token, :stripe_customer_id, settings_attributes: [:reminder_sms, :reminder_email, :reminder_slack, :reminder_whatsapp, :task_sms, :task_email, :task_slack, :task_whatsapp, :batch_sms, :batch_email, :batch_slack, :batch_whatsapp], :role_ids => [], company_attributes:[:id, :name, :connect_xero, address_attributes: [:id, :line_1, :line_2, :postal_code, :city, :country, :state]])
