021070d714d33c6f4558caf3da4ed31379baaf63
Jonathanlau92 <jonathan@excide.co>
2020-11-16 23:04:59 +0800

    Add communication hub page with empty states

--- app/controllers/motif/notes_controller.rb
+++ app/controllers/motif/notes_controller.rb
@@ -1,4 +1,8 @@
 class Motif::NotesController < ApplicationController
+  layout 'motif/application'
+
   def index
+    # get all notes (comment in the franchise)
+    @notes = current_user.company.get_notes
   end
 end
