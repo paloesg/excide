d62df81b5726bb8de7079077f65ac8fed4b61630
Jonathanlau92 <jonathan@excide.co>
2020-11-16 23:04:44 +0800

    Add company references to outlets and change relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_085625) do
+ActiveRecord::Schema.define(version: 2020_11_16_142748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -371,6 +371,8 @@ ActiveRecord::Schema.define(version: 2020_11_10_085625) do
     t.uuid "franchisee_id"
     t.string "name"
     t.string "contact"
+    t.uuid "company_id"
+    t.index ["company_id"], name: "index_outlets_on_company_id"
     t.index ["franchisee_id"], name: "index_outlets_on_franchisee_id"
   end
 
@@ -764,6 +766,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_085625) do
   add_foreign_key "invoices", "users"
   add_foreign_key "invoices", "workflows"
   add_foreign_key "notes", "users"
+  add_foreign_key "outlets", "companies"
   add_foreign_key "outlets", "franchisees"
   add_foreign_key "permissions", "roles"
   add_foreign_key "questions", "survey_sections"
