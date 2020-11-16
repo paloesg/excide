585747fdeccaab3d1d4f99c96b561b0af12c22c1
Han Sheng Chin <hschin@gmail.com>
2020-11-17 01:06:36 +0800

    Fix syntax for find_in_batches and only use batches for models with large quantities

--- db/migrate/20201020155843_change_company_id_to_uuid.rb
+++ db/migrate/20201020155843_change_company_id_to_uuid.rb
@@ -47,110 +47,130 @@ class ChangeCompanyIdToUuid < ActiveRecord::Migration[6.0]
     add_column :roles, :company_uuid, :uuid
     Company.find_each do |c|
       say "Creating a company #{c.name}"
-      Address.where(addressable_id: c.id).find_in_batches(batch_size: 100) do |add|
+      Address.where(addressable_id: c.id).find_each do |add|
         say "Created an address #{add.id}"
         add.company_uuid = c.uuid
         add.save!
       end
-      Role.where(resource_id: c.id).find_in_batches(batch_size: 100) do |role|
+      Role.where(resource_id: c.id).find_each do |role|
         say "Created a role #{role.id}"
         role.company_uuid = c.uuid
         role.save!
       end
-      Batch.where(company_id: c.id).find_in_batches(batch_size: 100) do |batch|
-        say "Created a batch #{batch.id}"
-        batch.company_uuid = c.uuid
-        batch.save!
+      Batch.where(company_id: c.id).find_in_batches(batch_size: 100) do |batches|
+        batches.each do |batch|
+          say "Created a batch #{batch.id}"
+          batch.company_uuid = c.uuid
+          batch.save!
+        end
       end
-      Client.where(company_id: c.id).find_in_batches(batch_size: 100) do |client|
-        say "Created a client #{client.id}"
-        client.company_uuid = c.uuid
-        client.save!
+      Client.where(company_id: c.id).find_in_batches(batch_size: 100) do |clients|
+        clients.each do |client|
+          say "Created a client #{client.id}"
+          client.company_uuid = c.uuid
+          client.save!
+        end
       end
-      Contact.where(company_id: c.id).find_in_batches(batch_size: 100) do |contact|
+      Contact.where(company_id: c.id).find_each do |contact|
         say "Created a contact #{contact.id}"
         contact.company_uuid = c.uuid
         contact.save!
       end
-      Document.where(company_id: c.id).find_in_batches(batch_size: 100) do |document|
-        say "Created a doc #{document.id}"
-        document.company_uuid = c.uuid
-        document.save!
+      Document.where(company_id: c.id).find_in_batches(batch_size: 100) do |documents|
+        documents.each do |document|
+          say "Created a doc #{document.id}"
+          document.company_uuid = c.uuid
+          document.save!
+        end
       end
-      Event.where(company_id: c.id).find_in_batches(batch_size: 100) do |event|
-        say "Created an event #{event.id}"
-        event.company_uuid = c.uuid
-        event.save(validate: false)
+      Event.where(company_id: c.id).find_in_batches(batch_size: 100) do |events|
+        events.each do |event|
+          say "Created an event #{event.id}"
+          event.company_uuid = c.uuid
+          event.save(validate: false)
+        end
       end
-      Folder.where(company_id: c.id).find_in_batches(batch_size: 100) do |folder|
+      Folder.where(company_id: c.id).find_each do |folder|
         say "Created a folder #{folder.id}"
         folder.company_uuid = c.uuid
         folder.save!
       end
-      Invoice.where(company_id: c.id).find_in_batches(batch_size: 100) do |inv|
-        say "Created a invoice #{inv.id}"
-        inv.company_uuid = c.uuid
-        inv.save(validate: false)
+      Invoice.where(company_id: c.id).find_in_batches(batch_size: 100) do |invs|
+        invs.each do |inv|
+          say "Created a invoice #{inv.id}"
+          inv.company_uuid = c.uuid
+          inv.save(validate: false)
+        end
       end
-      Outlet.where(company_id: c.id).find_in_batches(batch_size: 100) do |outlet|
+      Outlet.where(company_id: c.id).find_each do |outlet|
         say "Created a outlet #{outlet.id}"
         outlet.company_uuid = c.uuid
         outlet.save!
       end
-      RecurringWorkflow.where(company_id: c.id).find_in_batches(batch_size: 100) do |rec|
+      RecurringWorkflow.where(company_id: c.id).find_each do |rec|
         say "Created a recurring wf #{rec.id}"
         rec.company_uuid = c.uuid
         rec.save!
       end
-      Reminder.where(company_id: c.id).find_in_batches(batch_size: 100) do |reminder|
+      Reminder.where(company_id: c.id).find_each do |reminder|
         say "Created a reminder #{reminder.id}"
         reminder.company_uuid = c.uuid
         reminder.save!
       end
-      Survey.where(company_id: c.id).find_in_batches(batch_size: 100) do |survey|
+      Survey.where(company_id: c.id).find_each do |survey|
         say "Created a survey #{survey.id}"
         survey.company_uuid = c.uuid
         survey.save!
       end
-      SurveyTemplate.where(company_id: c.id).find_in_batches(batch_size: 100) do |sur_tem|
+      SurveyTemplate.where(company_id: c.id).find_each do |sur_tem|
         say "Created a survey tem #{sur_tem.id}"
         sur_tem.company_uuid = c.uuid
         sur_tem.save!
       end
-      Template.where(company_id: c.id).find_in_batches(batch_size: 100) do |tem|
+      Template.where(company_id: c.id).find_each do |tem|
         say "Created a tem #{tem.id}"
         tem.company_uuid = c.uuid
         tem.save!
       end
-      User.where(company_id: c.id).find_in_batches(batch_size: 100) do |user|
+      User.where(company_id: c.id).find_each do |user|
         say "Created a user #{user.id}"
         user.company_uuid = c.uuid
         user.save!
       end
-      Workflow.where(company_id: c.id).find_in_batches(batch_size: 100) do |wf|
-        say "Created a wf #{wf.id}"
-        wf.company_uuid = c.uuid
-        wf.save!
+      Workflow.where(company_id: c.id).find_in_batches(batch_size: 100) do |wfs|
+        wfs.each do |wf|
+          say "Created a wf #{wf.id}"
+          wf.company_uuid = c.uuid
+          wf.save!
+        end
       end
-      WorkflowAction.where(company_id: c.id).find_in_batches(batch_size: 100) do |wfa|
-        say "Created a wfa #{wfa.id}"
-        wfa.company_uuid = c.uuid
-        wfa.save!
+      WorkflowAction.where(company_id: c.id).find_in_batches(batch_size: 100) do |wfas|
+        wfas.each do |wfa|
+          say "Created a wfa #{wfa.id}"
+          wfa.company_uuid = c.uuid
+          wfa.save!
+        end
       end
-      XeroTrackingCategory.where(company_id: c.id).find_in_batches(batch_size: 100) do |xero_tracking_category|
-        say "Created a xerotc #{xero_tracking_category.id}"
-        xero_tracking_category.company_uuid = c.uuid
-        xero_tracking_category.save!
+      XeroTrackingCategory.where(company_id: c.id).find_in_batches(batch_size: 100) do |xero_tracking_categories|
+        xero_tracking_categories.each do |xero_tracking_category|
+          say "Created a xerotc #{xero_tracking_category.id}"
+          xero_tracking_category.company_uuid = c.uuid
+          xero_tracking_category.save!
+        end
       end
-      XeroContact.where(company_id: c.id).find_in_batches(batch_size: 100) do |xero_contact|
-        say "Created a xerocontacts #{xero_contact.id}"
-        xero_contact.company_uuid = c.uuid
-        xero_contact.save!
+      XeroContact.where(company_id: c.id).find_in_batches(batch_size: 100) do |xero_contacts|
+        xero_contacts.each do |xero_contact|
+          say "Created a xerocontacts #{xero_contact.id}"
+          xero_contact.company_uuid = c.uuid
+          xero_contact.save!
+        end
       end
-      XeroLineItem.where(company_id: c.id).find_in_batches(batch_size: 100) do |xero_line_item|
-        say "Created a xeroline #{xero_line_item.id}"
-        xero_line_item.company_uuid = c.uuid
-        xero_line_item.save!
+      XeroLineItem.where(company_id: c.id).find_in_batches(batch_size: 100) do |xero_line_items|
+        xero_line_items.each do |xero_line_item|
+          say "Created a xeroline #{xero_line_item.id}"
+          xero_line_item.company_uuid = c.uuid
+          xero_line_item.save!
+        end
       end
     end
     # Remove company_id references from related table
