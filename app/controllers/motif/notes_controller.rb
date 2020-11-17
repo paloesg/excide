bfb2fbe2f0dfd38d5724634d193ae2ef28e9c94b
Jonathanlau92 <jonathan@excide.co>
2020-11-17 10:15:27 +0800

    Add note INDEX view

--- app/controllers/motif/notes_controller.rb
+++ app/controllers/motif/notes_controller.rb
@@ -2,7 +2,6 @@ class Motif::NotesController < ApplicationController
   layout 'motif/application'
 
   def index
-    # get all notes (comment in the franchise)
-    @notes = current_user.company.get_notes
+    @outlets = current_user.company.outlets
   end
 end
