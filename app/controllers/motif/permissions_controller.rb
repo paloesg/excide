92799b5281a2c927d1eca8ed397cb6fe3e013f16
Jonathanlau92 <jonathan@excide.co>
2020-12-07 12:21:15 +0800

    Fix typo in permission access for doc repo

--- app/controllers/motif/permissions_controller.rb
+++ app/controllers/motif/permissions_controller.rb
@@ -5,7 +5,7 @@ class Motif::PermissionsController < ApplicationController
     # Depending on permissible_type, get the instance of the respective permissible (document or folder)
     @permissible = params[:permissible_type] == "folder" ? Folder.find(params[:permissible_id]) : Document.find(params[:permissible_id])
     # Check if permission allows user to write or download. can_write => full access, can_download means can download and view
-    @permission = params[:permission] == "Can write" ? Permission.new(user_id: params[:user_id], permissible: @permissible, can_view: true, can_view: true, can_download: true) : Permission.new(user_id: params[:user_id], permissible: @permissible, can_write: false, can_view: true, can_download: true)
+    @permission = params[:permission] == "Can write" ? Permission.new(user_id: params[:user_id], permissible: @permissible, can_write: true, can_view: true, can_download: true) : Permission.new(user_id: params[:user_id], permissible: @permissible, can_write: false, can_view: true, can_download: true)
     respond_to do |format|
       if @permission.save
         format.json { render json: @permission, status: :ok }
