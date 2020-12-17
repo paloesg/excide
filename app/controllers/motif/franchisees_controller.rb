0694f972340f8345b13715870fe12fc20c8cbbfe
Jonathanlau92 <jonathan@excide.co>
2020-12-17 23:47:32 +0800

    Add franchisee index page

--- app/controllers/motif/franchisees_controller.rb
+++ app/controllers/motif/franchisees_controller.rb
@@ -2,8 +2,17 @@ class Motif::FranchiseesController < ApplicationController
   layout 'motif/application'
   
   before_action :authenticate_user!
+  before_action :set_company
+
+  def index
+    @franchisees = @company.franchisees
+  end
 
   private
+
+  def set_company
+    @company = current_user.company
+  end
 
   def franchisee_params
     params.require(:franchisee).permit(:commencement_date, :expiry_date, :renewal_period_freq_unit, :renewal_period_freq_value, :franchise_licensee, :registered_address, :license_type, :max_outlet, :min_outlet, :storage_space)
