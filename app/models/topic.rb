a0265216ad99b5d7845810602fe78c85be7233ea
Jonathanlau92 <jonathan@excide.co>
2020-12-25 22:30:10 +0800

    Add enum status to topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :user
   belongs_to :company
+
+  enum status: { need_answer: 0, need_approval: 1, answered: 2, closed: 3}
+  enum question_category: { state_interest: 0, due_dilligence: 1, investor_management: 2}
 end
