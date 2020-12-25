31daeb188bf3310287dfd4598163d299718dd2d7
Jonathanlau92 <jonathan@excide.co>
2020-12-25 22:58:55 +0800

    Add state machine to question's status

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,26 @@ class Topic < ApplicationRecord
   belongs_to :user
   belongs_to :company
 
-  enum status: { need_answer: 0, need_approval: 1, answered: 2, closed: 3}
   enum question_category: { state_interest: 0, due_dilligence: 1, investor_management: 2}
+  enum status: { need_answer: 0, need_approval: 1, answered: 2, closed: 3}
+
+  aasm column: :status, enum: true do
+    state :need_answer, initial: true
+    # Assign startup users to answer investor's question
+    event :assign_user do
+      transitions from: :need_answer, to: :need_approval
+    end
+    # Startup admin rejects user's answer to investor's question
+    event :rejected do
+      transitions from: :need_approval, to: :need_answer
+    end
+    # Startup admin approves user's answer to investor's question
+    event :approved do
+      transitions from: :need_approval, to: :answered
+    end
+    # Investor can close the question at any time
+    event :close_question do
+      transitions from: [:need_answer, :need_approval, :answered], to: :closed
+    end
+  end
 end
