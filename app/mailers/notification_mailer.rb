0254dea5214aacfa5d2f83e6e5a3098f8b67d3cf
Han Sheng Chin <hschin@gmail.com>
2020-12-28 17:44:13 +0800

    Change email recipient to Paloe support email when new outlet created

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: 'Excide Symphony <admin@excide.co>'
   require 'sendgrid-ruby'
   include SendGrid
-  
+
   def batch_reminder(reminders, user)
     mail(to: user.email, from: 'Paloe Symphony <admin@excide.co>', subject: 'Here is your daily email summary', body: 'Some body',  template_id: ENV['SENDGRID_BATCH_REMINDER_EMAIL_TEMPLATE'], dynamic_template_data: {
         firstName: user.first_name,
@@ -28,7 +28,7 @@ class NotificationMailer < ApplicationMailer
         email: user.email,
         contactNumber: user.contact_number,
         profileName: profile.name,
-        url: profile.url, 
+        url: profile.url,
       }
     )
   end
@@ -57,14 +57,14 @@ class NotificationMailer < ApplicationMailer
 
   def motif_new_outlet(user)
     #to email will be changed with afc's support email
-    mail(to: user.email, from: 'Paloe Overture <admin@excide.co>', subject: 'TO SAM!', body: 'Some body', template_id: ENV['ADA_SENDGRID_MOTIF_NEW_OUTLET'], dynamic_template_data: {
+    mail(to: 'support@paloe.com.sg', from: 'Paloe Overture <admin@excide.co>', subject: 'New Outlet', body: 'New outlet', template_id: ENV['ADA_SENDGRID_MOTIF_NEW_OUTLET'], dynamic_template_data: {
         fullName: user.full_name,
         company: user.company.name,
         email: user.email
       }
     )
   end
-  
+
   # Conductor's email notification methods!
   # Most of the removed mailer methods below were called in event.rb. associate_notification was called in scehduler.rake as a daily reminder to associate.
   # Removed conductor's create_event, edit_event, destroy_event, user_removed_from_event, event_notification and associate_notification methods from the following PR:
