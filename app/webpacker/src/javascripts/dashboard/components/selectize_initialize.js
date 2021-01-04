8ddb350ebf212635767e55acddba0553c96f3fb0
LimNing <e0324216@u.nus.edu>
2021-01-04 22:10:59 +0800

    Fix filter logic to use select2

--- app/webpacker/src/javascripts/dashboard/components/selectize_initialize.js
+++ app/webpacker/src/javascripts/dashboard/components/selectize_initialize.js
@@ -155,14 +155,24 @@ $(document).on("turbolinks:load", function () {
     let startDate = $("#startDate").val();
     let endDate = $("#endDate").val();
 
-    let projectClientSelectize = $("select.project-clients").selectize();
-    let projectClientData = projectClientSelectize[0].selectize.getValue();
+    let projectClientSelect2 = $("select.project-clients")[0].selectedOptions
+    let projectClientData = [];
+    for (i = 0; i < projectClientSelect2 .length; i++) {
+      projectClientData.push(projectClientSelect2[i].value);
+    }
 
-    let allocationUserSelectize = $("select.allocation-users").selectize();
-    let allocationUserData = allocationUserSelectize[0].selectize.getValue();
+    let allocationUserSelect2 = $("select.allocation-users")[0].selectedOptions;
+    let allocationUserData = [];
+    for (i = 0; i < allocationUserSelect2.length; i++) {
+      allocationUserData.push(allocationUserSelect2[i].value);
+    }
 
-    let serviceLineSelectize = $("select.service-line").selectize();
-    let serviceLineData = serviceLineSelectize[0].selectize.getValue();
+    let serviceLineSelect2 = $("select.service-line")[0].selectedOptions;
+    let serviceLineData = [];
+    for (i = 0; i < serviceLineSelect2.length; i++) {
+      serviceLineData.push(serviceLineSelect2[i].value);
+    }
+
 
     Turbolinks.visit(
       "//" +
