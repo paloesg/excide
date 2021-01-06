61eb1a2896ec8801f2f71023ff6f6700a31931cc
Jonathanlau92 <jonathan@excide.co>
2021-01-06 23:48:15 +0800

    Remove policy_scope to share folders across franchisee layer

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -86,7 +86,7 @@ class FoldersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_folder
-      @folder = policy_scope(Folder).find(params[:id])
+      @folder = Folder.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
