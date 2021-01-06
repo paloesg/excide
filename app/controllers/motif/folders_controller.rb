0ca9f8f404671bda5934b81ee8e4279a6dfddb0a
Jonathanlau92 <jonathan@excide.co>
2021-01-06 23:56:25 +0800

    Remove policy_scope restriction for documents within folders

--- app/controllers/motif/folders_controller.rb
+++ app/controllers/motif/folders_controller.rb
@@ -23,7 +23,7 @@ class Motif::FoldersController < FoldersController
     @users = @company.users.includes(:permissions)
     @folders = policy_scope(Folder).children_of(@folder)
     @activities = PublicActivity::Activity.order("created_at desc").where(trackable_type: "Document").first(10)
-    @documents = policy_scope(Document).where(folder: @folder)
+    @documents = Document.where(folder: @folder)
 
     render "motif/documents/index"
   end
