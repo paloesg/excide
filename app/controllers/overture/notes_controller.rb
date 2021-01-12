205dff05ca9210e97dde8bf62f17ad172ef75eda
Jonathanlau92 <jonathan@excide.co>
2021-01-13 00:07:55 +0800

    Add state transtion from need_answer to answered
    and it's respective views

--- app/controllers/overture/notes_controller.rb
+++ app/controllers/overture/notes_controller.rb
@@ -5,26 +5,49 @@ class Overture::NotesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_company
   before_action :get_topic
+  before_action :get_note, only: [:update]
 
   def index
     @notes = get_notes(@company, @topic)
+    # @startup_notes = Note.includes(:topic).where(topic: { startup_id: @company.id })
     @note = Note.new
+    # This will only be called if user is from a startup
+    @users = @company.users if @user.company.startup?
   end
 
   def create
     @note = Note.new(note_params)
     @note.user = current_user
+    # Change topic status to approve answer if company is a startup
+    @topic.approve_answer if current_user.company.startup?
     @note.notable = @topic
-    if @note.save
+    if @note.save and @topic.save
       redirect_to overture_topic_notes_path(topic_id: @topic.id), notice: "Answer has been posted. Please wait for answer to be approved."
     else
       render :new
     end
   end
 
+  def update
+    if params[:status] == "approve"
+      @topic.approved
+    else
+      @topic.rejected
+    end
+    if @note.update(approved: params[:status] == "approve" ? true : false) and @topic.save
+      redirect_to overture_topic_notes_path(@topic), notice: "Successfully updated topic."
+    else
+      render :edit
+    end
+  end
+
   private
   def get_topic
     @topic = Topic.find(params[:topic_id])
+  end
+
+  def get_note
+    @note = Note.find(params[:id])
   end
 
   def set_company
--- app/controllers/overture/topics_controller.rb
+++ app/controllers/overture/topics_controller.rb
@@ -4,6 +4,7 @@ class Overture::TopicsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :set_company
+  before_action :set_topic, only: [:update]
 
   def index
     # Question category comes from the sidebar
@@ -24,6 +25,14 @@ class Overture::TopicsController < ApplicationController
     end
   end
 
+  def update
+    if @topic.update(topic_params)
+      redirect_to overture_topic_notes_path(@topic), notice: "Successfully updated topic."
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_company
@@ -31,7 +40,11 @@ class Overture::TopicsController < ApplicationController
     @company = current_user.company
   end
 
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
   def topic_params
-    params.require(:topic).permit(:subject_name, :status, :question_category, :user, :company, :startup_id, notes_attributes: [:content, :user_id, :approved])
+    params.require(:topic).permit(:subject_name, :status, :question_category, :user, :company, :startup_id, :assigned_user_id, notes_attributes: [:content, :user_id, :approved])
   end
 end
