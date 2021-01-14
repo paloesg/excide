0a49c68e432d9e58a4467980a96c97a3eb0cb192
Jonathanlau92 <jonathan@excide.co>
2021-01-14 09:41:43 +0800

    Minor code fixes to ensure QnA modal in dataroom works

--- app/controllers/overture/folders_controller.rb
+++ app/controllers/overture/folders_controller.rb
@@ -24,6 +24,7 @@ class Overture::FoldersController < FoldersController
     @folders = policy_scope(Folder).children_of(@folder)
     @activities = PublicActivity::Activity.order("created_at desc").where(trackable_type: "Document").first(10)
     @documents = Document.where(folder: @folder)
+    @topic = Topic.new
 
     render "overture/documents/index"
   end
