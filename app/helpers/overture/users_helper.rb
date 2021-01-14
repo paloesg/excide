a8b30603cb0f2d10642ff5e03fa8873ef1fdd006
Jonathanlau92 <jonathan@excide.co>
2021-01-14 10:20:10 +0800

    Add view embedded document to investor dataroom

--- app/helpers/overture/users_helper.rb
+++ app/helpers/overture/users_helper.rb
@@ -1,8 +1,8 @@
 module Overture::UsersHelper
   def get_users(company)
     if company.investors.present?
-      # Add users from current company & users from investor company
-      company.users.includes(:permissions) + company.investors.map(&:users).flatten
+      # Add users from current company & users from investor company (Since investor cant share currently, it might not even be used)
+      company.users.includes(:permissions) + company.startups.map(&:users).flatten
     elsif company.startups.present?
       company.users.includes(:permissions) + company.investors.map(&:users).flatten
     else
