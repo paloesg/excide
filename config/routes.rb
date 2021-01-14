ff2667e3b478c1256b5b50039906c2094148a4bb
Jonathanlau92 <jonathan@excide.co>
2021-01-14 13:52:29 +0800

    Route to dataroom

--- config/routes.rb
+++ config/routes.rb
@@ -153,7 +153,7 @@ Rails.application.routes.draw do
   end
 
   namespace :overture do
-    root to: 'home#index'
+    root to: 'documents#index'
     resources :companies
     resources :investments
     resources :permissions
