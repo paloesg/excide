f7c274f7f029589fa0f0af6c9558cf68463afaab
LimNing <e0324216@u.nus.edu>
2021-01-15 14:53:50 +0800

    Change ENV variable to new template id

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -67,7 +67,7 @@ class NotificationMailer < ApplicationMailer
 
   def motif_new_master_franchisee(user)
     #to email will be changed with afc's support email
-    mail(to: 'support@paloe.com.sg', from: 'Asiawide Digital Advantage (ADA) <admin@excide.co>', subject: 'New Outlet', body: 'New Master Franchisee', template_id: ENV['ADA_SENDGRID_MOTIF_NEW_OUTLET'], dynamic_template_data: {
+    mail(to: 'support@paloe.com.sg', from: 'Asiawide Digital Advantage (ADA) <admin@excide.co>', subject: 'New Franchisee', body: 'New franchisee', template_id: ENV['ADA_SENDGRID_MOTIF_NEW_FRANCHISEE'], dynamic_template_data: {
         fullName: user.full_name,
         company: user.company.name,
         email: user.email
