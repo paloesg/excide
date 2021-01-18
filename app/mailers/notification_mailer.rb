0d76c2bc9651bcb805c3fcd1956330d9fb7390e3
Han Sheng Chin <hschin@gmail.com>
2021-01-18 11:39:03 +0800

    Update email from address to Paloe support email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,10 @@
 class NotificationMailer < ApplicationMailer
-  default from: 'Excide Symphony <admin@excide.co>'
+  default from: 'Paloe <support@paloe.com.sg>'
   require 'sendgrid-ruby'
   include SendGrid
-  
+
   def batch_reminder(reminders, user)
-    mail(to: user.email, from: 'Paloe Symphony <admin@excide.co>', subject: 'Here is your daily email summary', body: 'Some body',  template_id: ENV['SENDGRID_BATCH_REMINDER_EMAIL_TEMPLATE'], dynamic_template_data: {
+    mail(to: user.email, from: 'Paloe Symphony <support@paloe.com.sg>', subject: 'Here is your daily email summary', body: 'Some body',  template_id: ENV['SENDGRID_BATCH_REMINDER_EMAIL_TEMPLATE'], dynamic_template_data: {
         firstName: user.first_name,
         reminders: reminders,
         reminder_count: reminders.count
@@ -13,7 +13,7 @@ class NotificationMailer < ApplicationMailer
   end
 
   def daily_summary(action_details, user, link)
-    mail(to: user.email, from: 'Paloe Symphony <admin@excide.co>', subject: 'Here is your daily email summary', body: 'Some body',  template_id: ENV['SENDGRID_DAILY_SUMMARY_EMAIL_TEMPLATE'], dynamic_template_data: {
+    mail(to: user.email, from: 'Paloe Symphony <support@paloe.com.sg>', subject: 'Here is your daily email summary', body: 'Some body',  template_id: ENV['SENDGRID_DAILY_SUMMARY_EMAIL_TEMPLATE'], dynamic_template_data: {
         firstName: user.first_name,
         actions: action_details,
         task_count: action_details.count,
@@ -23,18 +23,18 @@ class NotificationMailer < ApplicationMailer
   end
 
   def overture_notification(user, profile)
-    mail(to: "enquiry@paloe.com.sg", from: 'Paloe Overture <admin@excide.co>', subject: 'TO SAM!', body: 'Some body',  template_id: ENV['SENDGRID_OVERTURE_STATED_INTEREST'], dynamic_template_data: {
+    mail(to: "enquiry@paloe.com.sg", from: 'Paloe Overture <support@paloe.com.sg>', subject: 'TO SAM!', body: 'Some body',  template_id: ENV['SENDGRID_OVERTURE_STATED_INTEREST'], dynamic_template_data: {
         fullName: user.full_name,
         email: user.email,
         contactNumber: user.contact_number,
         profileName: profile.name,
-        url: profile.url, 
+        url: profile.url,
       }
     )
   end
 
   def motif_notify_franchisor(franchisor, user, wfa, link)
-    mail(to: franchisor.email, from: 'Paloe ADA <admin@excide.co>', subject: '', body: 'Some body',  template_id: ENV['ADA_SENDGRID_NOTIFY_FRANCHISOR'], dynamic_template_data: {
+    mail(to: franchisor.email, from: 'Paloe ADA <support@paloe.com.sg>', subject: '', body: 'Some body',  template_id: ENV['ADA_SENDGRID_NOTIFY_FRANCHISOR'], dynamic_template_data: {
         franchisor_fullname: franchisor.full_name,
         franchisor_email: franchisor.email,
         user_fullname: user.full_name,
@@ -46,7 +46,7 @@ class NotificationMailer < ApplicationMailer
   end
 
   def motif_renewal_notice_outlet(franchisee, user)
-    mail(to: user.email, from: 'Paloe ADA <admin@excide.co>', subject: '', body: 'Some body',  template_id: ENV['ADA_SENDGRID_FRANCHISEE_RENEWAL_NOTICE'], dynamic_template_data: {
+    mail(to: user.email, from: 'Paloe ADA <support@paloe.com.sg>', subject: '', body: 'Some body',  template_id: ENV['ADA_SENDGRID_FRANCHISEE_RENEWAL_NOTICE'], dynamic_template_data: {
         franchisee_name: franchisee.franchise_licensee,
         franchisee_expiry_date: franchisee.expiry_date,
         user_fullname: user.full_name,
@@ -54,7 +54,7 @@ class NotificationMailer < ApplicationMailer
       }
     )
   end
-  
+
   # Conductor's email notification methods!
   # Most of the removed mailer methods below were called in event.rb. associate_notification was called in scehduler.rake as a daily reminder to associate.
   # Removed conductor's create_event, edit_event, destroy_event, user_removed_from_event, event_notification and associate_notification methods from the following PR:
