632ebaa94bea3ffc4f9ed55a059de7c63e934bdf
Jonathanlau92 <jonathan@excide.co>
2021-01-22 09:43:01 +0800

    Fix using RichTextAreaField for more than 1 model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ApplicationRecord
   belongs_to :created_by, class_name: 'User'
   belongs_to :contact_status
 
-  has_rich_text :company_information
+  has_rich_text :investor_information
 
   has_many :notes, as: :notable
 end
