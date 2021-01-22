8dfd2f7117a1364499951408cb1f7fdf31bb5fd6
Jonathanlau92 <jonathan@excide.co>
2021-01-22 11:19:05 +0800

    Separate profiles and contacts SHOW page

--- app/controllers/overture/contacts_controller.rb
+++ app/controllers/overture/contacts_controller.rb
@@ -1,8 +1,28 @@
 class Overture::ContactsController < ApplicationController
+  layout 'overture/application'
+
   before_action :authenticate_user!
   before_action :set_company
+  before_action :set_contact, only: [:show]
+  after_action :verify_authorized
 
   def index
-    @contacts = Contact.all
+    # Only get investor's contact if they allow it to be public
+    @contacts = Contact.where(public: true)
+    @contacts = Kaminari.paginate_array(@contacts).page(params[:page]).per(5)
+  end
+
+  def show
+    authorize @contact
+    @topic = Topic.new
+  end
+
+  private
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def set_company
+    @company = current_user.company
   end
 end
