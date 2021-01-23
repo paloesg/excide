7be59771610620dd085bdf1880e55af4f13ca6ac
Jonathanlau92 <jonathan@excide.co>
2021-01-23 11:47:40 +0800

    Create duplicate contact when add to fundraising board

--- app/controllers/overture/contacts_controller.rb
+++ app/controllers/overture/contacts_controller.rb
@@ -12,6 +12,22 @@ class Overture::ContactsController < ApplicationController
     @contacts = Kaminari.paginate_array(@contacts).page(params[:page]).per(5)
   end
 
+  def create
+    # This method is called when adding contacts to fundraising board. It will duplicate the listed contact and set to the 1st contact status of the board.
+    @contact = Contact.find(params[:contact_id])
+    @duplicate_contact = @contact.dup
+    # Find the 1st contact status of the board (Shortlisted)
+    @contact_status = @company.contact_statuses.find_by(position: 1)
+    @duplicate_contact.contact_status = @contact_status
+    # Duplicate contact shouldn't be in the public listing
+    @duplicate_contact.public = false
+    if @duplicate_contact.save
+      redirect_to overture_contact_statuses_path, notice: "Investor contact added to fundraising board."
+    else
+      redirect_to overture_root_path, alert: "Error occurred when adding investor. Add a support ticket or try again in awhile."
+    end
+  end
+
   def show
     authorize @contact
     @topic = Topic.new
