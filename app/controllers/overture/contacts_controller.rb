651f77e3808a0f0e8752ff22facec64a44458766
Jonathanlau92 <jonathan@excide.co>
2021-01-26 11:55:29 +0800

    Use deep_cloneable gem to clone contacts with actiontext and active storage

--- app/controllers/overture/contacts_controller.rb
+++ app/controllers/overture/contacts_controller.rb
@@ -14,17 +14,27 @@ class Overture::ContactsController < ApplicationController
 
   def create
     if params[:contact_id].present?
-      # This method is called when adding contacts or adding existing investor to fundraising board. It will duplicate the listed contact and set to the 1st contact status of the board.
-      @contact_to_be_duplicated = Contact.find(params[:contact_id])
-      @contact = @contact_to_be_duplicated.dup
+      # This method is called when adding contacts or adding existing investor to fundraising board. It will clone the contact and set to the 1st contact status of the board.
+      contact_to_be_duplicated = Contact.find(params[:contact_id])
+      # Deep clone active storage attachment and action text rich text
+      @contact = contact_to_be_duplicated.deep_clone include: [:rich_text_investor_information] do |original, kopy|
+        if kopy.is_a?(Contact) && original.investor_company_logo.attached?
+          original.investor_company_logo.open do |tempfile|
+            kopy.investor_company_logo.attach({
+              io: File.open(tempfile.path),
+              filename: original.investor_company_logo.blob.filename,
+              content_type: original.investor_company_logo.blob.content_type
+            })
+          end
+        end
+      end
+      # @contact = @contact_to_be_duplicated.dup
       # Find the 1st contact status of the board (Shortlisted)
-      @contact_status = @company.contact_statuses.find_by(position: 1)
-      @contact.contact_status = @contact_status
+      contact_status = @company.contact_statuses.find_by(position: 1)
+      @contact.contact_status = contact_status
       # Duplicate contact shouldn't be searchable
       @contact.searchable = false
       @contact.cloned_by = @company
-      # Attach the investor company logo when duplicating contact
-      @contact.investor_company_logo.attach(@contact_to_be_duplicated.investor_company_logo.blob) if @contact_to_be_duplicated.investor_company_logo.attached?
     else
       # Add new investor's contact
       @contact = Contact.new(contact_params)
@@ -60,7 +70,7 @@ class Overture::ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :phone, :email, :company_name, :created_by_id, :company_id, :contact_status_id, :cloned_by_id, :searchable)
+    params.require(:contact).permit(:name, :phone, :email, :company_name, :created_by_id, :company_id, :contact_status_id, :cloned_by_id, :searchable, :investor_information, :investor_company_logo)
   end
 
   def set_contact
