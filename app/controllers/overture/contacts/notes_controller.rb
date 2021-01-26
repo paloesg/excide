b3f6b2c920ad22d3c6ce7e511dbd5b5486f8666f
Jonathanlau92 <jonathan@excide.co>
2021-01-26 14:07:38 +0800

    Add adding notes to fundraising board contacts

--- app/controllers/overture/contacts/notes_controller.rb
+++ app/controllers/overture/contacts/notes_controller.rb
@@ -2,7 +2,22 @@ class Overture::Contacts::NotesController < Overture::NotesController
   before_action :set_contact
 
   def index
+    @notes = @contact.notes
+  end
 
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+    @note.notable = @contact
+    if @note.save
+      redirect_to overture_contact_notes_path(contact_id: @contact.id), notice: "Note has been saved successfully."
+    else
+      render :new
+    end
   end
 
   private
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,5 @@
 class Note < ApplicationRecord
   belongs_to :notable, polymorphic: true
-  belongs_to :contact
   belongs_to :user
   belongs_to :workflow_action
 end
