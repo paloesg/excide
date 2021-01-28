216865176bb0dc30dd1f5870076097129983bed6
Jonathanlau92 <jonathan@excide.co>
2021-01-29 00:37:14 +0800

    Add condition to NOT display subfranchisee on 1 level higher company

--- app/helpers/motif/outlets_helper.rb
+++ app/helpers/motif/outlets_helper.rb
@@ -5,12 +5,12 @@ module Motif::OutletsHelper
         # Get all outlets of the company (direct owned)
         Outlet.includes(:company).where(company_id: company).order("created_at asc")
       else
-        # Get all outlets from 1 layer down. Add condition to check for unit franchisee and remove them from queries
-        company.children.map(&:outlets).flatten
+        # Get all outlets from 1 layer down. Get only outlets which franchisees has no franchise_licensee name, as it meant direct outlets. Should NOT get children's unit franchisee outlets
+        company.children.includes(outlets: [:franchisee]).where(outlets: { franchisees: { franchise_licensee: ""}}).map(&:outlets).flatten
       end
     else
       # Add all outlets (direct-owned or sub franchised)
-      Outlet.includes(:company).where(company_id: company).order("created_at asc") + company.children.map(&:outlets).flatten
+      Outlet.includes(:company).where(company_id: company).order("created_at asc") + company.children.includes(outlets: [:franchisee]).where(outlets: { franchisees: { franchise_licensee: ""}}).map(&:outlets).flatten
     end
   end
 end
