c1db63d0fdd47ad809b16d69f2f326ae69312b7c
LimNing <e0324216@u.nus.edu>
2021-02-08 21:46:13 +0800

    Perform background job to import the excel

--- app/services/generate_events_service.rb
+++ app/services/generate_events_service.rb
@@ -17,22 +17,7 @@ class GenerateEventsService
   def import_excel
     # Import excel using roo and generates events in the timesheet
     data = Roo::Spreadsheet.open(@file).sheet("Template")
-    header = data.row(8)
-    data.drop(8).each do |row|
-      # create hash from headers and cells
-      event_data = Hash[[header, row].transpose]
-      @event = Event.new
-      @event.company = @user.company
-      @department = @user.department
-      @client = Category.create_or_find_by(name: event_data["Client"], category_type: "client", department: @department)
-      @service_line = Category.create_or_find_by(name: event_data["Job Function"], category_type: "service_line", department: @department)
-      @project = Category.create_or_find_by(name: event_data["Project"], category_type: "project", department: @department)
-      @task = Category.create_or_find_by(name: event_data["Job Nature"], category_type: "task", department: @department)
-      @event.categories << @client << @service_line << @project << @task
-      @event.start_time = event_data["Date (MM/DD/YYYY)"]
-      @event.number_of_hours = event_data["No. of hours"]
-      @event.save!
-      GenerateTimesheetAllocationService.new(@event, @user).run
-    end
+    data = data.parse(client: "Client", service_line: "Job Function", project: "Project", task: "Job Nature", date: "Date (DD/MM/YYYY)", hours: "No. of hours")
+    CreateEventsJob.perform_later(@user, data)
   end
 end
