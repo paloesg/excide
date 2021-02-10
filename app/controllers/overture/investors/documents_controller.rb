31ddd0d96c9e57d012aa150736757890dfcc72c7
Jonathanlau92 <jonathan@excide.co>
2021-02-10 15:13:53 +0800

    Add shared drive to startup documents

--- app/controllers/overture/investors/documents_controller.rb
+++ app/controllers/overture/investors/documents_controller.rb
@@ -12,7 +12,7 @@ class Overture::Investors::DocumentsController < Overture::DocumentsController
     @permissible_company = params[:company_id].present? ? Company.find_by(id: params[:company_id]) : @company
     # For investor, they can see documents where they have can_view (or higher) permissions in their group. Whereas for startup, they can see if they have role permissions
     @documents = Document.where(folder_id: nil, company: @permissible_company).order(created_at: :desc).includes(:permissions).where(permissions: {can_view: true, role_id: @user.roles.map(&:id)})
-    @folders = Folder.roots.includes(:permissions).where(company: @permissible_company, permissions: { can_view: true, role_id: @user.roles.map(&:id)}).where.not(name: "Resource Portal")
+    @folders = Folder.roots.includes(:permissions).where(company: @permissible_company, permissions: { can_view: true, role_id: @user.roles.map(&:id)}).where.not(name: ["Resource Portal", "Shared Drive"])
     @roles = Role.where(resource_id: @company.id, resource_type: "Company").where.not(name: ["admin", "member"])
     @users = get_users(@company)
     # Show shared files for investor without being signed for their due diligence
