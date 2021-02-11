c72dec235cb0a8d7f2f95bfdb64015981e51c62e
Jonathanlau92 <jonathan@excide.co>
2021-02-11 21:54:42 +0800

    Do not allow resource portal and shared drive folders to show in dataroom investor

--- app/controllers/overture/investors/documents_controller.rb
+++ app/controllers/overture/investors/documents_controller.rb
@@ -10,7 +10,9 @@ class Overture::Investors::DocumentsController < Overture::DocumentsController
     @permissible_company = params[:company_id].present? ? Company.find_by(id: params[:company_id]) : @company
     # For investor, they can see documents where they have can_view (or higher) permissions in their group. Whereas for startup, they can see if they have role permissions
     @documents = Document.where(folder_id: nil, company: @permissible_company).order(created_at: :desc).includes(:permissions).where(permissions: {can_view: true, role_id: @user.roles.map(&:id)})
-    @folders = Folder.roots.includes(:permissions).where(company: @permissible_company, permissions: { can_view: true, role_id: @user.roles.map(&:id)}).where.not(name: ["Resource Portal", "Shared Drive"])
+    @shared_folder = Folder.find_by(name: "Shared Drive")
+    @resource_portal_folder = Folder.find_by(name: "Resource Portal")
+    @folders = Folder.all.includes(:permissions).where(company: @permissible_company, permissions: { can_view: true, role_id: @user.roles.map(&:id)}).where.not(name: ["Resource Portal", "Shared Drive"], ancestry: [@shared_folder.id, @resource_portal_folder.id])
     @roles = Role.where(resource_id: @company.id, resource_type: "Company").where.not(name: ["admin", "member"])
     @users = get_users(@company)
     @topic = Topic.new
