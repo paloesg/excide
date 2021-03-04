a4adcb609ee9796b8931c73b71d77493eade7c44
Jonathanlau92 <jonathan@excide.co>
2021-03-04 20:09:30 +0800

    Add ability to edit notes

--- app/controllers/overture/contacts/notes_controller.rb
+++ app/controllers/overture/contacts/notes_controller.rb
@@ -1,6 +1,7 @@
 class Overture::Contacts::NotesController < Overture::NotesController
   before_action :set_contact
-  before_action :set_contact_statuses, only: [:index, :new]
+  before_action :set_note, only: [:edit, :update]
+  before_action :set_contact_statuses, only: [:index, :new, :edit]
 
   def index
     @notes = @contact.notes
@@ -21,6 +22,17 @@ class Overture::Contacts::NotesController < Overture::NotesController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @note.update(note_params)
+      redirect_to overture_contact_notes_path(contact_id: @contact.id), notice: "Note has been updated successfully."
+    else
+      render :edit
+    end
+  end
+
   private
   def set_contact_statuses
     @contact_statuses = ContactStatus.where(startup: @company)
@@ -28,5 +40,9 @@ class Overture::Contacts::NotesController < Overture::NotesController
 
   def set_contact
     @contact = Contact.find(params[:contact_id])
+  end
+
+  def set_note
+    @note = @contact.notes.find(params[:id])
   end
 end
