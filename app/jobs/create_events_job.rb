d529bb7e94a2cbd67c8882da43e8e881bef38f93
LimNing <e0324216@u.nus.edu>
2021-03-09 13:57:57 +0800

    Apply the titleize helper method when importing excel

--- app/jobs/create_events_job.rb
+++ app/jobs/create_events_job.rb
@@ -1,5 +1,6 @@
 class CreateEventsJob < ApplicationJob
   include SuckerPunch::Job
+  include ApplicationHelper
   queue_as :default
 
   def perform(user, data)
@@ -8,10 +9,10 @@ class CreateEventsJob < ApplicationJob
       @event = Event.new
       @event.company = user.company
       @department = user.department
-      @event.categories << Category.find_or_create_by(name: row[:client], category_type: "client", department: @department) if row[:client].present?
-      @event.categories << Category.find_or_create_by(name: row[:service_line], category_type: "service_line", department: @department) if row[:service_line].present?
-      @event.categories << Category.find_or_create_by(name: row[:project], category_type: "project", department: @department) if row[:project].present?
-      @event.categories << Category.find_or_create_by(name: row[:task], category_type: "task", department: @department) if row[:task].present?
+      @event.categories << Category.find_or_create_by(name: titleize_keep_uppercase(row[:client]), category_type: "client", department: @department) if row[:client].present?
+      @event.categories << Category.find_or_create_by(name: titleize_keep_uppercase(row[:service_line]), category_type: "service_line", department: @department) if row[:service_line].present?
+      @event.categories << Category.find_or_create_by(name: titleize_keep_uppercase(row[:project]), category_type: "project", department: @department) if row[:project].present?
+      @event.categories << Category.find_or_create_by(name: titleize_keep_uppercase(row[:task]), category_type: "task", department: @department) if row[:task].present?
       @event.start_time = row[:date]
       @event.number_of_hours = row[:hours]
       @event.save!
