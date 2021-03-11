14ddb6c602cc2c8f2c6401ad4f88a62851b21329
LimNing <e0324216@u.nus.edu>
2021-03-11 19:48:07 +0800

    Return code that was deleted

--- app/controllers/conductor/allocations_controller.rb
+++ app/controllers/conductor/allocations_controller.rb
@@ -126,6 +126,8 @@ class Conductor::AllocationsController < ApplicationController
   end
 
   def set_month_allocations
+    @date_from = params[:start_date] ? params[:start_date].to_date : Date.current.beginning_of_month
+    @date_to = params[:end_date] ? params[:end_date].to_date : Date.current.end_of_month
     @allocations = Allocation.where(event: params[:events]).order(allocation_date: :desc, start_time: :asc, id: :asc)
   end
 
