3631d49bbb7a8b64cc7685520a3289624a783180
LimNing <e0324216@u.nus.edu>
2021-03-12 09:02:33 +0800

    Create and update announcements on index page using modal

--- app/controllers/overture/startups/posts_controller.rb
+++ app/controllers/overture/startups/posts_controller.rb
@@ -9,6 +9,26 @@ class Overture::Startups::PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.author = @user
+    @post.company = @company
+    if @post.save
+      redirect_to overture_startups_posts_path
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to overture_startups_posts_path, notice: 'Announcement was successfully updated.' }
+      else
+        format.html { redirect_to overture_startups_posts_path, alert: 'Announcement was not updated.' }
+      end
+    end
+  end
+
   private
 
   def post_params
