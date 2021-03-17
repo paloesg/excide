4e0de23ac8b7ac07525b9edc2bb0880ff7d431fa
Jonathanlau92 <jonathan@excide.co>
2021-03-17 22:43:02 +0800

    Add comments to policies

--- app/policies/overture/company_policy.rb
+++ app/policies/overture/company_policy.rb
@@ -1,4 +1,5 @@
 class Overture::CompanyPolicy < CompanyPolicy
+  # This policy covers accessing the potential startups page for investor's dataroom. Should not authorize startup users to access this page
   def index?
     user.company.investor?
   end
--- app/policies/overture/investors/document_policy.rb
+++ app/policies/overture/investors/document_policy.rb
@@ -1,4 +1,5 @@
 class Overture::Investors::DocumentPolicy < DocumentPolicy
+  # This policy authorize only investor company to access investor's dataroom
   def index?
     user.company.investor?
   end
