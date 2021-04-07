04cc1b2dc0215925a1b8844395c2c1d5221c2d11
Jonathanlau92 <jonathan@excide.co>
2021-04-07 23:21:41 +0800

    Add dataroom banner and tour

--- app/webpacker/src/javascripts/dashboard/components/product_tour/dataroom_intro.js
+++ app/webpacker/src/javascripts/dashboard/components/product_tour/dataroom_intro.js
@@ -4,22 +4,22 @@ $(document).on("turbolinks:load", function () {
     introJs().setOptions({
       steps: [{
         title: 'Welcome',
-        intro: 'Hello World! 👋'
+        intro: 'Here is a tour on how to use overture dataroom! 👋'
       },
       {
         element: document.querySelector('.dataroom-tour-1'),
-        title: "Secured sharing with potential investors and others",
-        intro: 'You can share confidential files easily and securely here. Take a tour to quickly learn.'
-      },
-      {
-        title: 'Upload and Create Folder',
-        element: document.querySelector('.dataroom-tour-2'),
+        title: "Upload and Create Folder",
         intro: 'To upload documents or create new folder, click this "+" button.'
       },
       {
         title: 'Give Different Access',
+        element: document.querySelector('.dataroom-tour-2'),
+        intro: 'You can give different access to different people to different files. Clicking each of the icon will turn on or off each access. Hover your mouse over to see the types of access.'
+      },
+      {
+        title: 'One-click Share to Many',
         element: document.querySelector('.dataroom-tour-3'),
-        intro: 'You can give different access to different people to different files. Clicking each of the icon will turn on or off each access.'
+        intro: 'Each column represent one group of people. Every time you give access, it is applied to multiple users at the same time.'
       },
       {
         title: 'Customise Your Groups',
