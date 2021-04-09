21c3b6ee9c2a5b9c6ccb62dc8fbf241f014aabe4
Jonathanlau92 <jonathan@excide.co>
2021-04-09 09:50:59 +0800

    Add policy for contact statuses

--- app/controllers/motif/contact_statuses_controller.rb
+++ app/controllers/motif/contact_statuses_controller.rb
@@ -3,6 +3,11 @@ class Motif::ContactStatusesController < ApplicationController
 
   before_action :authenticate_user!
 
+  def index
+    @contact_statuses = policy_scope(ContactStatus).order('position ASC')
+    # @existing_contacts = Contact.includes(:company).where(companies: {company_type: "investor"}, searchable: true)
+  end
+
   private
   def contact_status_params
     params.require(:contact_status).permit(:name, :position, :company_id)
