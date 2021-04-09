d1693b001a546efbeb81fcd88735464b43b036b3
Jonathanlau92 <jonathan@excide.co>
2021-04-09 12:38:14 +0800

    Add contacts controller and route

---
+++ app/controllers/motif/contacts_controller.rb
@@ -0,0 +1,86 @@
+class Motif::ContactsController < ApplicationController
+  layout 'motif/application'
+
+  before_action :authenticate_user!
+  before_action :set_company
+  # before_action :set_contact, only: [:show, :update]
+  # after_action :verify_authorized
+
+  def index
+    authorize Contact
+    # Only get investor's contact if they allow it to be public
+    @contacts = Contact.where(searchable: true)
+    @contact_statuses = ContactStatus.where(startup: @company)
+    @contacts = Kaminari.paginate_array(@contacts).page(params[:page]).per(5)
+  end
+
+  # def create
+  #   if params[:contact_id].present?
+  #     # This method is called when adding contacts or adding existing investor to fundraising board. It will clone the contact and set to the 1st contact status of the board.
+  #     contact_to_be_duplicated = Contact.find(params[:contact_id])
+  #     # Deep clone active storage attachment and action text rich text
+  #     @contact = contact_to_be_duplicated.deep_clone include: [:rich_text_investor_information] do |original, kopy|
+  #       if kopy.is_a?(Contact) && original.investor_company_logo.attached?
+  #         original.investor_company_logo.open do |tempfile|
+  #           kopy.investor_company_logo.attach({
+  #             io: File.open(tempfile.path),
+  #             filename: original.investor_company_logo.blob.filename,
+  #             content_type: original.investor_company_logo.blob.content_type
+  #           })
+  #         end
+  #       end
+  #     end
+  #     # Find the 1st contact status of the board (Shortlisted)
+  #     contact_status = @company.contact_statuses.find_by(position: 1)
+  #     @contact.contact_status = contact_status
+  #     # Duplicate contact shouldn't be searchable
+  #     @contact.searchable = false
+  #     @contact.cloned_by = @company
+  #   else
+  #     # Add new investor's contact
+  #     @contact = Contact.new(contact_params)
+  #     @contact.created_by = current_user
+  #   end
+  #   # Redirect based on validation of contact
+  #   if @contact.save
+  #     redirect_to overture_contact_statuses_path, notice: "Investor contact added to fundraising board."
+  #   else
+  #     redirect_to overture_root_path, alert: "Error occurred when adding investor. Add a support ticket or try again in awhile."
+  #   end
+  # end
+
+  # def update
+  #   @contact_status = ContactStatus.find_by(id: params[:contact_status_id])
+  #   # On the search investor page, the AJAX contact is the searchable contact, hence we need to get the cloned contact and update the cloned one instead of the searchable contact.
+  #   @cloned_contact = get_cloned_contact(@contact, current_user) if params[:contact_type].present?
+  #   respond_to do |format|
+  #     # If cloned contact
+  #     if params[:contact_type].present? ? @cloned_contact.update(contact_status: @contact_status) : @contact.update(contact_status: @contact_status)
+  #       format.json { render json: { link_to: overture_contact_statuses_path, status: "ok" } }
+  #     else
+  #       format.html { redirect_to overture_root_path }
+  #       format.json { render json: @contact.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # def show
+  #   authorize @contact
+  #   @topic = Topic.new
+  #   @contact_statuses = ContactStatus.where(startup: @company)
+  # end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :email, :company_name, :created_by_id, :company_id, :contact_status_id)
+  end
+
+  # def set_contact
+  #   @contact = Contact.find(params[:id])
+  # end
+
+  def set_company
+    @company = current_user.company
+  end
+end
