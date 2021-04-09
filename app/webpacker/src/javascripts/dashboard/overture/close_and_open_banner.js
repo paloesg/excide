e371831f4bd034713fdf2a4fd6aa5b412cf4cc91
LimNing <e0324216@u.nus.edu>
2021-04-09 18:09:47 +0800

    Add banner toggle js animation

--- app/webpacker/src/javascripts/dashboard/overture/close_and_open_banner.js
+++ app/webpacker/src/javascripts/dashboard/overture/close_and_open_banner.js
@@ -1,12 +1,10 @@
 $(document).on("turbolinks:load", function () {
   // Hide the onboarding banner when clicked close button
   $(".button-top-right").click(function(){
-    $(this).closest(".card").addClass('d-none');
+    $(this).closest(".card").slideUp();
   });
 
   $(".open-banner").click(function(){
-    console.log("What is this: ", $(this));
-    console.log("What is this parents: ", $(this).closest(".row").prev());
-    $(this).closest(".row").prev().removeClass('d-none');
+    $(this).closest(".row").next().slideToggle();
   })
 });
