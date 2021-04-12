27f5d743758a015591a6583636a089ff675f3d74
Jonathanlau92 <jonathan@excide.co>
2021-04-12 15:11:18 +0800

    Simplify creating status in kanban board

--- app/controllers/motif/contact_statuses_controller.rb
+++ app/controllers/motif/contact_statuses_controller.rb
@@ -2,11 +2,27 @@ class Motif::ContactStatusesController < ApplicationController
   layout 'motif/application'
 
   before_action :authenticate_user!
+  before_action :set_company, only: :create
   before_action :set_contact_status, only: [:update, :destroy]
 
   def index
     @contact_statuses = policy_scope(ContactStatus).order('position ASC')
     @contact = Contact.new
+  end
+
+  def create
+    @contact_status = ContactStatus.new(contact_status_params)
+    @contact_status.position = ContactStatus.where(company: @company).order(position: :asc).last.position + 1
+    @contact_status.company = @company
+    respond_to do |format|
+      if @contact_status.save
+        format.html { redirect_to motif_contact_statuses_path, notice: 'Status created successfully!' }
+        format.json { render json: @contact_status, status: :ok }
+      else
+        format.html { render :edit }
+        format.json { render json: @contact_status.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
@@ -33,6 +49,10 @@ class Motif::ContactStatusesController < ApplicationController
   private
   def set_contact_status
     @contact_status = ContactStatus.find(params[:id])
+  end
+
+  def set_company
+    @company = current_user.company
   end
 
   def contact_status_params
