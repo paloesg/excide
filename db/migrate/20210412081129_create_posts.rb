c876ea8da89032c3b6be04d7cc7f1975b1e3da9b
Jonathanlau92 <jonathan@excide.co>
2021-04-12 16:15:42 +0800

    Add create post and post ref to documents migration

---
+++ db/migrate/20210412081129_create_posts.rb
@@ -0,0 +1,13 @@
+# Create posts table and add reference to documents
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts, id: :uuid do |t|
+      t.string :title
+      t.text :content
+      t.references :company, type: :uuid, foreign_key: true
+      t.references :author, foreign_key: {to_table: :users}
+
+      t.timestamps
+    end
+  end
+end
