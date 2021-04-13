4ee05cb68bdb6a96918a025261fb3db2df33b0ef
Jonathanlau92 <jonathan@excide.co>
2021-04-13 14:47:59 +0800

    Modify helper to get outlets correctly when creating workflow

--- app/helpers/motif/outlets_helper.rb
+++ app/helpers/motif/outlets_helper.rb
@@ -18,13 +18,13 @@ module Motif::OutletsHelper
   def get_outlets_that_has_no_workflows(template_type, company)
     # Check if workflow of a particular template_type exists
     workflows = Workflow.includes(:template).where(company: company, templates: { template_type: template_type })
-    # This checks for parent workflows, as direct owned outlet are usually created by parent company (for eg, franchisor onboards for it's franchisee). We will need to exclude these workflow's outlets so that it would not appear in the franchisee's dropdown
+    # This checks for parent workflows, as direct owned outlet for franchisee is usually created by parent company (for eg, franchisor onboards for it's franchisee). We will need to exclude these workflow's outlets so that it would not appear in the franchisee's dropdown
     parent_workflows = Workflow.includes(:template).where(company: company.parent, templates: { template_type: template_type })
     # If workflow of that template_type exists, then we will exclude outlets in the dropdown when creating workflows using the array method excluding given in Rails 6
     if workflows.present?
-      (company.outlets + company.children.includes(outlets: [:franchisee]).where(outlets: { franchisees: { franchise_licensee: ""}}).map(&:outlets).flatten).excluding(workflows.map(&:outlet))
+      (company.outlets + company.children.includes(outlets: [:franchisee]).where(outlets: { franchisees: { franchisee_company: [company.children]}}).map(&:outlets).flatten).excluding(workflows.map(&:outlet))
     else
-      (company.outlets + company.children.includes(outlets: [:franchisee]).where(outlets: { franchisees: { franchise_licensee: ""}}).map(&:outlets).flatten).excluding(parent_workflows.map(&:outlet))
+      (company.outlets + company.children.includes(outlets: [:franchisee]).where(outlets: { franchisees: { franchisee_company: [company.children]}}).map(&:outlets).flatten).excluding(parent_workflows.map(&:outlet))
     end
   end
 end
