bde2e426861fcc6b839797945e34c746f23ec402
bilb-blob <66553602+damiandaman@users.noreply.github.com>
2021-05-25 10:56:54 +0800

    Updated code to jQuery

--- app/webpacker/src/javascripts/dashboard/overture/version_control.js
+++ app/webpacker/src/javascripts/dashboard/overture/version_control.js
@@ -1,10 +1,10 @@
 $(document).on("turbolinks:load", function () {
-    $("#version_control_valid_file" ).change(function() {
-        if (document.getElementById("version_control_valid_file").value != "") {
-            $("#vc_submit_button").attr("disabled", false);
-        }
-        else if (document.getElementById("version_control_valid_file").value == "") {
-            $("#vc_submit_button").attr("disabled", true);
-        }
-    });
+  $("#version_control_valid_file").change(function() {
+    if ($("#version_control_valid_file").val() != "") {
+      $("#vc_submit_button").attr("disabled", false);
+    }
+    else {
+      $("#vc_submit_button").attr("disabled", true);
+    }
+  });
 });
