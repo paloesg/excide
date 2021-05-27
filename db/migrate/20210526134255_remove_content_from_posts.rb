b03faf425548dd9a79f85365e0d0dd558641cbf6
Jonathanlau92 <jonathan@excide.co>
2021-05-27 12:07:05 +0800

    Add description to migration file

--- db/migrate/20210526134255_remove_content_from_posts.rb
+++ db/migrate/20210526134255_remove_content_from_posts.rb
@@ -1,3 +1,4 @@
+# This migration removes content from posts as content is using actiontext currently
 class RemoveContentFromPosts < ActiveRecord::Migration[6.1]
   def change
     remove_column :posts, :content
