584cc1f01bade344d383ffb468f798d710c0e4e5
AngelicaTay <angelicatay3@gmail.com>
2021-06-03 17:20:43 +0800

    Improved on the company dropdown display

--- app/webpacker/src/javascripts/dashboard/symphony/header.js
+++ app/webpacker/src/javascripts/dashboard/symphony/header.js
@@ -1,10 +1,28 @@
-$(document).on("turbolinks:load", function(){
+/*$(document).on("turbolinks:load", function(){
   if($('.break-inside-avoid').length > 20) {
     $('#company-dropdown').addClass("col3");
   }
   else if($('.break-inside-avoid').length > 10){
     $('#company-dropdown').addClass("col2");
   }
+  $("#notificationDropdown").click(function() {
+    $("#notificationCount").hide();
+  });
+});
+*/
+
+
+
+$(document).on("turbolinks:load", function(){
+	let len = $('.break-inside-avoid').length;
+	let factor = len/10;
+
+	let noOfCols = "col" + factor;
+
+	if (len > 10) {
+	  $('#company-dropdown').addClass(noOfCols);
+	}
+
   $("#notificationDropdown").click(function() {
     $("#notificationCount").hide();
   });
