748ca4bbc5b3917f1320d854eb82874db6805d2d
LimNing <47408304+LimNing@users.noreply.github.com>
2021-06-03 17:45:03 +0800

    Revert "Overture usage page"

--- app/policies/overture/startups/document_policy.rb
+++ app/policies/overture/startups/document_policy.rb
@@ -3,12 +3,4 @@ class Overture::Startups::DocumentPolicy < DocumentPolicy
   def index?
     user.company.startup?
   end
-
-  def create?
-    user.company.storage_progress < 100
-  end
-
-  def update?
-    create?
-  end
 end
