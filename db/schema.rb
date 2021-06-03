6b7ca2614c0a669820ace3482d90e947d93893f8
LimNing <e0324216@u.nus.edu>
2021-06-03 19:08:07 +0800

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_134255) do
+ActiveRecord::Schema.define(version: 2021_05_31_024015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
