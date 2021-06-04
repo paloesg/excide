dbec3d7cf8281106d6971ee3ee3f4fc4029d581f
AngelicaTay <angelicatay3@gmail.com>
2021-06-04 09:24:02 +0800

    Cleaned up file and reformatted the code

--- app/webpacker/src/javascripts/dashboard/symphony/header.js
+++ app/webpacker/src/javascripts/dashboard/symphony/header.js
@@ -1,28 +1,14 @@
-/*$(document).on("turbolinks:load", function(){
-  if($('.break-inside-avoid').length > 20) {
-    $('#company-dropdown').addClass("col3");
-  }
-  else if($('.break-inside-avoid').length > 10){
-    $('#company-dropdown').addClass("col2");
-  }
-  $("#notificationDropdown").click(function() {
-    $("#notificationCount").hide();
-  });
-});
-*/
 
 $(document).on("turbolinks:load", function(){
-	let len = $('.break-inside-avoid').length;
-	let remainder = len%10;
+  let len = $('.break-inside-avoid').length;
+  let remainder = len%10;
+  let factor = (len - remainder)/10;
 
-	let factor = (len - remainder)/10;
-	alert (factor);
+  let noOfCols = "col" + factor;
 
-	let noOfCols = "col" + factor;
-
-	if (len > 10) {
-	  $('#company-dropdown').addClass(noOfCols);
-	}
+  if (len > 10) {
+    $('#company-dropdown').addClass(noOfCols);
+  }
 
   $("#notificationDropdown").click(function() {
     $("#notificationCount").hide();
