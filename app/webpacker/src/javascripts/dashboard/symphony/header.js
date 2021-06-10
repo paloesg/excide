528e76f3b7183029d5f88fe0876529fa90c4bbc4
AngelicaTay <angelicatay3@gmail.com>
2021-06-10 11:45:49 +0800

    Made company dropdown scrollable vertically with 1 column

--- app/webpacker/src/javascripts/dashboard/symphony/header.js
+++ app/webpacker/src/javascripts/dashboard/symphony/header.js
@@ -1,6 +1,6 @@
 
 $(document).on("turbolinks:load", function(){
-  let len = $('.break-inside-avoid').length;
+  /*let len = $('.break-inside-avoid').length;
   let remainder = len%10;
   let factor = (len - remainder)/10;
 
@@ -8,7 +8,7 @@ $(document).on("turbolinks:load", function(){
 
   if (len > 10) {
     $('#company-dropdown').addClass(noOfCols);
-  }
+  }*/
 
   $("#notificationDropdown").click(function() {
     $("#notificationCount").hide();
