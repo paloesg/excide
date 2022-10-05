b39075c656c7905ff4270f47a0e8b17d773b37ef
Jonathanlau92 <jonathan.lau@paloe.com.sg>
2022-10-05 18:26:08 +0800

    Redirect users to business opportunity page upon login and sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -120,7 +120,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(_resource)
-    "/motif"
+    "/motif/contacts"
   end
 
   def sign_up_params
