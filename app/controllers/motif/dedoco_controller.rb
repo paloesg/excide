a09a454fbe0450005e20db5e5fd6a52ab95e1fd9
Jonathanlau92 <jonathanlaukf92@gmail.com>
2023-01-06 11:22:10 +0800

    Add logs to test for webhook response

--- app/controllers/motif/dedoco_controller.rb
+++ app/controllers/motif/dedoco_controller.rb
@@ -7,6 +7,10 @@ class Motif::DedocoController < ApplicationController
     # Find document based on document hash saved in database
     @document = Document.find_by(doc_hash: params["documents"][0]["document_hash"])
     if @document.present?
+      puts "Returned from Dedoco webhook"
+      puts "Returned from Dedoco webhook #{params["folder_name"]}"
+      puts "Returned from Dedoco webhook hash params #{params.to_h}"
+      puts "Returned from Dedoco #{params}"
       DedocoJob.perform_later(@document, @document.task, "webhook", params)
     else
       puts "No document returned from webhook"
